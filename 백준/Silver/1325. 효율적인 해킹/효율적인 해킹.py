5339e5a954891646ead242f2aeb5d1d7541f2712
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-09-18 18:44:34 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10920 ms, Memory: 215452 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,38 +1,40 @@
-from collections import deque
 import sys
+from collections import deque
 
 input = sys.stdin.readline
+for test_case in range(1):
 
-# bfs
-def graph(n):
-    queue = deque([n])
-    cnt = 0
-    visited = [0] * (N+1)
-    visited[n] = 1
-    while queue:
-        q = queue.popleft()
-        cnt += 1
-        # 해킹가능한 컴퓨터가 존재하지 않을 때 까지 탐색
-        for i in comArr[q]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = 1
-    return cnt
+    n, m = map(int, input().split())
+    graph = [[] for _ in range(n+1)]
+    for _ in range(m):
+        a, b = map(int, input().split())
+        graph[b].append(a)
 
-N, M = map(int, input().split())
-comArr = [[] for _ in range(N+1)]
 
-# 컴퓨터의 A 와 B 관계를 2차원 배열로 정리
-for _ in range(M):
-    A, B = map(int, input().split())
-    comArr[B].append(A)
 
-# 각 컴퓨터마다 최대 해킹 가능한 컴퓨터의 수 저장
-answer = [0 for _ in range(N+1)]
-for i in range(1, N+1):
-    answer[i] = graph(i)
+    def bfs(num):
+        visited = [0] * (n+1)
+        q = deque()
+        ans = 0
+        q.append(num)
+        visited[num] = 1
+        while q:
+            cur_num = q.popleft()
 
-# 해킹 가능한 컴퓨터가 가장 많은 컴퓨터의 번호 출력
-for i in range(1, N+1):
-    if answer[i] == max(answer):
-        print(i, end=' ')
+
+            ans += 1
+            for tmp in graph[cur_num]:
+                if not visited[tmp]:
+
+                    q.append(tmp)
+                    visited[tmp] = 1
+
+        return ans
+
+
+    answer = [0 for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        answer[i] = bfs(i)
+    for i in range(1, n + 1):
+        if answer[i] == max(answer):
+            print(i, end=' ')
