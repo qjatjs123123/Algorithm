c6f6c81c02c76d94eccf069efad17d74df94c3ca
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-09-21 12:43:51 +0900

    [Silver I] Title: 기타리스트, Time: 52 ms, Memory: 31892 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.py
+++ 백준/Silver/1495. 기타리스트/기타리스트.py
@@ -2,18 +2,16 @@ import sys
 for test_case in range(1):
     n, s, m = map(int, sys.stdin.readline().split())
     arr = list(map(int, sys.stdin.readline().split()))
-    dp = [[0 for _ in range(m+1)] for _ in range(n + 1)]
+    dp = [set()] + [set() for _ in range(n)]
+    dp[0].add(s)
 
-    dp[0][s] = 1
-
-    for i in range(n):
-        for j in range(m + 1):
-            if dp[i][j] == 1 and j - arr[i] >= 0:
-                dp[i+1][j - arr[i]] = 1
-            if dp[i][j] == 1 and j + arr[i] <= m:
-                dp[i+1][j + arr[i]] = 1
-    ans = -1
-    for i in range(m + 1):
-        if dp[n][i] == 1:
-            ans = i
-    print(ans)
+    for i in range(1, n + 1):
+        for prev in dp[i-1]:
+            if prev + arr[i-1] <= m:
+                dp[i].add(prev+arr[i-1])
+            if prev - arr[i-1] >= 0:
+                dp[i].add(prev-arr[i-1])
+    if not dp[n]:
+        print(-1)
+    else:
+        print(max(dp[n]))
