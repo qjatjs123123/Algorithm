d5e27844f7a8ddbc2b6d6dd935931ecfb885b4b1
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-09-21 17:16:20 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 124 ms, Memory: 34724 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -1,41 +1,45 @@
 import sys
+from collections import deque
+
+# t = int(sys.stdin.readline())
+input = sys.stdin.readline
 for test_case in range(1):
     n, m, r = map(int, sys.stdin.readline().split())
 
     arr = []
     for _ in range(n):
         arr.append(list(map(int, sys.stdin.readline().split())))
-
+    s = 0
     for depth in range(min(n,m) // 2):
+        q = deque()
+        start_row, end_row, start_col, end_col = depth, n - depth - 1, depth, m - depth - 1
+        for row in range(start_row, end_row):
+            q.append(arr[row][start_col])
 
-        rotate = (m-2*depth)*2 + (n-2*depth)*2 - 4
-        for _ in range(r % rotate):
-            start_row, end_row, start_col, end_col = depth, n - depth - 1, depth, m - depth - 1
-            tmp = arr[start_row][start_col]
-            ## bottom
-            for row in range(start_row, end_row):
-                temp = arr[row + 1][start_col]
-                arr[row + 1][start_col] = tmp
-                tmp = temp
+        for col in range(start_col, end_col):
+            q.append(arr[end_row][col])
 
-            ## right
-            for col in range(start_col,end_col):
-                temp = arr[end_row][col + 1]
-                arr[end_row][col + 1] = tmp
-                tmp = temp
+        for row in range(end_row,start_row,-1):
+            q.append(arr[row][end_col])
 
-            ## top
-            for row in range(end_row, start_row, -1):
-                temp = arr[row-1][end_col]
-                arr[row - 1][end_col] = tmp
-                tmp = temp
+        for col in range(end_col, start_col, -1):
+            q.append(arr[start_row][col])
 
+        cnt = (n - 2*depth)*2 +  (m - 2*depth)*2 - 4
 
-            ##left
-            for col in range(end_col, start_col, -1):
-                temp = arr[start_row][col - 1]
-                arr[start_row][col - 1] = tmp
-                tmp = temp
+        q.rotate(r % cnt)
+
+        for row in range(start_row, end_row):
+            arr[row][start_col] = q.popleft()
+
+        for col in range(start_col, end_col):
+            arr[end_row][col] = q.popleft()
+
+        for row in range(end_row,start_row,-1):
+            arr[row][end_col] = q.popleft()
+
+        for col in range(end_col, start_col, -1):
+            arr[start_row][col] = q.popleft()
 
     for tmp in arr:
         print(' '.join(map(str, tmp)))
