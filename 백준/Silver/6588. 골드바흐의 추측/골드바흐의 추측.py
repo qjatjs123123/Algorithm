2c9ca42cf0d6b74e7f70e105de8dd11ef6f3c9aa
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-09-29 16:12:33 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 2204 ms, Memory: 48600 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,17 +1,19 @@
 import sys
 from collections import deque
 
+
 # t = int(sys.stdin.readline())
 
 prime = [False, False] + [True for i in range(1000000)]
-primes = []
+dp = {}
 for i in range(2, 1000001):
 
     if prime[i]:
-        primes.append(i)
+        if i != 2:
+            dp[i] = True
         for j in range(2*i, 1000001, i):
             prime[j] = False
-primes.pop(0)
+primes = list(dp.keys())
 while True:
     n = int(sys.stdin.readline())
     if n == 0:
@@ -20,8 +22,8 @@ while True:
     for p in primes:
         if p > n // 2:
             break
-        if prime[n-p]:
-            print(f'{n} = {p} + {n-p}')
+        if n-p in dp:
+            print(str(n) + " = " + str(p) + " + " + str(n-p))
             flg = True
             break
 
