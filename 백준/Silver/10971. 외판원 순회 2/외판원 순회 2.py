81065552b5a159d923a7729813a38ff64f936c61
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-10-28 02:27:26 +0900

    [Silver II] Title: 외판원 순회 2, Time: 580 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,29 +1,35 @@
 import sys
-from itertools import permutations
+for test_case in range(1):
+    n = int(sys.stdin.readline())
 
-def solution():
-    for test_case in range(1):
-        n = int(sys.stdin.readline())
+    arr = []
+    for _ in range(n):
+        arr.append(list(map(int, sys.stdin.readline().split())))
 
-        arr = []
-        for _ in range(n):
-            arr.append(list(map(int, sys.stdin.readline().split())))
+    ans = float("inf")
+    def backtracking(start, total, city):
+        global ans
 
-        node = [i for i in range(n)]
+        visited[start] = True
 
-        cases = list(permutations(node, n))
-        ans = float("inf")
-        for case in cases:
-            tmp = list(case) + [case[0]]
-            total = 0
-            flg = False
-            for i in range(len(tmp)-1):
-                start, end =  tmp[i], tmp[i+1]
-                if arr[start][end] == 0:
-                    flg = True
-                    break
-                total += arr[start][end]
-            if not flg:
+        if city == n-1:
+            if arr[start][sta] != 0:
+                total += arr[start][sta]
                 ans = min(ans, total)
+            return
+        for i in range(n):
+            if visited[i] or arr[start][i] == 0:
+                continue
+
+            visited[i] = True
+            backtracking(i, total + arr[start][i], city + 1)
+            visited[i] = False
+
+    visited = [False for i in range(n)]
+
+    for i in range(n):
+        sta = i
+        backtracking(i, 0, 0)
+
+
     print(ans)
-solution()
