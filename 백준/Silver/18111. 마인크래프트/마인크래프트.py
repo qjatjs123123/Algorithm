5fda5e96e5d43595160534897512ea416ca82888
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-10-28 19:14:10 +0900

    [Silver II] Title: 마인크래프트, Time: 144 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,34 +1,36 @@
 import sys
 
-def solution():
-    for test_case in range(1):
-        n, m, b = map(int, sys.stdin.readline().split())
-        graph = []
-        start = 0
-        end = int(6.4 * 10**7)
-    
-        for _ in range(n):
-            graph.append(list(map(int, sys.stdin.readline().split())))
-    
-        ans = []
-        for earthHeight in range(256, -1, -1):
-            inventory = b
-            needs = 0
-            time = 0
-            for row in range(n):
-                for col in range(m):
-                    if graph[row][col] >= earthHeight:
-                        inventory += graph[row][col]-earthHeight
-                        time += 2*(graph[row][col]-earthHeight)
-                    else:
-                        needs += earthHeight - graph[row][col]
-                        time += earthHeight - graph[row][col]
-    
-            if inventory >= needs:
-                ans.append([time, earthHeight])
-    
-        ans.sort(key=lambda x:(x[0], -x[1]))
-    
-        print(' '.join(map(str,ans[0])))
+for test_case in range(1):
+    n, m, b = map(int, sys.stdin.readline().split())
+    graph = []
+    dp = {}
 
-solution()
+    for _ in range(n):
+        tmp = list(map(int, sys.stdin.readline().split()))
+        for num in tmp:
+            if num not in dp:
+                dp[num] = 1
+            else:
+                dp[num] += 1
+
+
+    ans = []
+    for earthHeight in range(257):
+        inventory = b
+        needs = 0
+        time = 0
+
+        for height in list(dp.keys()):
+            cnt = dp[height]
+            if height >= earthHeight:
+                inventory += (height - earthHeight)*cnt
+                time += 2*(height - earthHeight)*cnt
+            else:
+                needs += (earthHeight - height)*cnt
+                time += (earthHeight - height)*cnt
+        if inventory >= needs:
+            ans.append([time, earthHeight])
+
+    ans.sort(key=lambda x:(x[0], -x[1]))
+
+    print(' '.join(map(str,ans[0])))
