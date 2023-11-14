46a7bdd4bb6b81d0a16105d688474dd968f56c84
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-11-14 23:51:21 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,25 @@
+import sys
+for test_case in range(1):
+    n, m = map(int, sys.stdin.readline().split())
+
+    tmp = []
+    ans = []
+    def backtracking(l):
+        if l == m:
+            ans.append(' '.join(map(str,tmp)))
+            return
+        for i in range(1, n + 1):
+            if not tmp:
+                tmp.append(i)
+                backtracking(l+1)
+                tmp.pop()
+                continue
+
+            if  tmp[-1] <= i:
+                tmp.append(i)
+                backtracking(l + 1)
+                tmp.pop()
+
+    backtracking(0)
+    for a in ans:
+        print(a)
