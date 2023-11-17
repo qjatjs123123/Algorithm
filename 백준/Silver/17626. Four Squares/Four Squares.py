d8d4bcf57d8e60939c050c8182760f20f31dfa88
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-11-17 18:41:50 +0900

    [Silver III] Title: Four Squares, Time: 144 ms, Memory: 113064 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -4,17 +4,19 @@ def solution():
     for test_case in range(1):
         n = int(sys.stdin.readline())
 
-        dp = [float("inf") for i in range(n + 1)]
-        dp[0], dp[1] = 0, 1
+        dp = [0, 1]
 
         for num in range(2, n + 1):
+            target=4
             if math.sqrt(num) % 1 == 0:
-                dp[num] = 1
+                target = 1
+                dp.append(target)
                 continue
-            for i in range(1, int(math.sqrt(n)) + 1):     
-                dp[num] = min(dp[num], 1 + dp[num - i**2])
-                if dp[num] == 2:
+            for i in range(1, int(math.sqrt(num)) + 1):
+                target = min(target, 1 + dp[num - i**2])
+                if target == 2:
                     break
+            dp.append(target)
 
         print(dp[n])
 
