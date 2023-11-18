360d6e77c551391b64f461dad649a39b44ad13b9
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-11-18 19:29:29 +0900

    [level 2] Title: 주식가격, Time: 28.19 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#qna) 
 
 ### 성능 요약
 
-메모리: 17.1 MB, 시간: 98.06 ms
+메모리: 17.1 MB, 시간: 28.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 18:56:10
+2023년 11월 6일 19:29:27
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,27 +1,28 @@
-from heapq import heappush, heappop
 def solution(prices):
     answer = [0 for i in range(len(prices))]
-    q = []
+    stack = []
     for i in range(len(prices)):
-        if not q:
-            heappush(q, (-prices[i], i))
+        if not stack:
+            stack.append([prices[i], i])
         else:
-            while True:
-                if not q:
-                    break
-                max_num, max_idx = heappop(q)
-                max_num = -max_num
-                
-                if max_num > prices[i]:
-                    answer[max_idx] = i-max_idx
-                    continue
-                else:
-                    heappush(q, (-max_num, max_idx))
-                    break
-            heappush(q, (-prices[i], i))
-    
-    while q:
-        max_num, max_idx = heappop(q)
-        answer[max_idx] = len(prices)-1-max_idx
+            cur_max, cur_idx = stack[-1][0], stack[-1][1]
+            
+            if cur_max <= prices[i]:
+                stack.append([prices[i], i])
+            else:
+                while True:
+                    if not stack:
+                        break
+                    cur_max, cur_idx = stack[-1][0], stack[-1][1]
                     
+                    if cur_max <= prices[i]:
+                        break
+                    
+                    answer[cur_idx] = i-cur_idx
+                    stack.pop()
+                stack.append([prices[i], i])
+
+    while stack:
+        tmp = stack.pop()
+        answer[tmp[1]] = len(prices) - 1 - tmp[1]
     return answer
