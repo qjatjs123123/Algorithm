70c3951cee151aa30af4b384443439872d8aec5b
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-11-24 22:34:49 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 92.86 ms, Memory: 48.1 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,29 +1,58 @@
-from collections import deque
 
-
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
 def solution(queue1, queue2):
-    queue1 = deque(queue1)
-    queue2 = deque(queue2)
-    q1s =sum(queue1)
-    q2s =sum(queue2)
-    if q1s+q2s % 2 == 1:
-        return -1
-    limit = len(queue1) + len(queue2)
-    count = 0
-    while q1s != q2s:
-        if count >= limit:
-            return -1
-        while queue2 and q1s < q2s:
-            tmp = queue2.popleft()
-            queue1.append(tmp)
-            count += 1
-            q2s -= tmp
-            q1s += tmp
+    small, big = [], []
+    if sum(queue1) > sum(queue2):
+        small, big = queue2.copy(), queue1.copy()
+    else:
+        small, big = queue1.copy(), queue2.copy()
+    if sum(queue1) == sum(queue2):
+        return 0
+    ans = -1
+    sum_small, sum_big = sum(small), sum(big)
+    target = (sum_small + sum_big) / 2
+    dp, total, l = {} , 0, len(small)
+    for i in range(len(small)):
+        total += small[i]
+        dp[(total)] = i + 1
+    total_big = 0
+    dp[0] = 0
+    for i in range(len(big) - 1):
+        total_big += big[i]
+        total += big[i]
+        dp[total] = l + i + 1
+        if sum_big - total_big > target:
+            continue
+        if target - (sum_big - total_big) in dp:
+            ans = i + dp[target - (sum_big - total_big)] + 1
+            break
 
-        while queue1 and q1s > q2s:
-            tmp = queue1.popleft()
-            queue2.append(tmp)
-            count += 1
-            q1s -= tmp
-            q2s += tmp
-    return count
+
+    return ans
