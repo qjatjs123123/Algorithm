abc883348f9924a680cd0649a9df77fa3e981fbf
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-12-14 01:53:49 +0900

    [Gold V] Title: 동전 1, Time: 180 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,18 +1,19 @@
 import sys
+
 for test_case in range(1):
     n, k = map(int, sys.stdin.readline().split())
     coins = []
 
     for _ in range(n):
         coins.append(int(sys.stdin.readline()))
+
     coins.sort()
 
     dp = [0 for i in range(k + 1)]
     dp[0] = 1
 
     for coin in coins:
-        for i in range(1, k + 1):
-            if i - coin >= 0:
-                dp[i]= dp[i-coin] + dp[i]
+        for num in range(coin, k + 1):
+            dp[num] += dp[num - coin]
 
     print(dp[k])
