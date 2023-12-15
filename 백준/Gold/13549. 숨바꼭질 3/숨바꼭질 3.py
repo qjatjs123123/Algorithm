195e98f030c616ada413646ae33827474b3e4349
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-12-15 21:57:31 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 1420 ms, Memory: 73292 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+
+for test_case in range(1):
+    n, k = map(int, sys.stdin.readline().split())
+
+    dp = [float("inf") for i in range(1000001)]
+
+    q = deque()
+
+    q.append((n, 0))
+    dp[n] = 0
+    while q:
+        cur_pos, cur_cnt = q.popleft()
+
+        cases = [(cur_pos - 1, 1), (cur_pos + 1, 1), (cur_pos * 2, 0)]
+        if dp[cur_pos] < cur_cnt:
+            continue
+
+        if cur_pos == k:
+            print(cur_cnt)
+            break
+
+        for new_pos, time in cases:
+            if 0 <= new_pos <= 1000000 and dp[new_pos] > cur_cnt + time:
+                dp[new_pos] = cur_cnt + time
+                q.append((new_pos, cur_cnt + time))
