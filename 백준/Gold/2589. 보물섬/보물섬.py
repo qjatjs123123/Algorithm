58193f7d55f409c646360a5d90a421bfeb75fbe0
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-12-18 17:41:34 +0900

    [Gold V] Title: 보물섬, Time: 4824 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -2,35 +2,37 @@ import sys
 
 from collections import deque
 
+def solution():
+    for test_case in range(1):
+        l, w = map(int, sys.stdin.readline().split())
+        graph = [sys.stdin.readline().rstrip() for _ in range(l)]
 
-for test_case in range(1):
-    l, w = map(int, sys.stdin.readline().split())
-    graph = [sys.stdin.readline().rstrip() for _ in range(l)]
+        def bfs(row, col):
+            q = deque()
 
-    def bfs(row, col):
-        q = deque()
+            direction = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+            visited = [[-1 for _ in range(w)] for _ in range(l)]
+            visited[row][col] = 0
+            q.append((row, col, 0))
 
-        direction = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-        visited = [[-1 for _ in range(w)] for _ in range(l)]
-        visited[row][col] = 0
-        q.append((row, col, 0))
+            while q:
+                cur_row, cur_col, cur_cnt = q.popleft()
 
-        while q:
-            cur_row, cur_col, cur_cnt = q.popleft()
+                for direct in direction:
+                    new_row, new_col = cur_row + direct[0], cur_col + direct[1]
 
-            for direct in direction:
-                new_row, new_col = cur_row + direct[0], cur_col + direct[1]
+                    if 0 <= new_row < l and 0 <= new_col < w and graph[new_row][new_col] == 'L' and visited[new_row][new_col] == -1:
+                        q.append((new_row, new_col, cur_cnt + 1))
+                        visited[new_row][new_col] = cur_cnt + 1
 
-                if 0 <= new_row < l and 0 <= new_col < w and graph[new_row][new_col] == 'L' and visited[new_row][new_col] == -1:
-                    q.append((new_row, new_col, cur_cnt + 1))
-                    visited[new_row][new_col] = cur_cnt + 1
-        return visited[cur_row][cur_col]
+            return visited[cur_row][cur_col]
 
-    ans = 0
-    dp = {}
-    for row in range(l):
-        for col in range(w):
-            if graph[row][col] == 'L':
-                ans = max(ans, bfs(row, col))
+        ans = 0
+        dp = {}
+        for row in range(l):
+            for col in range(w):
+                if graph[row][col] == 'L':
+                    ans = max(ans, bfs(row, col))
 
-    print(ans)
+        print(ans)
+solution()
