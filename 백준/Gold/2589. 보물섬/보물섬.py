a256b61e301681ab9c843d41ceb1ab13ba95599c
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-12-18 17:36:49 +0900

    [Gold V] Title: 보물섬, Time: 1012 ms, Memory: 120204 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -3,7 +3,6 @@ import sys
 from collections import deque
 
 
-
 for test_case in range(1):
     l, w = map(int, sys.stdin.readline().split())
     graph = [sys.stdin.readline().rstrip() for _ in range(l)]
@@ -12,8 +11,8 @@ for test_case in range(1):
         q = deque()
 
         direction = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-        visited = [[False for _ in range(w)] for _ in range(l)]
-        visited[row][col] = True
+        visited = [[-1 for _ in range(w)] for _ in range(l)]
+        visited[row][col] = 0
         q.append((row, col, 0))
         ans = 0
         while q:
@@ -22,13 +21,14 @@ for test_case in range(1):
             for direct in direction:
                 new_row, new_col = cur_row + direct[0], cur_col + direct[1]
 
-                if 0 <= new_row < l and 0 <= new_col < w and graph[new_row][new_col] == 'L' and not visited[new_row][new_col]:
+                if 0 <= new_row < l and 0 <= new_col < w and graph[new_row][new_col] == 'L' and visited[new_row][new_col] == -1:
                     q.append((new_row, new_col, cur_cnt + 1))
-                    visited[new_row][new_col] = True
+                    visited[new_row][new_col] = cur_cnt + 1
                     ans = max(ans, cur_cnt + 1)
-        return ans
+        return visited[cur_row][cur_col]
 
     ans = 0
+    dp = {}
     for row in range(l):
         for col in range(w):
             if graph[row][col] == 'L':
