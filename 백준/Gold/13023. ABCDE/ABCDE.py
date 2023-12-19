cd6191bc784b2d1229263ff2a0c35a63c3d59425
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-12-19 20:01:34 +0900

    [Gold V] Title: ABCDE, Time: 3504 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,35 +1,23 @@
 import sys
 sys.setrecursionlimit(10**6)
 
-
 def solution():
     n, m = map(int, sys.stdin.readline().split())
-    graph = {}
-
+    g = [[] for _ in range(n)]
     for _ in range(m):
         a, b = map(int, sys.stdin.readline().split())
-
-        if a not in graph:
-            graph[a] = [b]
-        else:
-            graph[a] += [b]
-
-        if b not in graph:
-            graph[b] = [a]
-        else:
-            graph[b] += [a]
+        g[a].append(b)
+        g[b].append(a)
 
     ans = False
     visited = [False for _ in range(n + 1)]
 
     def dfs(cur_node, cnt):
         nonlocal visited
-        if cur_node not in graph:
-            return 0
         if cnt == 4:
             return 0
         ans = 0
-        for new_node in graph[cur_node]:
+        for new_node in g[cur_node]:
             if not visited[new_node]:
 
                 visited[new_node] = True
@@ -39,7 +27,6 @@ def solution():
         return ans
     for i in range(n):
         visited[i] = True
-
         if dfs(i, 0) == 4:
             ans = True
             break
