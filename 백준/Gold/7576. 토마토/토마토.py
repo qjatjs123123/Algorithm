9c6545fc25993905dfcf3e1c9c2251171d016977
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-12-26 13:26:49 +0900

    [Gold V] Title: 토마토, Time: 996 ms, Memory: 98632 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,21 +1,22 @@
 import sys
-
 from collections import deque
-for test_case in range(1):
-    m, n = map(int, sys.stdin.readline().split())
-    graph = []
 
+
+def solution():
+    m, n = map(int, sys.stdin.readline().split())
+
+    graph = []
     for _ in range(n):
         graph.append(list(map(int, sys.stdin.readline().split())))
 
     q = deque()
-
     for row in range(n):
         for col in range(m):
             if graph[row][col] == 1:
-                q.append((row, col, 0))
+                q.append((row, col, 1))
 
     direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    ans = 0
     while q:
         cur_row, cur_col, cur_cnt = q.popleft()
 
@@ -25,19 +26,17 @@ for test_case in range(1):
             if 0 <= new_row < n and 0 <= new_col < m and graph[new_row][new_col] == 0:
                 graph[new_row][new_col] = cur_cnt + 1
                 q.append((new_row, new_col, cur_cnt + 1))
-
-    ans = 0
-
+                ans = cur_cnt
+    flg = False
     for row in range(n):
-        flg = False
         for col in range(m):
             if graph[row][col] == 0:
                 flg = True
-                ans = -1
                 break
-            ans = max(ans, graph[row][col])
         if flg:
             break
-    if ans == 1:
-        ans = 0
-    print(ans)
+    if flg:
+        print(-1)
+    else:
+        print(ans)
+solution()
