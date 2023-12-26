f76fae13ed5aed1f2bf0c8b51b670efefe041b07
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-12-26 15:13:30 +0900

    [Gold V] Title: 평범한 배낭, Time: 2916 ms, Memory: 229084 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,21 +1,19 @@
 import sys
 
-for test_case in range(1):
+def solution():
     n, k = map(int, sys.stdin.readline().split())
 
-    arr = []
-    for _ in range(n):
-        arr.append(list(map(int, sys.stdin.readline().split())))
-
-    dp = [[0 for i in range(k + 1)] for j in range(n + 1)]
+    dp = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
+    graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
     for i in range(1, n + 1):
+        w, v = graph[i-1]
         for j in range(1, k + 1):
-            cur_weight, cur_value = arr[i-1][0], arr[i-1][1]
-
-            if cur_weight <= j:
-                dp[i][j] = max(dp[i-1][j - cur_weight] + cur_value, dp[i-1][j])
+            if j < w:
+                dp[i][j] = dp[i-1][j]
             else:
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
 
     print(dp[n][k])
+
+solution()
