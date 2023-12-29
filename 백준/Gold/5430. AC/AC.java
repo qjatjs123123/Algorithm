df837dc3d4307a15e373b6770012739ea7af9388
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2023-12-29 19:02:27 +0900

    [Gold V] Title: AC, Time: 840 ms, Memory: 79952 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String args[]) throws Exception
+	{
+//		BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		
+		int tc = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < tc; i++) {
+			char[] cmd = br.readLine().toCharArray();
+			int n = Integer.parseInt(br.readLine());
+			String tmp = br.readLine().replace("[", "").replace("]", "");	
+			String[] arr = tmp.split(",");
+			
+			Deque<String> q = new ArrayDeque<>(Arrays.asList(arr));
+			boolean isReverse = false;
+			boolean flg = true;
+			if(tmp.equals("")) q.remove();
+			
+			for (char command: cmd) {
+				if (command == 'R') {
+					isReverse = !isReverse;
+					continue;
+				}
+				if (q.size() == 0) {
+					flg = false;
+					break;
+				}
+				if(!isReverse) {
+					q.removeFirst();
+				}else {
+					q.removeLast();
+				}
+				
+			}
+			
+			if (!flg) System.out.println("error");
+			else {
+				if (isReverse) {
+					ArrayList<String> array = new ArrayList<>(q);
+					Collections.reverse(array);
+					System.out.println("["+String.join(",", array)+"]");
+				}else {
+					System.out.println("["+String.join(",", q)+"]");	
+				}
+				
+			}
+			
+			
+		}
+    }
+
+}
