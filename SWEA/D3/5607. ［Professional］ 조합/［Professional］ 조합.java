b00b6fa53066fddff038df6cd591d61f80e390f4
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2024-04-01 21:32:27 +0900

    [D3] Title: [Professional] 조합, Time: 201 ms, Memory: 28,248 KB -BaekjoonHub

--- SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
+++ SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
@@ -1,46 +1,54 @@
-import java.util.*;
-class Solution
-{
-    static final int MOD = 1234567891;
-    static long factorial[] = new long[1000001];
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-        StringBuffer sb = new StringBuffer();
-        init();
-
-        int T = sc.nextInt();
-        for(int tc=1; tc<=T; tc++){
-            int N = sc.nextInt();
-            int K = sc.nextInt();
-
-            long c1 = factorial[N];
-            long c2 = (factorial[N-K] * factorial[K]) % MOD;
-            long c3 = calculatePow(c2, MOD-2);
-
-            sb.append("#").append(tc).append(" ");
-            sb.append(c1 * c3 % MOD).append("\n");
-        }
-        System.out.println(sb);
-    }
-    //분할정복으로 구하면 O(logN)만에 구할수 있다.
-    private static long calculatePow(long n, long k) {
-        if(k == 1){
-            return n;
-        }
-        long x = calculatePow(n, k/2) % MOD;
-        if(k % 2 == 0){
-            return x * x % MOD;
-        }else{
-            return ((x * x) % MOD * n) % MOD;
-        }
-    }
-
-    private static void init() {
-        factorial[0] = 1;
-        for(int i=1; i<=1000000; i++){
-            factorial[i] = factorial[i-1] * i % MOD;
+public class Solution {
+    private static final int MOD = 1234567891;
+    static int T;
+    static int N, R;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        T = Integer.parseInt(st.nextToken());
+        
+        for (int t = 1; t <= T; t++) {
+        	st = new StringTokenizer(br.readLine());
+        	int N, R;
+        	N = Integer.parseInt(st.nextToken());
+        	R = Integer.parseInt(st.nextToken());
+        	
+        	long parent = 1;
+        	for (int i = N; i > N-R; i--) {
+        		parent *= i;
+        		parent %= MOD;
+        	}
+        	
+        	long child = 1;
+        	
+        	for (int i = 1; i <= R; i++) {
+        		child *= i;
+        		child %= MOD;
+        	}
+        	
+        	long c = backtracking(child, MOD-2);
+        	long a = (parent * c) % MOD ;
+        	
+        	System.out.println("#" + t + " " + a );
         }
     }
+    
+    static long backtracking(long num, int p) {
+    	if (p == 1) return num;
+    	long n = backtracking(num, p / 2) % MOD;
+    	if (p % 2 == 0) {
+    		return (n * n) % MOD;
+    	}
+    	else {
+    		return ( (n * n) % MOD * num) % MOD;
+    	}
+    }
+    
 }
