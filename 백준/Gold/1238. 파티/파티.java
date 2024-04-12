6baecf31f750fcf135988e9942b3174badbf0c47
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2024-04-13 01:45:41 +0900

    [Gold III] Title: 파티, Time: 928 ms, Memory: 135604 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, X;
+    static HashMap<Integer, ArrayList<int[]>> graph = new HashMap<>();
+    static int[] distance;
+    static boolean[] visited;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    	BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+    	N = Integer.parseInt(st.nextToken());
+    	M = Integer.parseInt(st.nextToken());
+    	X = Integer.parseInt(st.nextToken());
+    	
+    	for (int i = 0; i < M; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		
+    		int from = Integer.parseInt(st.nextToken());
+    		int to = Integer.parseInt(st.nextToken());
+    		int dist = Integer.parseInt(st.nextToken());
+    		
+    		if (graph.containsKey(from)) {
+    			ArrayList<int[]> tmp = graph.get(from);
+    			tmp.add(new int[] {to, dist});
+    			graph.put(from, tmp);
+    		} else {
+    			ArrayList<int[]> tmp = new ArrayList<>();
+    			tmp.add(new int[] {to, dist});
+    			graph.put(from, tmp);
+    		}
+    	}
+    	int ans = 0;
+    	for (int i = 1; i <= N; i++) {
+    		int total = dijkstra(i, X) + dijkstra(X, i);
+    		ans = Math.max(ans, total);
+    	}
+    	
+    	System.out.println(ans);
+    }  
+    
+    static int dijkstra(int start, int end) {
+    	
+    	PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0]-b[0]);
+    	pq.add(new int[] {0, start});
+    	
+    	distance = new int[N + 1];
+    	visited = new boolean[N + 1];
+    	for (int i = 0; i <= N; i++) distance[i] = Integer.MAX_VALUE;	
+    	distance[start] = 0;
+    	
+    	while (!pq.isEmpty()) {
+    		int[] arr = pq.poll();
+    		int cur_dist = arr[0];
+    		int cur_node = arr[1];
+    		
+    		
+    		if (distance[cur_node] < cur_dist) continue;
+    		
+    		ArrayList<int[]> list = graph.get(cur_node);
+    		
+    		for (int[] tmp : list) {
+    			int new_node = tmp[0];
+    			int dist = tmp[1];
+
+    			
+    			if (distance[new_node] > cur_dist + dist) {
+    				distance[new_node] = cur_dist + dist;
+    				pq.add(new int[] {cur_dist + dist, new_node});
+    			}
+    		}
+
+    	}
+    	
+    	return distance[end];
+    }
+}
