56967f88ac848873e1b22bda02333293a046b9f9
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2024-04-13 22:15:38 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 940 ms, Memory: 88000 KB -BaekjoonHub

---
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static HashMap<Integer, ArrayList<int[]>> graph = new HashMap<>();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    	BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        for (int i = 0; i < M; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	int from = Integer.parseInt(st.nextToken());
+        	int to = Integer.parseInt(st.nextToken());
+        	int dist = Integer.parseInt(st.nextToken());
+        	
+        	if (graph.containsKey(from)) {
+        		ArrayList<int[]> tmp = graph.get(from);
+        		tmp.add(new int[] {to, dist});
+        		graph.put(from, tmp);
+        	} else {
+        		ArrayList<int[]> tmp = new ArrayList<>();
+        		tmp.add(new int[] {to, dist});
+        		graph.put(from, tmp);
+        	}
+        	
+        	if (graph.containsKey(to)) {
+        		ArrayList<int[]> tmp = graph.get(to);
+        		tmp.add(new int[] {from, dist});
+        		graph.put(to, tmp);
+        	} else {
+        		ArrayList<int[]> tmp = new ArrayList<>();
+        		tmp.add(new int[] {from, dist});
+        		graph.put(to, tmp);
+        	}
+        }
+        
+        st = new StringTokenizer(br.readLine());
+        int from = Integer.parseInt(st.nextToken());
+        int to = Integer.parseInt(st.nextToken());
+        
+        int one = dijkstra(1, from);
+        int two = dijkstra(from, to);
+        int three = dijkstra(to, N);
+        int case1 = one + two + three;
+        
+        if (one == Integer.MAX_VALUE || two == Integer.MAX_VALUE || three == Integer.MAX_VALUE) case1 = Integer.MAX_VALUE;
+        one = dijkstra(1, to);
+        two = dijkstra(to, from);
+        three = dijkstra(from, N);
+        int case2 = one + two + three;
+        if (one == Integer.MAX_VALUE || two == Integer.MAX_VALUE || three == Integer.MAX_VALUE) case2 = Integer.MAX_VALUE;
+  
+        int ans = Math.min(case1, case2);
+        if (ans == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(Math.min(case1, case2));
+
+    }  
+    
+    static int dijkstra(int from, int to) {
+    	int[] distance = new int[N + 1];
+    	
+    	for (int i = 0; i <= N; i++) distance[i] = Integer.MAX_VALUE;
+    	distance[from] = 0;
+    	
+    	PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+    	pq.add(new int[] {0, from});
+    	
+    	while (!pq.isEmpty()) {
+    		int[] tmp = pq.poll();
+    		int cur_dist = tmp[0];
+    		int cur_node = tmp[1];
+    		
+    		if (distance[cur_node] < cur_dist) continue;
+    		if (!graph.containsKey(cur_node)) continue;
+    		ArrayList<int[]> list = graph.get(cur_node);
+    		for (int[] arr : list) {
+    			int new_node = arr[0];
+    			int new_dist = cur_dist + arr[1];
+    			
+    			if (distance[new_node] > new_dist) {
+    				distance[new_node] = new_dist;
+    				pq.add(new int[] {new_dist, new_node});
+    			}
+    		}
+    	}
+
+    	return distance[to];
+    }
+}
+
+
