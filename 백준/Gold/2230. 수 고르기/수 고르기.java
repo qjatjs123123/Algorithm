94dc4a2e1c515bb0cc2193f056d5758e4be88276
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2024-06-27 00:21:18 +0900

    [Gold V] Title: 수 고르기, Time: 428 ms, Memory: 32172 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,49 +1,44 @@
 import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String[] args) throws IOException
-    {
-//      BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N, M;
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	arr[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        Arrays.sort(arr);
-        
-        int left = 0;
-        long total = Long.MAX_VALUE;
-        
-        for (int right = 0; right < N; right++) {
-        	int diff = arr[right] - arr[left];
-        	
-        	if (diff == M) {
-        		total = M;
-        		break;
-        	}
-        	if (diff > M) {
-        		total = Math.min(total, diff);
-        		while (true) {
-        			left++;
-        			diff = arr[right] - arr[left];
-        			if (diff < M) break;
-        			total = Math.min(total, diff);
-        		}
-        	}
-
-        }
-        
-        System.out.println(total);
-    }
-    
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N, M;
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int left = 0;
+		int right = 0;
+		int ans = Integer.MAX_VALUE;
+		
+		while (right < N && left <= right) {
+			int gap = arr[right] - arr[left];
+			
+			if (gap >= M) {
+				ans = Math.min(ans, gap);
+				left++;
+			} else {
+				right++;
+			}
+		}
+		
+		System.out.println(ans);
+	}
 }
