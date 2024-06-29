e14f0973dc99d0ab2ef13e727e1f834765590e8c
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2024-06-29 21:53:31 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 564 ms, Memory: 65184 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -36,7 +36,8 @@ public class Main {
 		int end = Integer.parseInt(st.nextToken());
 		
 		int[] distance = new int[cityCnt + 1];
-		ArrayList<Integer> track = new ArrayList<>();
+		int[] track = new int[cityCnt + 1];
+
 		
 		
 		for (int i = 0; i <= cityCnt; i++) {
@@ -45,17 +46,15 @@ public class Main {
 		distance[start] = 0;
 		
 		PriorityQueue<Pos> pq = new PriorityQueue<>();
-		ArrayList<Integer> init_track = new ArrayList<>();
-		init_track.add(start);
 		Pos init_pos = new Pos(start, 0);
-		init_pos.track = init_track;
+
 		pq.add(init_pos);
 		
 		while (!pq.isEmpty()) {
 			Pos cur_pos = pq.poll();
 			int cur_node = cur_pos.node;
 			int cur_dist = cur_pos.dist;
-			ArrayList<Integer> cur_track = cur_pos.track;
+
 			
 			if (distance[cur_node] < cur_dist) continue;
 			if (!graph.containsKey(cur_node)) continue;
@@ -69,14 +68,10 @@ public class Main {
 					
 					ArrayList<Integer> new_track = new ArrayList<>();
 					
-					for (int num : cur_track) new_track.add(num);
-					new_track.add(pos.node);
 					Pos new_pos = new Pos(pos.node, cur_dist + pos.dist);
-					new_pos.track = new_track;
-					
+
 					pq.add(new_pos);
-					if (pos.node == end)
-						track = new_track;
+					track[pos.node] = cur_node;
 					
 				}
 			}
@@ -84,10 +79,22 @@ public class Main {
 		
 		
 		System.out.println(distance[end]);
-		System.out.println(track.size());
+
+		
+		Stack<Integer> stack = new Stack<>();
 		StringBuilder sb = new StringBuilder();
 		
-		for(int num : track) sb.append(num).append(" ");
+		int cur_node = end;
+		int cnt = 1;
+		while (cur_node != start) {
+			stack.push(cur_node);
+			cnt++;
+			cur_node = track[cur_node];
+		}
+		stack.push(start);
+		
+		while (!stack.isEmpty()) sb.append(stack.pop()).append(" ");
+		System.out.println(cnt);
 		System.out.println(sb.toString());
 	}
 	
