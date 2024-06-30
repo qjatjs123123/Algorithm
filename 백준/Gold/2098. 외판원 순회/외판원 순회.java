62d546a95d3306502a8918752bd02a82d824dcd4
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2024-06-30 15:06:32 +0900

    [Gold I] Title: 외판원 순회, Time: 168 ms, Memory: 19416 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -1,32 +1,33 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-	static int N;
-	static int[][] graph;
 	static int[][] dp;
-	static int MASK;
-	static final int MAX_NUM = 100_000_000;
+	static int[][] graph;
+	static int MAX_NUM;
+	static int visited;
+	static int N;
 	
 	public static void main(String[] args) throws IOException {
-//		BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
+		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());	
 		
 		N = Integer.parseInt(st.nextToken());
+		visited = (int)Math.pow(2, N) - 1;
+		dp = new int[N][visited + 1];
 		graph = new int[N][N];
-		MASK = (int)Math.pow(2, N) - 2;
-		dp = new int[N][MASK + 1];
+		MAX_NUM = 100_000_000;
 		
 		for (int row = 0; row < N; row++) {
-			for (int col = 0; col <= MASK; col++ ) {
+			for (int col = 0; col <= visited; col++) {
 				dp[row][col] = Integer.MAX_VALUE;
 			}
 		}
 		
 		for (int row = 0; row < N; row++) {
-			st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());	
 			for (int col = 0; col < N; col++) {
 				graph[row][col] = Integer.parseInt(st.nextToken());
 			}
@@ -35,26 +36,25 @@ public class Main {
 		System.out.println(recursion(0, 0));
 	}
 	
-	static int recursion(int cur_node, int bitmask) {
-		
-		if (dp[cur_node][bitmask] != Integer.MAX_VALUE) return dp[cur_node][bitmask];
-		if ((bitmask & MASK) == MASK) {
+	static int recursion(int node, int bitmask) {
+		 if (dp[node][bitmask] != Integer.MAX_VALUE) return dp[node][bitmask];
+		 
+		 if (bitmask == visited - 1) {
+			 if (graph[node][0] == 0) return MAX_NUM;
+			 return graph[node][0];
+		 }
+		 
+		 int max_num = MAX_NUM;
+		 for (int col = 1; col < N; col++) {
+			 if (graph[node][col] == 0) continue;
+			 if ((bitmask & (1 << col)) > 0) continue;
+			 
+			 int new_bitmask = bitmask | (1 << col);
+			 max_num = Math.min(max_num, recursion(col, new_bitmask) + graph[node][col]);
 			
-			if (graph[cur_node][0] != 0) return graph[cur_node][0];
-			return MAX_NUM;
-		}
-		
-		int max_num = MAX_NUM;
-		for (int new_node = 1; new_node < N; new_node++) {
-			if (graph[cur_node][new_node] == 0) continue;
-			if ( (bitmask & (1 << new_node)) > 0) continue;
-			
-			int new_bitmask = bitmask | (1 << new_node);
-			
-			max_num = Math.min(max_num, recursion(new_node, new_bitmask) + graph[cur_node][new_node]);
-		}
-		
-		dp[cur_node][bitmask] = max_num;
-		return max_num;
+		 }
+		 
+		 dp[node][bitmask] = max_num;
+		 return max_num;
 	}
 }
