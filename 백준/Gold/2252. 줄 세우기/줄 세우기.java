df00cd9ce79cda0316f8cd89888bf168e12b19f5
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2024-07-01 00:50:29 +0900

    [Gold III] Title: 줄 세우기, Time: 496 ms, Memory: 52136 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,69 +1,66 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-	private static StringBuilder sb = new StringBuilder();
-	static int n, m;
 	
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws IOException {
 		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		int[] arr = new int[n+1];
-		boolean[] visited = new boolean[n+1];
-		arr[0] = 1;
-		ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 		
-		for (int i = 0; i < n + 1; i++) {
-			ArrayList<Integer> tmp = new ArrayList<>();
-			graph.add(tmp);
-		}
+		StringTokenizer st = new StringTokenizer(br.readLine());	
 		
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
+		int N, M;
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		int[] count = new int[N + 1];
+		HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());	
+			
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
-			graph.get(start).add(end);
-			arr[end] += 1;
-
+			
+			count[end]++;
+			
+			if (graph.containsKey(start)) {
+				ArrayList<Integer> list = graph.get(start);
+				list.add(end);
+				graph.put(start, list);	
+			}
+			else {
+				ArrayList<Integer> list = new ArrayList<>();
+				list.add(end);
+				graph.put(start, list);	
+			}
 		}
 		
 		Deque<Integer> deque = new LinkedList<>();
 		
-		for (int i = 1; i <= n; i++) {
-			if (arr[i] == 0) {
-				visited[i] = true;
-				deque.addLast(i);
-			}
+		for (int i = 1; i < N + 1; i++) {
+			if (count[i] == 0) deque.add(i);
 		}
-		while (!deque.isEmpty()) {
-			int cur_node = deque.pollFirst();
+		StringBuilder sb = new StringBuilder();
+		
+		while(!deque.isEmpty()) {
+			int cur_num = deque.pollFirst();
 			
-			sb.append(cur_node + " ");
-			visited[cur_node] = true;
-			
-			ArrayList<Integer> new_node_list = graph.get(cur_node);
-			
-			for (int i = 0 ; i < new_node_list.size(); i++) {
-				int new_node = new_node_list.get(i);
-				if (new_node >= 1) arr[new_node]--;
-				if (arr[new_node] == 0) {
-					deque.addLast(new_node);
-					new_node_list.remove(i);
-					i--;
-				}
+			sb.append(cur_num);
+			sb.append(" ");
+			if (!graph.containsKey(cur_num)) continue;
+			ArrayList<Integer> list = graph.get(cur_num);
+			for(int num: list) {
+				count[num]--;
+				if (count[num] == 0) deque.add(num);
 			}
-
 		}
 		
-		for (int i = 1; i <= n; i++) {
-			if (!visited[i]) sb.append(i + " ");
+		for (int i = 1; i < N + 1; i++) {
+			if (count[i] != 0) sb.append(i);
 		}
 		
 		System.out.println(sb.toString());
-	}	
-	
+	}
+
 }
