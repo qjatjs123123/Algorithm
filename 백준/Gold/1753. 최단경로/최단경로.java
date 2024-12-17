55b177c58267f6b92ff95480d7fe6088ce2f46ab
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2024-12-17 14:52:04 +0900

    [Gold IV] Title: 최단경로, Time: 668 ms, Memory: 107788 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,82 +1,93 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+class Main {
+    static int V, E;
+    static int K;
+    static ArrayList<ArrayList<Pos>> graph = new ArrayList<>();
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-public class Main {
-	private static StringBuilder sb = new StringBuilder();
-	static int V, E;
-	static int K;
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
 
-	static int[] distance;
-	static boolean[] visited;
-	static ArrayList<ArrayList<int[]>> graph;
-	
-	public static void main(String[] args) throws Exception{
-		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		V = Integer.parseInt(st.nextToken());
-		E  = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		K = Integer.parseInt(st.nextToken());
-		graph = new ArrayList<>();
-		
-		for (int i = 0; i <= V; i++) {
-			graph.add(new ArrayList<>());
-		}
-		
-		
-		for (int row = 0; row < E; row++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			
-			graph.get(start).add(new int[] {cost, end});
-		}
-		
-		
-		distance = new int[V + 1];
-		visited = new boolean[V + 1];
-		
-		for (int i = 0; i <= V; i++) distance[i] = Integer.MAX_VALUE;
-		
-		dijkstra();
-		for (int i = 1; i<= V; i++) {
-			if (distance[i] == Integer.MAX_VALUE) {
-				sb.append("INF").append("\n");
-			}
-			else sb.append(distance[i]).append("\n");
-		}
-		
-		System.out.println(sb.toString());
-	}	
-	
-	static void dijkstra() {
-		PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
-		pq.add(new int[] {0, K});
-		distance[K] = 0;
-		
-		while (!pq.isEmpty()) {
-			int[] tmp = pq.poll();
-			int cost = tmp[0];
-			int node = tmp[1];
-			
-			if (distance[node] < cost || visited[node]) continue;
-			visited[node] = true;
-			ArrayList<int[]> list = graph.get(node);
-			
-			for (int[] arr : list) {
-				if (visited[arr[1]]) continue;
-				
-				if (distance[arr[1]] > cost + arr[0]) {
-					distance[arr[1]] = cost + arr[0];
-					pq.add(new int[] {distance[arr[1]], arr[1]});
-				}
-			}
-			
-		}
-	}
+        st = new StringTokenizer(br.readLine());
+        K = Integer.parseInt(st.nextToken());
+
+        for (int row = 0; row <= V; row++) {
+            ArrayList<Pos> new_list = new ArrayList<>();
+            graph.add(new_list);
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            ArrayList<Pos> cur_list = graph.get(from);
+            cur_list.add(new Pos(to, weight));
+
+            
+        }
+        
+        PriorityQueue<Pos> pq = new PriorityQueue<>();
+
+        int[] distance = new int[V + 1];
+        boolean[] visited = new boolean[V + 1];
+        for (int i = 0; i <= V; i++) distance[i] = Integer.MAX_VALUE;
+        distance[K] = 0;
+        pq.add(new Pos(K, 0));
+
+        while(!pq.isEmpty()) {
+            Pos cur_pos = pq.poll();    
+
+            int cur_node = cur_pos.v;
+            int cur_weight = cur_pos.w;
+            ArrayList<Pos> new_graph = graph.get(cur_node);
+
+            if (new_graph.size() == 0) continue;
+            if (cur_weight > distance[cur_node]) continue;
+
+            for (Pos new_pos : new_graph) {
+                int new_node = new_pos.v;
+                int new_weight = new_pos.w;
+
+                if (distance[new_node] > cur_weight + new_weight) {
+                    distance[new_node] = cur_weight + new_weight;
+                    pq.add(new Pos(new_node, cur_weight + new_weight));
+                }
+            }
+            
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int col = 1; col <= V; col++) {
+            if (distance[col] == Integer.MAX_VALUE) sb.append("INF");
+            else sb.append(distance[col]);
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    static class Pos implements Comparable<Pos> {
+        int v, w;
+
+        Pos(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Pos pos) {
+            return Integer.compare(this.w, pos.w);
+        }
+    }
 }
