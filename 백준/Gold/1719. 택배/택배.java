c7ce2d3132bb2dcabdcab243fba0addb4be835cb
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2024-12-17 12:29:29 +0900

    [Gold III] Title: 택배, Time: 332 ms, Memory: 21608 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.java
+++ 백준/Gold/1719. 택배/택배.java
@@ -2,12 +2,10 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-// The main method must be in a class named "Main".
 class Main {
     static int n, m;
-    static int EXP = 1000 * 1000;
     static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    static int[][] time;
+    static int[][] weight;
     static int[][] ans;
     
     public static void main(String[] args) throws IOException{
@@ -16,18 +14,20 @@ class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        time = new int[n + 1][n + 1];
+        weight = new int[n + 1][n + 1];
         ans = new int[n + 1][n + 1];
 
-        for (int i = 0; i <= n; i++) {
+        for (int row = 0; row <= n; row++) {
             ArrayList<Integer> new_list = new ArrayList<>();
             graph.add(new_list);
         }
 
-        for (int row = 0; row <= n; row++) {
-            for (int col = 0; col <= n; col++) {
-                if (row == col) time[row][col] = 0;
-                else time[row][col] = EXP;
+        for (int row = 0 ; row <= n; row++) {
+            for(int col = 0; col <= n; col++) {
+                if (row == col) weight[row][col] = 0;
+                else {
+                    if (weight[row][col] == 0) weight[row][col] = 1000*1000;
+                }
             }
         }
         
@@ -35,37 +35,40 @@ class Main {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            time[from][to] = Math.min(weight, time[from][to]);
-            time[to][from] = Math.min(weight, time[to][from]);
-            ans[from][to] = to;
-            ans[to][from] = from;
-            
+            int w = Integer.parseInt(st.nextToken());
+
             ArrayList<Integer> cur_list = graph.get(from);
             cur_list.add(to);
             cur_list = graph.get(to);
             cur_list.add(from);
+
+            weight[from][to] = Math.min(weight[from][to], w);
+            weight[to][from] = Math.min(weight[to][from], w);
+            ans[from][to] = to;
+            ans[to][from] = from;
         }
 
         for (int mid = 1; mid <= n; mid++) {
-            for(int from = 1; from <= n; from++) {
+            for (int from = 1; from <= n; from++) {
                 for (int to = 1; to <= n; to++) {
-                    if (time[from][to] > time[from][mid] + time[mid][to]) {
-                        time[from][to] = time[from][mid] + time[mid][to];
+                    if (weight[from][to] > weight[from][mid] + weight[mid][to]) {
+                        weight[from][to] = weight[from][mid] + weight[mid][to];
                         ans[from][to] = ans[from][mid];
                     }
                     
                 }
             }
+        } 
+        StringBuffer sb = new StringBuffer();
+        for (int row = 1; row <= n; row++) {
+            for (int  col = 1; col <= n; col++) {
+                if (ans[row][col] == 0) sb.append("-");
+                else sb.append(ans[row][col]);
+                sb.append(" ");
+            }
+            sb.append("\n");
         }
 
-        for (int row = 1; row <= n; row++) {
- 
-            for(int col = 1; col <= n; col++) {
-                if (ans[row][col] == 0) System.out.print("- ");
-                else System.out.print(ans[row][col] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println(sb.toString());
     }
 }
