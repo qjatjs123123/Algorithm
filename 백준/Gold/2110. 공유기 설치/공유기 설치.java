b3767cf30c91132f9c3591e030552ae1d9b7a03a
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-01-06 18:27:58 +0900

    [Gold IV] Title: 공유기 설치, Time: 336 ms, Memory: 38824 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,71 +1,64 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N, C;
-		
-		N = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		ArrayList<Integer> house = new ArrayList<>();
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			
-			house.add(Integer.parseInt(st.nextToken()));
-		}
-		
-		Collections.sort(house);
-		
-		int start = 1;
-		int end = 1_000_000_000;
-		
-		while (start <= end) {
-			int mid = (start + end) / 2;
-			
-			int count = 1;
-			int cur_house = house.get(0);
-			int cur_idx = 0;
-			// mid 범위 계산
-			while (true) {
-				int target = cur_house + mid;
-				
-				int new_idx = upperBound(target, house);
-				
-				if (new_idx == house.size()) break;
-				count++;
-				cur_idx = new_idx;
-				cur_house = house.get(new_idx);
-			}
-			
-			if (count < C) end = mid - 1;
-			else start = mid + 1;
-			
-		}
-		System.out.println(start);
-	}
-	
-	static int upperBound(int target, ArrayList<Integer> list) {
-		int start = 0;
-		int end = list.size() - 1;
-		
-		while (start <= end) {
-			int mid = (start + end) / 2;
-			
-			if (target >= list.get(mid)) start = mid + 1;
-			else end = mid - 1;
-		}
-		
-		return start;
-	}
-	
-	
-	
+// The main method must be in a class named "Main".
+class Main {
+    static int N, C;
+    static int[] arr;    
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+
+        int start = 1;
+        int end = arr[N - 1];
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            
+            int count = 1;
+            int cur_num = arr[0];
+            
+            while (true) {
+                int idx = upperBound( cur_num + mid );
+                if (idx == N) break;
+                count++;
+                cur_num = arr[idx];
+            }
+
+            if (count >= C) start = mid + 1;
+            else end = mid - 1;
+            
+
+        }
+
+        System.out.println(start);
+    }
+
+    static int upperBound(int target) {
+        int start = 0;
+        int end = N - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (target >= arr[mid]) start = mid + 1;
+            else end =  mid - 1;
+        }
+
+        return start;
+    }
 }
