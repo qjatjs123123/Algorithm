6caf975363cc6cada1486db4fc0aae815bbef1cc
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-01-10 12:29:17 +0900

    [Gold V] Title: 집합의 표현, Time: 1208 ms, Memory: 55324 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,44 +1,57 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main
-{
-	static int[] node;
-	public static void main(String args[]) throws Exception
-	{
-//		BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
-		
-		String[] str = br.readLine().split(" ");
-		int n = Integer.parseInt(str[0]);
-		int m = Integer.parseInt(str[1]);
-		
-		node = new int[n + 1];
-		for (int i = 0; i < n + 1; i++) {
-			node[i] = i;
-		}
-		
-		for (int i = 0; i < m; i++) {
-			str = br.readLine().split(" ");
-			int cmd = Integer.parseInt(str[0]);
-			int a = Integer.parseInt(str[1]);
-			int b = Integer.parseInt(str[2]);	
-			
-			int pa = findParentNode(a);
-			int pb = findParentNode(b);
-			if (cmd == 0) {
-				node[Math.min(pa, pb)] = Math.max(pa, pb);
-			}else {
-				if (pa == pb) System.out.println("YES");
-				else System.out.println("NO");
-			}
-		}
-		
-    }	
-	
-	static int findParentNode(int cur_node) {
-		if (cur_node == node[cur_node]) return cur_node;
-		return findParentNode(node[cur_node]);
-	}
-	
+class Main {
+    static int n, m;
+    static int[] parents;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        parents = new int[n + 1];
+
+        for (int i = 0 ; i < n + 1; i++) parents[i] = i;
+        
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            
+            int cmd = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            if (cmd == 0) {
+                union(from, to);
+            } else {
+                check(from, to);
+            }
+        }
+
+
+    }
+
+    static void check(int a, int b) {
+        if (find(a) == find(b)) System.out.println("YES");
+        else System.out.println("NO");
+    }
+    
+    static int find(int a) {
+        if (a == parents[a]) return a;
+
+        return parents[a] = find(parents[a]);
+    }
+
+    static boolean union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if (rootA == rootB) return false;
+
+        parents[rootB] = rootA;
+        return true;
+    }
 }
