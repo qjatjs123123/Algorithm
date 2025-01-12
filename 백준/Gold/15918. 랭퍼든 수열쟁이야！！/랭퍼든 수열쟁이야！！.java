10d73d95b7deeff6535e550944676e19f1f552f6
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-01-12 16:26:51 +0900

    [Gold V] Title: 랭퍼든 수열쟁이야!!, Time: 248 ms, Memory: 12372 KB -BaekjoonHub

--- 백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.java
+++ 백준/Gold/15918. 랭퍼든 수열쟁이야！！/랭퍼든 수열쟁이야！！.java
@@ -7,6 +7,7 @@ class Main {
     static int n, x, y;
     static int ans = 0;
     static int[] arr;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,29 +17,37 @@ class Main {
         y = Integer.parseInt(st.nextToken());
 
         arr = new int[n * 2];
+        visited = new boolean[n + 1];
         arr[x - 1] = (y - x) - 1;
         arr[y - 1] = (y - x) - 1;
+        visited[(y - x - 1)] = true;
         
-        backtracking(1);
+        backtracking(0);
         System.out.println(ans);
     }
 
-    static void backtracking(int num) {
-        if (num > n) {
-            ans++; 
+    static void backtracking(int idx) {
+ 
+        if (idx == n * 2) {
+            ans++;
             return;
         }
-
-        if (num == (y - x - 1)) backtracking(num + 1);
-        
-        for (int i = 0; i < n * 2; i++) {
-            if (arr[i] == 0 && i + num + 1 < n * 2 && arr[i + num + 1] == 0) {
-                arr[i] = num;
-                arr[i + num + 1] = num;
-                backtracking(num + 1);
-                arr[i] = 0;
-                arr[i + num + 1] = 0;
+        if (arr[idx] == 0) {
+            for (int num = 1; num <= n; num++) {
+                if (!visited[num] && idx + num + 1 < n * 2 && arr[idx + num + 1] == 0) {
+                    visited[num] = true;
+                    arr[idx] = num;
+                    arr[idx + num + 1] = num; 
+                    backtracking(idx + 1);
+                    visited[num] = false;
+                    arr[idx] = 0;
+                    arr[idx + num + 1] = 0; 
+                } 
             }
+        } else {
+            backtracking(idx + 1);
         }
+        
+        
     }
 }
