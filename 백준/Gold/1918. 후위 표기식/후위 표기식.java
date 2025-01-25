989471cc483d4b9cf7c2c70165be441631daf1e0
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-01-25 17:40:23 +0900

    [Gold II] Title: 후위 표기식, Time: 68 ms, Memory: 11560 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.java
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -1,71 +1,91 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	
-	static HashMap<String, Integer> dict = new HashMap<>();
-	
-    public static void main(String[] args) throws IOException {
+// The main method must be in a class named "Main".
+class Main {
+    static char[] charArr;
+    static char[] operation= {'+', '-', '*', '/', '(', ')'};
+    static int[] level = {2, 2, 1, 1, 0, 0};
+    static StringBuilder sb = new StringBuilder();
+    static Stack<Character> stack = new Stack();
+    
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//    	BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-    	// init
-    	dict.put("+", 1);
-    	dict.put("-", 1);
-    	dict.put("*", 2);
-    	dict.put("/", 2);
-    	dict.put("(", 3);
-    	dict.put(")", 3);
-    	
-    	
-        String[] command = br.readLine().split("");
-        
-        Stack<String> stack = new Stack<>();
-        
-        String ans = "";
-        boolean isOpen = false;
-        for (String cmd : command) {
-        	int num = (int)cmd.charAt(0);
-        	
-        	if (num >= 65 && num <= 90) {
-        		ans += cmd;
-        		continue;
-        	}
-        	
-        	if (stack.isEmpty()) stack.push(cmd);
-        	else {
-        		while(!stack.isEmpty()) {
-        			String top = stack.peek();
-        			 			
-        			if (cmd.equals(")")) {
-        				while(true) {
-        					
-        					String top1 = stack.pop();
-        					if (top1.equals("(")) break;
-        					ans += top1;
-        				}
-        				break;
-        			}
-        			
-        			if (compare(top, cmd) || top.equals("(")) break;	
-        			ans += stack.pop();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    
+        String str = st.nextToken();
+        charArr = new char[str.length()];
 
-        		}
-        		if (!cmd.equals(")")) stack.push(cmd);
-        		
-        	}
-        	
+        for (int i = 0; i < charArr.length; i++) {
+            charArr[i] = str.charAt(i);
+        }
+
+        
+
+        for (char c : charArr) {
+            if (!isOperation(c)) {
+                sb.append(c);
+                continue;
+            }
+
+            boolean isInsert = shouldInsertStack(c);
+            
+            if (isInsert) stack.push(c);
+            else removeStack(c);
+            
+        }
+
+        while(!stack.isEmpty()) sb.append(stack.pop());
+
+        System.out.println(sb.toString());
+        
+    }
+
+    static void removeStack(char opr) {
+
+        if (opr == ')') {
+            
+            while (true) {
+                char top = stack.pop();
+
+                if (top == '(') break;
+                sb.append(top);
+            }
+        } else{
+            while (!stack.isEmpty()) {
+                char top = stack.peek();
+                
+                if (top == '(' || getOperationLevel(top) > getOperationLevel(opr)) break;
+
+                sb.append(stack.pop());
+                
+            }
+
+            stack.push(opr);
         }
         
-        while (!stack.isEmpty()) ans += stack.pop();
-        System.out.println(ans);
-        
-    }  
+    }
     
-    static boolean compare(String top, String cmd) {
-    	return dict.get(top) < dict.get(cmd);
+    static boolean shouldInsertStack(char opr) {
+        if (stack.size() == 0) return true;
+        if (opr == '(') return true;
+        if (opr == ')') return false;
+        
+        char top = stack.peek();
+        if (getOperationLevel(top) > getOperationLevel(opr)) return true;
+        return false;
+    }
+
+    static boolean isOperation(char c) {
+        for (char opr : operation) if (opr == c) return true;
+        return false;
+    }
+
+    static int getOperationLevel(char opr) {
+        for (int i = 0; i < operation.length; i++) {
+            if (operation[i] == opr) return level[i];
+        }
+        return -1;
     }
 }
-
-
