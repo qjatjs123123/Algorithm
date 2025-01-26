465562a14e2daa623a43ab504afd49dcab1945a9
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-01-26 15:28:39 +0900

    [Gold II] Title: 중앙값 구하기, Time: 236 ms, Memory: 20528 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
@@ -4,46 +4,62 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int T;
-    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        T = Integer.parseInt(st.nextToken());
+
+        int T = Integer.parseInt(st.nextToken());
         StringBuilder sb = new StringBuilder();
-        
         for (int t = 0; t < T; t++) {
             st = new StringTokenizer(br.readLine());
-            int m = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
 
-            int rowCnt = m / 10 + 1;
-            ArrayList<Integer> list = new ArrayList<>();
-            int cnt = 0;
-            StringBuilder sbb = new StringBuilder();
-            
+            PriorityQueue<Integer> min_pq = new PriorityQueue<>();
+            PriorityQueue<Integer> max_pq = new PriorityQueue<>((a, b) -> b - a);
+
+            int totalCnt = 0;
+            int rowCnt = M / 10 + 1;
+            StringBuilder tmp = new StringBuilder();
             for (int row = 0; row < rowCnt; row++) {
                 st = new StringTokenizer(br.readLine());
+
                 int colCnt = st.countTokens();
                 for (int col = 0; col < colCnt; col++) {
-                    list.add(Integer.parseInt(st.nextToken()));
-                    if (col % 2 == 0) {
-                        Collections.sort(list);
-                        cnt++;
-                        sbb.append(list.get(list.size() / 2) + " ");
-                        if (cnt % 10 == 0) sbb.append("\n");
-                        
+                    int num = Integer.parseInt(st.nextToken());
+
+                    if (totalCnt == 0) {
+                        max_pq.add(num);
+                        tmp.append(num).append(" ");
+                        totalCnt++;
+                        continue;
                     }
                     
+                    int middle = max_pq.peek();
+                    if (middle >= num) max_pq.add(num);
+                    else min_pq.add(num);
+
+                    if (col % 2 != 0 && min_pq.size() < max_pq.size()) {
+                        min_pq.add(max_pq.poll());
+                    }
+                    if (col % 2 != 0 && min_pq.size() > max_pq.size()) {
+                        max_pq.add(min_pq.poll());
+                    }
+
+                    if (col % 2 == 0) {
+                        if (max_pq.size() < min_pq.size()) max_pq.add(min_pq.poll());
+                        
+                        tmp.append(max_pq.peek() + " ");
+                        totalCnt++;   
+                        if (totalCnt % 10 == 0) tmp.append("\n");
+                    }
+
                 }
                 
             }
-            if (cnt % 10 != 0)
-                sbb.append("\n");
-            sb.append(cnt).append("\n");
-            sb.append(sbb.toString());
-            
+            sb.append(totalCnt).append("\n");
+            if (totalCnt % 10 != 0 ) tmp.append("\n");
+            sb.append(tmp.toString());
         }
-
         System.out.println(sb.toString());
     }
 }
