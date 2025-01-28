a80ab56029e92d433c3c06e13f66ef5850693a50
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-01-28 17:08:45 +0900

    [Gold IV] Title: 트리의 지름, Time: 2676 ms, Memory: 129680 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,85 +1,55 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	static int N;
-	static HashMap<Integer, ArrayList<int[]>> graph = new HashMap<>();
-	static boolean[] visited;
-	static int max_num = 0;
-	static int max_node = 0;
-	
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//    	BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-    	N = Integer.parseInt(st.nextToken());
-        while (true){
-        	try {
-	        	st = new StringTokenizer(br.readLine());
-	        	int from = Integer.parseInt(st.nextToken());
-	        	int to = Integer.parseInt(st.nextToken());
-	        	int weight = Integer.parseInt(st.nextToken());
-	        	
-	        	if (graph.containsKey(from)) {
-	        		ArrayList<int[]> tmp = graph.get(from);
-	        		tmp.add(new int[] {to, weight});
-	        		graph.put(from, tmp);
-	        	} else {
-	        		ArrayList<int[]> tmp = new ArrayList<>();
-	        		tmp.add(new int[] {to, weight});
-	        		graph.put(from, tmp);
-	        	}
-	        	
-	        	if (graph.containsKey(to)) {
-	        		ArrayList<int[]> tmp = graph.get(to);
-	        		tmp.add(new int[] {from, weight});
-	        		graph.put(to, tmp);
-	        	} else {
-	        		ArrayList<int[]> tmp = new ArrayList<>();
-	        		tmp.add(new int[] {from, weight});
-	        		graph.put(to, tmp);
-	        	}
-	        	
-        	}catch(Exception e) {
-        		break;
-        	}
-        }
-        visited = new boolean[N + 1];
-        visited[1] = true;
-        dfs(1, 0);
-        visited = new boolean[N + 1];
-        visited[max_node] = true;
-        max_num = 0;
-        dfs(max_node, 0);
-        System.out.println(max_num);
-    }  
+// The main method must be in a class named "Main".
+class Main {
+    static int n;
+    static ArrayList<int[]>[] graph;
+    static int ans = 0;
+    static boolean[] visited;
     
-    static void dfs(int cur_node, int cur_num) {
-    	
-    	visited[cur_node] = true;
-    	boolean flg = true;
-    	if (graph.containsKey(cur_node)) {
-	    	ArrayList<int[]> list = graph.get(cur_node);
-	    	
-	    	
-	    	for (int[] arr : list) {
-	    		int new_node = arr[0];
-	    		int new_num = cur_num + arr[1];
-	    		if (visited[new_node]) continue;
-	    		dfs(new_node, new_num);
-	    		flg = false;
-	    	}
-    	} 
-    	
-    	if (flg) {
-    		if (max_num < cur_num) {
-    			max_num = cur_num;
-    			max_node = cur_node;
-    		}
-    		return;
-    	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[n + 1];
+        
+        for (int i = 0; i < n + 1; i++) graph[i] = new ArrayList<>();
+
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            
+            int parent = Integer.parseInt(st.nextToken());
+            int child = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            graph[parent].add(new int[] {child, weight});
+            graph[child].add(new int[] {parent, weight});
+        }
+
+        for (int cur_node = 1; cur_node <= n; cur_node++) {
+            visited = new boolean[n + 1];
+            visited[cur_node] = true;
+            recursion(cur_node, 0);
+        }
+
+        System.out.println(ans);
+    }
+
+    static void recursion(int cur_node, int total) {
+
+         boolean flg = false;
+        
+         for (int[] cur_arr : graph[cur_node]) {
+             if (visited[cur_arr[0]]) continue;
+
+             visited[cur_arr[0]] = true;
+             recursion(cur_arr[0], total + cur_arr[1]);
+             flg = true;
+         }       
+
+        if (!flg) ans = Math.max(ans, total);
     }
 }
-
-
