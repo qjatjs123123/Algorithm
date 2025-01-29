e445dd470aad00fac7384b2ebec076552fe2bbbe
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-01-30 00:47:32 +0900

    [Gold II] Title: 트리의 높이와 너비, Time: 168 ms, Memory: 20220 KB -BaekjoonHub

--- 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.java
+++ 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.java
@@ -4,17 +4,16 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int n;
-    static int[] arr;
-    static boolean[] isRoot;
-    static Node[] nodeArr;
-    static Deque<Integer> depthDeque[];
-    static HashMap<Integer, int[]> dict = new HashMap<>();
+    static int N;
+    static boolean isNotRoot[];
+    static Node NodeArr[];
+    static Deque<Integer>[] depthArr;
+    static Deque<Integer> deque = new LinkedList<>();
     
     static class Node {
         int node, left, right;
 
-        Node(int node, int left, int right) {
+        Node(int node, int left, int right){
             this.node = node;
             this.left = left;
             this.right = right;
@@ -25,136 +24,92 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        arr = new int[n ];
+        N = Integer.parseInt(st.nextToken());
+        isNotRoot = new boolean[N + 1];
+        NodeArr = new Node[N + 1];
+        depthArr = new Deque[N + 1];
 
-        isRoot = new boolean[n + 1];
-        nodeArr = new Node[n + 1];
-        depthDeque = new Deque[n + 1];
-
-        for (int i = 0; i < n + 1; i++) depthDeque[i] = new LinkedList<>();
+        for (int i = 0; i < N + 1; i++) depthArr[i] = new LinkedList<>();
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int root = Integer.parseInt(st.nextToken());
+            int node = Integer.parseInt(st.nextToken());
             int left = Integer.parseInt(st.nextToken());
             int right = Integer.parseInt(st.nextToken());
 
-            nodeArr[root] = new Node(root, left, right);
-            if (left != -1)
-                isRoot[left] = true;
-            if (right != -1)
-                isRoot[right] = true;
+            if (left != -1) isNotRoot[left] = true;
+            if (right != -1) isNotRoot[right] = true;
+
+            NodeArr[node] = new Node(node, left, right);
         }
 
-        int root = findRoot();
-        dfs(nodeArr[root], nodeArr[root], 0, 1);
+        int rootNum = getRootNum();
+        inOrder(rootNum, 0);
 
-        int depth = 0;
+        getAnswer();
 
-
-
-        HashMap<Integer, Integer> posMap = new HashMap();
-        while (true) {
-            Deque<Integer> deque = depthDeque[depth];  
-            if (deque.isEmpty()) break;
-
-            
-            if (depth == 0) {
-                int[] pos = dict.get(root);
-                int leftCnt = pos[0];
-                int rightCnt = pos[1];
-
-                arr[leftCnt] = root;
-                posMap.put(root, leftCnt);
-            } else {          
-                for (int num : deque) {
-                    if (dict.containsKey(num)) {
-                        int[] pos = dict.get(num);
-                        int leftCnt = pos[0];
-                        int rightCnt = pos[1];
-                        int cur_root = pos[2];
-                        int isLeft = pos[3];
-                        int root_idx = posMap.get(cur_root);
-                        
-                        if (isLeft == 1) {
-                            int cur_idx = root_idx - rightCnt - 1;
-                            arr[cur_idx] = num;
-                            posMap.put(num, cur_idx);
-                        } else {
-                            int cur_idx = root_idx + leftCnt + 1;
-                            arr[cur_idx] = num;
-                            posMap.put(num, cur_idx);
-                        }  
-                        
-                    } 
-                }
-            }
-            
-            depth++;      
-        }
-        int width = 1;
-        int ans_depth = 1;
-        depth = 0;
         
-        while(true) {
-            Deque<Integer> deque = depthDeque[depth];  
-            if (deque.isEmpty()) break;
-
-            if (deque.size() == 1) {
-                depth++;
-                continue;
-            }
-            
-            int left = deque.pollFirst();
-            int right = deque.pollLast();
-
-            int left_idx = posMap.get(left);
-            int right_idx = posMap.get(right);
-
-            int cur_width = right_idx - left_idx + 1;
-
-            if (width < cur_width) {
-                width = cur_width;
-                ans_depth = depth + 1;
-            }
-            
-            depth++;
-        }
-
-        System.out.println(ans_depth + " " + width);
     }
 
-    static int dfs(Node node, Node prev, int depth, int isLeft) {
-        int left = node.left;
-        int right = node.right;
-        depthDeque[depth].add(node.node);
-        if (left == -1 && right == -1) {
-            dict.put(node.node, new int[] {0, 0, prev.node, isLeft});
-            return 1;
+    static void getAnswer() {
+        HashMap<Integer, Integer> dict = new HashMap<>();
+
+        int idx = 0;
+        for (int node : deque) {
+            dict.put(node, idx);
+            idx++;
         }
 
-        int leftCnt = 0;
-        int rightCnt = 0;
-        if (left != -1) leftCnt = dfs(nodeArr[left], node, depth + 1, 1);
-        if (right != -1) rightCnt = dfs(nodeArr[right], node, depth + 1, 0);
+        int depth = 1;
+        int width = 1;
         
-        // System.out.println(node.node + " " + leftCnt + " "+ rightCnt);
-        dict.put(node.node, new int[] {leftCnt, rightCnt, prev.node, isLeft});
-        return 1 + leftCnt + rightCnt;
+        for (int i = 0; i <= N; i++) {
+            Deque<Integer> cur_deque = depthArr[i];
+            
+            if(depthArr[i].isEmpty()) break;
+
+            if (cur_deque.size() == 1) continue;
+
+            int left_idx = dict.get(cur_deque.pollFirst());
+            int right_idx = dict.get(cur_deque.pollLast());
+            int new_width = right_idx - left_idx + 1;
+
+            if (new_width > width){
+                depth = i + 1;
+                width = new_width;
+            }
+            
+        }
+
+        System.out.println(depth + " " + width);
     }
     
-    static int findRoot() {
-        int root = 0;
+    static void inOrder(int nodeNum, int cur_depth) {
+        Node curNode = NodeArr[nodeNum];
+
+        if (curNode.left == -1 && curNode.right == -1) {
+            depthArr[cur_depth].add(nodeNum);
+            deque.add(nodeNum);
+            return;
+        }
+
+        if (curNode.left != -1) inOrder(curNode.left, cur_depth + 1);
+        depthArr[cur_depth].add(nodeNum);
+        deque.add(nodeNum);
+        if (curNode.right != -1) inOrder(curNode.right, cur_depth + 1);
+    }
+    
+    static int getRootNum() {
+        int rootNum = 0;
         
-        for (int i = 1; i < n + 1; i++) {
-            if (!isRoot[i]) {
-                root = i;
+        for (int i = 1; i < N + 1; i++) {
+            if (!isNotRoot[i]) {
+                rootNum = i;
                 break;
             }
         }
 
-        return root;
+        return rootNum;
     }
 }
