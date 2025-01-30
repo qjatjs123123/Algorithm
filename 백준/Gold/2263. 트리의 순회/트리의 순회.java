eee61ee955c2a7aa34d755879d90de026cd8dd3e
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-01-30 14:06:44 +0900

    [Gold I] Title: 트리의 순회, Time: 2176 ms, Memory: 72760 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.java
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -1,49 +1,52 @@
-
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	
-	static int N = 0;
-	static int[] inorder;
-	static int[] postorder;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
-		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		inorder = new int[N];
-		postorder = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) inorder[i] = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) postorder[i] = Integer.parseInt(st.nextToken());
-		
-		preorder(0, N - 1, 0, N - 1);
-		System.out.println(sb.toString());
-	}
-	
-	static void preorder(int in_s, int in_e, int post_s, int post_e) {
-		if (in_s > in_e || post_s > post_e) return;
-		
-		int root = postorder[post_e];
-		int root_idx = 0;
-		
-		for (int i = 0; i < N ; i++) {
-			if (inorder[i] == root) {
-				root_idx = i;
-				break;
-			}
-		}
-		
-		int leftCnt = root_idx - in_s;
-		sb.append(root).append(" ");
-		preorder(in_s, root_idx - 1, post_s, post_s + leftCnt - 1);
-		preorder(root_idx + 1, in_e, post_s + leftCnt, post_e - 1);
-	}
+// The main method must be in a class named "Main".
+class Main {
+    static int N;
+    static int[] inorder;
+    static int[] postorder;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        inorder = new int[N];
+        postorder = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) inorder[i] = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) postorder[i] = Integer.parseInt(st.nextToken());
+
+        preorder(0, N, 0, N);
+    }
+
+    static void preorder(int in_left, int in_right, int post_left, int post_right) {
+        if (in_right <= in_left) return;
+        
+        if (in_right - in_left == 1) {
+            System.out.print(inorder[in_left] + " ");
+            return;
+        }
+
+        int root = postorder[post_right - 1];
+        int root_idx = 0;
+        
+        for (int i = in_left; i < in_right; i++) {
+            if (inorder[i] == root) {
+                root_idx = i;
+                break;
+            }
+        }
+
+        int leftCnt = root_idx - in_left;
+        System.out.print(root + " ");
+        preorder(in_left, root_idx, post_left, post_left + leftCnt);
+        preorder(root_idx + 1, in_right, post_left + leftCnt, post_right - 1);
+    }
 }
