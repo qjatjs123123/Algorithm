141ba5b71d5dc535c65505ece252962b2334af5b
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-01-30 14:09:11 +0900

    [Gold I] Title: 트리의 순회, Time: 1852 ms, Memory: 59448 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.java
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -7,6 +7,7 @@ class Main {
     static int N;
     static int[] inorder;
     static int[] postorder;
+    static StringBuilder sb = new StringBuilder();
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,15 +25,13 @@ class Main {
         for (int i = 0; i < N; i++) postorder[i] = Integer.parseInt(st.nextToken());
 
         preorder(0, N, 0, N);
+        System.out.println(sb.toString());
     }
 
     static void preorder(int in_left, int in_right, int post_left, int post_right) {
         if (in_right <= in_left) return;
         
-        if (in_right - in_left == 1) {
-            System.out.print(inorder[in_left] + " ");
-            return;
-        }
+
 
         int root = postorder[post_right - 1];
         int root_idx = 0;
@@ -45,7 +44,7 @@ class Main {
         }
 
         int leftCnt = root_idx - in_left;
-        System.out.print(root + " ");
+        sb.append(root + " ");
         preorder(in_left, root_idx, post_left, post_left + leftCnt);
         preorder(root_idx + 1, in_right, post_left + leftCnt, post_right - 1);
     }
