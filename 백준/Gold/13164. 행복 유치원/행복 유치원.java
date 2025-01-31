6488d8b5e882cb34222529181fa89af3d81c4e57
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-01-31 19:34:00 +0900

    [Gold V] Title: 행복 유치원, Time: 376 ms, Memory: 62780 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N, K;
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        int[] arr_diff = new int[N - 1];
+
+        for (int i = 0; i < N - 1; i++) arr_diff[i] = arr[i + 1] - arr[i];
+
+        Arrays.sort(arr_diff);
+
+        int ans = 0;
+        for (int i = 0; i < N - K; i++) ans += arr_diff[i];
+
+        System.out.println(ans);
+    }
+}
