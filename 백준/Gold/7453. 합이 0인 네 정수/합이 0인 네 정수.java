7c31830b8e19e2d47ca2343c96be21ecc0e77c96
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-02-23 21:51:52 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 10148 ms, Memory: 96732 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
@@ -1,82 +1,81 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	static int[][] arr;
-	static int[] list1;
-	static int[] list2;
-	static int n = 0;
-	public static void main(String[] args) throws IOException {
-		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		arr = new int[n][4];
-		list1 = new int[n*n];
-		list2 = new int[n*n];
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			
-			for (int j = 0; j < 4; j++) arr[i][j] = Integer.parseInt(st.nextToken());
-		}
-		
+// The main method must be in a class named "Main".
+class Main {
+    static int n;
+    static int[][] graph;
+    static int[] right;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		make(2, list2);
-		
-		Arrays.sort(list2);
+        n = Integer.parseInt(st.nextToken());
+        graph = new int[n][4];
+        right = new int[n * n];
+        
+        for (int row = 0; row < n; row++) {
+            st = new StringTokenizer(br.readLine());
+            for (int col = 0; col < 4; col++) {
+                graph[row][col] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		long ans = 0;
-		
-		for (int i = 0; i < n; i++) {	
-			for (int j = 0; j < n; j++) {
-				int temp = arr[i][0] + arr[j][1];
-				int l1 = upperBound(list2, temp *  -1);
-				int l2 = lowerBound(list2, temp *  -1);
-				ans += (l1 - l2);
-			}
+        
 
-		}
-		
+        int idx = 0;
 
-		System.out.println(ans);
-	}
-	
-	static void make(int start, int[] list) {
-		int index = 0;
-		for (int i = 0; i < n; i++) {	
-			for (int j = 0; j < n; j++) {
-				list[index++] = arr[i][start] + arr[j][start + 1];
-			}
+        
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                right[idx] = graph[row][2] + graph[col][3];
+                idx++;
+            }
+        }
 
-		}
-	}
-	
-	static int upperBound(int[] arr, int key) {
-		int start = 0, end = arr.length-1;
-		while (start <= end) {
-			int mid = (start + end)/2;
-			if (arr[mid] > key) {
-				end = mid - 1;
-			} else {
-				start = mid + 1;
-			}
- 		}
-		return end;
-	}
-	
-	static int lowerBound(int[] arr, int key) {
-		int start = 0, end = arr.length-1;
-		while (start <= end) {
-			int mid = (start + end)/2;
-			if (arr[mid] >= key) {
-				end = mid - 1;
-			} else {
-				start = mid + 1;
-			}
- 		}
-		return end;
-	}
+        Arrays.sort(right);
+
+
+        long ans = 0;
+        
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                int num = graph[row][0] + graph[col][1];
+
+                ans += (lower_bound(-num) - upper_bound(-num));
+            }
+        }
+        
+        System.out.println(ans);
+    }
+
+    static int upper_bound(int target) {
+        int start = 0;
+        int end = right.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            
+            if (right[mid] >= target) end = mid - 1;
+            else start = mid + 1;
+        }
+
+        return start;
+    }
+
+    static int lower_bound(int target) {
+        int start = 0;
+        int end = right.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            
+            if (right[mid] > target) end = mid - 1;
+            else start = mid + 1;
+        }
+
+        return start;
+    }
 }
-
