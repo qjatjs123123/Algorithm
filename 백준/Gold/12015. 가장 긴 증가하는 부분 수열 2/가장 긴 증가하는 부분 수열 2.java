5df9742339baba1aacd5ece90bfa3513f2e1ef50
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-02-24 12:59:53 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 564 ms, Memory: 149788 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,44 +1,47 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int[] arr = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
-		
-		ArrayList<Integer> list = new ArrayList<>();
-		list.add(arr[0]);
+// The main method must be in a class named "Main".
+class Main {
+    static int N;
+    static int[] arr;
+    static ArrayList<Integer> list = new ArrayList<>();
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		for (int i = 1; i < N; i++) {
-			int idx = lowerBound(arr[i], list);
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[N];
 
-			if (list.size() == idx) list.add(arr[i]);
-			else list.set(idx, arr[i]);
-		}
-		
-		System.out.println(list.size());
-	}
-	
-	static int lowerBound(int target, ArrayList<Integer> list) {
-		int start = 0;
-		int end = list.size() - 1;
-		
-		while (start <= end) {
-			int mid = (start + end) / 2;
-			
-			if (target <= list.get(mid)) end = mid - 1;
-			else start = mid + 1;
-		}
-		
-		return start;
-	}
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        list.add(arr[0]);
+
+        for (int i = 1; i < N; i++) {
+            int idx = lower_bound(arr[i]);
+
+            
+            if (idx >= list.size()) list.add(arr[i]);
+            else list.set(idx, arr[i]);
+        }
+
+        System.out.println(list.size());
+    }
+
+    static int lower_bound(int target) {
+        int start = 0;
+        int end = list.size() - 1;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            if (list.get(mid) >= target) end = mid - 1;
+            else start = mid + 1;
+        }
+
+        return start;
+    }
 }
