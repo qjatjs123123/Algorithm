1cc55fe20eadafa4d85b7ecb2f94394bf69b1031
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-02-24 16:41:27 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 64 ms, Memory: 11668 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -1,61 +1,57 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N, M, L;
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		L = Integer.parseInt(st.nextToken());
-		
-		ArrayList<Integer> list = new ArrayList<>();
-		list.add(0);
-		
-		if (N != 0) {
-			st = new StringTokenizer(br.readLine());
-			
-			for (int i = 0; i < N; i++) {
-				list.add(Integer.parseInt(st.nextToken()));
-			}
-		}
-		
-		list.add(L);
-		
-		Collections.sort(list);
-		
-		ArrayList<Integer> distance = new ArrayList<>();
-		
-		for (int i = 1; i < list.size(); i++) {
-			int gap = list.get(i) - list.get(i - 1);
-			distance.add(gap);
-		}
-		
-		int start = 1;
-		int end = L - 1;
-		
-		while (start <= end) {
-			int mid = (start + end) / 2;
-			
-			int count = 0;
-			
-			for (int dist : distance) {
-				count += dist / mid;
-				
-				if (dist % mid == 0) count--;
-			}
-			
-			if (count <= M) end = mid - 1;
-			else start = mid + 1;
-		}
-		
-		System.out.println(start);
-	}
-	
+// The main method must be in a class named "Main".
+class Main {
+    static int N, M, L;
+    static int[] arr;
+    static PriorityQueue<Integer> pq = new PriorityQueue();
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+
+        arr = new int[N + 2];
+        st = new StringTokenizer(br.readLine());
+        
+        for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        arr[N + 1] = L;
+        Arrays.sort(arr);
+
+
+        int max_num = 0;
+        int min_num = Integer.MAX_VALUE;
+        int[] gapArr = new int[N + 1];
+
+        for (int i = 1; i < arr.length; i++) {
+            gapArr[i - 1] = arr[i] - arr[i - 1];
+            max_num = Math.max(max_num, gapArr[i - 1]);
+            min_num = Math.min(min_num, gapArr[i - 1]);
+        }
+
+        int start = 1;
+        int end = 1000;
+        
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            int count = 0;
+            for (int gap : gapArr) {
+                int tmp  = gap / mid;
+                count += tmp;
+                if (gap % mid == 0) count--;
+            }
+            
+            if (count > M) start = mid + 1;
+            else end = mid - 1;
+            
+        }
+
+        System.out.println(start);
+    }
 }
