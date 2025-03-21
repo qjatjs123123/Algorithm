e7e3a611c04136d754aaba383412501ee3154cf2
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-03-21 16:26:36 +0900

    [Gold III] Title: 내리막 길, Time: 280 ms, Memory: 40184 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,64 +1,57 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	static int N, M;
-	static int[][] graph;
-	static int[][] memo;
-	static int[][] direction = {
-			{1, 0 },
-			{-1, 0},
-			{0, 1},
-			{0, -1}
-	};
-	
-	public static void main(String[] args) throws IOException {		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		graph = new int[N][M];
-		memo = new int[N][M];
-		
-		for (int row = 0; row < N; row++) {
-			for(int col = 0; col < M; col++) {
-				for(int i = 0; i < 4; i++) memo[row][col] = -1;
-			}
-		}
-		
-		for (int row = 0; row < N; row++) {
-			st = new StringTokenizer(br.readLine());
-			for (int col = 0; col < M; col++) {
-				graph[row][col] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		for (int[] direct: direction) dp(0,0);
-		
-		System.out.println(memo[0][0]);
-	}
-	
-	static int dp(int row, int col) {
-		
-		int total = 0;
-		if (row == N -1 && col == M - 1) {
-			return 1;
-		}
-		if (memo[row][col] != -1) return memo[row][col];
-		
-		for (int[] direct: direction) {
-			int new_row = row + direct[0];
-			int new_col = col + direct[1];
-			
-			if (new_row < 0 || new_row == N || new_col < 0 || new_col == M) continue;
-			if (graph[row][col] <= graph[new_row][new_col]) continue;
-			
-			total += dp(new_row, new_col);
-		}
-		
-		memo[row][col] = total;
-		return total;
-	}
+// The main method must be in a class named "Main".
+class Main {
+    static int M, N;
+    static int[][] graph;
+    static int[] dy = new int[] {1, -1, 0, 0};
+    static int[] dx = new int[] {0, 0, 1, -1};
+    static long[][] dp;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        graph = new int[M][N];
+        dp = new long[M][N];
+        
+        for (int row = 0; row < M; row++) {
+            st = new StringTokenizer(br.readLine());
+            
+            for (int col = 0; col < N; col++) {
+                graph[row][col] = Integer.parseInt(st.nextToken());
+                dp[row][col] = -1;
+            }
+            
+        }
+
+        System.out.println(dfs(0, 0));
+    }
+
+    static long dfs(int cur_row, int cur_col) {
+        if (cur_row == M - 1  && cur_col  == N - 1) {
+            return 1;
+        }
+
+        if (dp[cur_row][cur_col] != -1) return dp[cur_row][cur_col];
+        
+        long result = 0;
+        for (int i = 0; i < 4; i++) {
+            int new_row = cur_row + dy[i];
+            int new_col = cur_col + dx[i];
+
+            if (new_row < 0 || new_row == M || new_col < 0 || new_col == N) continue;
+            if (graph[new_row][new_col] >= graph[cur_row][cur_col]) continue;
+            
+            result += dfs(new_row, new_col);
+        }
+
+        dp[cur_row][cur_col] = result;
+        return result;
+    } 
 }
