60fb2e7992a51a3052e66f8515b86fef67c2051c
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-04-25 15:51:30 +0900

    [Gold III] Title: 앱, Time: 104 ms, Memory: 16212 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -5,43 +5,58 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
     static int N, M;
-    static int[] memory;
-    static int[] cost;
+    static int[] memoryArr;
+    static int[] costArr;
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        memory = new int[N];
-        cost = new int[N];
 
+        memoryArr = new int[N + 1];
+        costArr = new int[N + 1];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) memory[i] = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) cost[i] = Integer.parseInt(st.nextToken());
-
-        int[][] memo = new int[N][10001];
-        memo[0][cost[0]] = memory[0];
         
-        for (int r = 1; r < N; r++) {
-            memo[r][cost[r]] = Math.max(memo[r][cost[r]], memory[r]);
-            for (int c = 0; c <= 10000; c++) {
-                if (memo[r-1][c] != 0) {
-                    memo[r][c] = Math.max(memo[r][c], memo[r - 1][c]);
-                    memo[r][c + cost[r]] = Math.max(memo[r][c + cost[r]], memo[r-1][c] + memory[r]);
+        for (int i = 1; i <= N; i++) {
+            memoryArr[i] = Integer.parseInt(st.nextToken());    
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            costArr[i] = Integer.parseInt(st.nextToken());    
+        }
+
+        int[][] dp = new int[101][10_001];
+        int answer = Integer.MAX_VALUE;
+        
+        for (int row = 1; row <= N; row++) {
+            int memory = memoryArr[row];
+            int cost = costArr[row];
+            
+            if (row == 1) {
+                dp[row][cost] = memory;
+                continue;
+            }
+
+            
+            
+            for (int col = 0; col <= 10_000; col++) {
+                dp[row][col] = dp[row - 1][col];
+                if (col >= cost) {
+                    dp[row][col] = Math.max(dp[row - 1][col - cost] + memory, dp[row - 1][col]);
                 }
             }
         }
 
         for (int c = 0; c <= 10000; c++) {
-            if (memo[N-1][c] >= M) {
+            if (dp[N][c] >= M) {
                 System.out.println(c);
                 break;
             }
         }
-    }
 
+        
+    }
 }
