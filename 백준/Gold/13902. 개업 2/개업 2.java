dfb2a14db35c5486e52e81cab57bbc0f0286cdbb
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-04-27 17:43:00 +0900

    [Gold IV] Title: 개업 2, Time: 564 ms, Memory: 27604 KB -BaekjoonHub

--- 백준/Gold/13902. 개업 2/개업 2.java
+++ 백준/Gold/13902. 개업 2/개업 2.java
@@ -1,66 +1,61 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
+// The main method must be in a class named "Main".
 class Main {
     static int N, M;
-    static boolean[] possibleSize;
-    static int[] cache;
-    static final int INF = 1000000000; // 충분히 큰 값
-
-    public static void main(String[] args) throws IOException {
+    static ArrayList<Integer> sizeArr = new ArrayList<>();
+    static int[] dp;
+    
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        int[] wok = new int[M];
-
+        int[] arr = new int[M];
+        dp = new int[N + 1];
+        
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            wok[i] = Integer.parseInt(st.nextToken());
-        }
-
-        possibleSize = new boolean[10001];
-        preprocess(wok);
-
-        cache = new int[N + 1];
-        Arrays.fill(cache, -1);
-
-        int result = dfs(N);
-        if (result >= INF) System.out.println(-1);
-        else System.out.println(result);
-    }
-
-    static void preprocess(int[] wok) {
-        int M = wok.length;
-        // 한 개 사용하는 경우
-        for (int i = 0; i < M; i++) {
-            possibleSize[wok[i]] = true;
-        }
-        // 두 개 사용하는 경우
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++) arr[i] = Integer.parseInt(st.nextToken());
+        
+        for (int i = 0; i < M - 1; i++) {
+            sizeArr.add(arr[i]);
             for (int j = i + 1; j < M; j++) {
-                int sum = wok[i] + wok[j];
-                if (sum <= 10000) {
-                    possibleSize[sum] = true;
-                }
+                sizeArr.add(arr[i] + arr[j]);
             }
         }
+        sizeArr.add(arr[M - 1]);
+
+        for (int num : sizeArr) {
+            if (num > N) continue;
+            dp[num] = 1;
+        }
+        dfs(N);
+
+        // System.out.println(Arrays.toString(dp));
+        
+        if (dp[N] == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(dp[N]);
     }
 
-    static int dfs(int remain) {
-        if (remain == 0) return 0;
-        if (remain < 0) return INF;
-        if (cache[remain] != -1) return cache[remain];
+    static int dfs(int num) {
+        if (dp[num] != 0) return dp[num];
+        if (num == 0) return 0;
 
-        int minCnt = INF;
-        for (int i = 1; i <= remain; i++) {
-            if (possibleSize[i]) {
-                minCnt = Math.min(minCnt, dfs(remain - i) + 1);
-            }
+        int total = Integer.MAX_VALUE;
+        for (int n : sizeArr) {
+            int target = num - n;
+            if (target < 0) continue;
+            if (dp[target] == Integer.MAX_VALUE) continue;
+            int tmp = dfs(target) + 1;
+
+            if (tmp <= Integer.MIN_VALUE + 1) continue;
+            total = Math.min(tmp, total);
         }
 
-        cache[remain] = minCnt;
-        return cache[remain];
+        dp[num] = total;
+        return total;
     }
 }
