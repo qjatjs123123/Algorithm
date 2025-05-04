5ac13de98bc2b07f55ef9334bc327a2babc07bba
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-05-04 16:11:53 +0900

    [Gold IV] Title: 최단경로, Time: 668 ms, Memory: 107788 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,24 +2,10 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-// The main method must be in a class named "Main".
 class Main {
     static int V, E;
-    static ArrayList<Edge>[] graph;
-    
-    static class Edge implements Comparable<Edge> {
-        int node, dist;
-
-        Edge(int node, int dist) {
-            this.node = node;
-            this.dist = dist;
-        }
-
-        @Override 
-        public int compareTo(Edge o){
-            return Integer.compare(this.dist, o.dist);
-        }
-    }
+    static int K;
+    static ArrayList<ArrayList<Pos>> graph = new ArrayList<>();
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,56 +14,80 @@ class Main {
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[V + 1];
-        for (int i = 0; i < V + 1; i++) graph[i] = new ArrayList<>();
-
         st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        for (int row = 0; row <= V; row++) {
+            ArrayList<Pos> new_list = new ArrayList<>();
+            graph.add(new_list);
+        }
 
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
 
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            int dist = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
 
-            graph[from].add(new Edge(to, dist));
+            ArrayList<Pos> cur_list = graph.get(from);
+            cur_list.add(new Pos(to, weight));
+
+            
         }
+        
+        PriorityQueue<Pos> pq = new PriorityQueue<>();
 
         int[] distance = new int[V + 1];
-        for (int i = 0; i < V + 1; i++) distance[i] = Integer.MAX_VALUE;
-        distance[start] = 0;
-
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(start, 0));
+        boolean[] visited = new boolean[V + 1];
+        for (int i = 0; i <= V; i++) distance[i] = Integer.MAX_VALUE;
+        distance[K] = 0;
+        pq.add(new Pos(K, 0));
 
         while(!pq.isEmpty()) {
-            Edge edge = pq.poll();
+            Pos cur_pos = pq.poll();    
 
-            if (edge.dist > distance[edge.node]) continue;
-            
-            ArrayList<Edge> list = graph[edge.node];
-            for (Edge new_edge : list) {
-                if (distance[new_edge.node] >= new_edge.dist + edge.dist) {
-                    distance[new_edge.node] = new_edge.dist + edge.dist;
-                    pq.add(new Edge(new_edge.node, new_edge.dist + edge.dist));
+            int cur_node = cur_pos.v;
+            int cur_weight = cur_pos.w;
+            ArrayList<Pos> new_graph = graph.get(cur_node);
+
+            if (new_graph.size() == 0) continue;
+            if (cur_weight > distance[cur_node]) continue;
+
+            for (Pos new_pos : new_graph) {
+                int new_node = new_pos.v;
+                int new_weight = new_pos.w;
+
+                if (distance[new_node] > cur_weight + new_weight) {
+                    distance[new_node] = cur_weight + new_weight;
+                    pq.add(new Pos(new_node, cur_weight + new_weight));
                 }
             }
             
         }
 
         StringBuilder sb = new StringBuilder();
-        int idx = 0;
-        for (int dist : distance) {
-            if (idx == 0){
-                idx++;
-                continue;
-            }
-            if (dist == Integer.MAX_VALUE) sb.append("INF");
-            else sb.append(dist);
+
+        for (int col = 1; col <= V; col++) {
+            if (distance[col] == Integer.MAX_VALUE) sb.append("INF");
+            else sb.append(distance[col]);
+
             sb.append("\n");
         }
 
         System.out.println(sb.toString());
+    }
+
+    static class Pos implements Comparable<Pos> {
+        int v, w;
+
+        Pos(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Pos pos) {
+            return Integer.compare(this.w, pos.w);
+        }
     }
 }
