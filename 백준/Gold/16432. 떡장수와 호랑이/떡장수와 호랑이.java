4d601643359c92344c92517b30adfe7489f0f7d0
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-05-14 00:52:39 +0900

    [Gold IV] Title: 떡장수와 호랑이, Time: 124 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Gold/16432. 떡장수와 호랑이/떡장수와 호랑이.java
+++ 백준/Gold/16432. 떡장수와 호랑이/떡장수와 호랑이.java
@@ -1,52 +1,74 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main {
+// The main method must be in a class named "Main".
+class Main {
     static int N;
-    static List<Integer>[] graph;
-    static boolean[][] visited;
-    static int[] answer;
-    static boolean found = false;
-
-    public static void main(String[] args) throws IOException {
+    static HashSet<Integer>[] graph;
+    static Stack<Integer> stack = new Stack<>();
+    static boolean flg = false;
+    static boolean[][] dp;
+    
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        graph = new ArrayList[N];
-        visited = new boolean[N][10]; // [day][prev] 떡
-        answer = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        N = Integer.parseInt(st.nextToken());
+        graph = new HashSet[N];
+        boolean isNoAnswer = false;
+        dp = new boolean[N + 1][10];
+            
         for (int i = 0; i < N; i++) {
-            graph[i] = new ArrayList<>();
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            
+            graph[i] = new HashSet<>();
             int cnt = Integer.parseInt(st.nextToken());
+
             for (int j = 0; j < cnt; j++) {
                 graph[i].add(Integer.parseInt(st.nextToken()));
             }
-        }
 
-        dfs(0, 0); // day=0, prev=0 (떡 번호는 1~9 이므로 0은 없음 의미)
-
-        if (!found) {
-            System.out.println("-1");
+            if (i >= 1) {
+                if (graph[i-1].size() == 1 && graph[i].size() == 1) {
+                    int prev = 0;
+                    int next = 0;
+                    for (int tmp : graph[i - 1]) prev = tmp;
+                    for (int tmp : graph[i]) next = tmp;
+                    
+                    if (prev == next) isNoAnswer = true;
+                }
+            }
         }
+        
+        
+         dfs(0, 0);
+        if(!flg) System.out.println(-1);
+
     }
 
-    static void dfs(int day, int prev) {
-        if (found) return;
-        if (day == N) {
-            // 성공!
-            for (int x : answer) System.out.println(x);
-            found = true;
+    static void dfs(int depth, int prev) {
+        if (flg) return;
+        if (dp[depth][prev]) return;
+        if (depth == N) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int num : stack) sb.append(num).append("\n");
+            System.out.println(sb.toString());
+            flg = true;
             return;
         }
 
-        if (visited[day][prev]) return;
-        visited[day][prev] = true;
-
-        for (int curr : graph[day]) {
-            if (curr == prev) continue;
-            answer[day] = curr;
-            dfs(day + 1, curr);
+        HashSet<Integer> list = graph[depth];
+        for (int num : list){
+            if (flg) return;
+            if (num == prev) continue;
+            stack.push(num);
+            dfs(depth + 1, num);
+            stack.pop();
         }
+        dp[depth][prev] = true;
+        
     }
 }
+
