b19edeb5f223ad46c899beba22957709a4d28aa8
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-05-16 16:55:23 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 264 ms, Memory: 40108 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -11,59 +11,48 @@ class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[] track = new int[1_000_000];
-        for (int i = 0; i < 1_000_000; i++) track[i] = -1;
+        int[] dp = new int[300_001];
+        for (int i = 0; i < 300_001; i++) dp[i] = -1;
+        dp[N] = 0;
 
-        
-        Deque<int[]> deque = new LinkedList<>();
-        deque.add(new int[] {0, N});
-        track[N] = -2;
-        
-        while(!deque.isEmpty()) {
-            int[] cur_arr = deque.pollFirst();
-            int cur_dist = cur_arr[0];
-            int cur_node = cur_arr[1];
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.add(N);
 
-            if (cur_node == K) {
-                Stack<Integer> stack = new Stack();
-                stack.push(K);
-                int cur_num = track[cur_node];
+        while (!deque.isEmpty()) {
+            int cur_num = deque.pollFirst();
+            if (cur_num == K) {
+                int cnt = 0;
+                Stack<Integer> stack = new Stack<>();
                 
-                while(true) {
-                    
-                    if (cur_num == -2) break;
-
+                while (cur_num != N) {
                     stack.push(cur_num);
-                    cur_num = track[cur_num];
+                    cur_num = dp[cur_num];
+                    cnt++;
                 }
 
-                StringBuilder sb = new StringBuilder();
-                sb.append(cur_dist).append("\n");
+                stack.push(cur_num);
 
-                while (!stack.isEmpty()) sb.append(stack.pop()).append(" ");
+                StringBuilder sb = new StringBuilder();
+                sb.append(cnt).append("\n");
+
+                while (!stack.isEmpty()) {
+                    sb.append(stack.pop()).append(" ");
+                }
 
                 System.out.println(sb.toString());
                 break;
+                
             }
             
-            int case1 = cur_node - 1;
-            if (case1 >= 0 && track[case1] == -1) {
-                track[case1] = cur_node;
-                deque.add(new int[] {cur_dist + 1, case1});
-            }
+            int[] tmp = new int[] {cur_num - 1, cur_num + 1, cur_num * 2};
 
-            int case2 = cur_node + 1;
-            if (case2 < 1_000_000 && track[case2] == -1) {
-                track[case2] = cur_node;
-                deque.add(new int[] {cur_dist + 1, case2});
-            }
+            for (int n : tmp) {
+                if (n < 0 || n > 300_000) continue;
+                if (dp[n] != -1) continue;
 
-            int case3 = cur_node * 2;
-            if (case3 < 1_000_000 && track[case3] == -1) {
-                track[case3] = cur_node;
-                deque.add(new int[] {cur_dist + 1, case3});
+                dp[n] = cur_num;
+                deque.add(n);
             }
-            
         }
         
     }
