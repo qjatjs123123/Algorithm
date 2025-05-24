88f178ba5f14081820b4f44d7081dc7fe1c6c080
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-05-24 16:23:49 +0900

    [Gold II] Title: 후위 표기식, Time: 64 ms, Memory: 11552 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.java
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -4,88 +4,58 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static char[] charArr;
-    static char[] operation= {'+', '-', '*', '/', '(', ')'};
-    static int[] level = {2, 2, 1, 1, 0, 0};
-    static StringBuilder sb = new StringBuilder();
-    static Stack<Character> stack = new Stack();
-    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-    
+
         String str = st.nextToken();
-        charArr = new char[str.length()];
+        char[] charArr = new char[str.length()];
+        Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < charArr.length; i++) {
-            charArr[i] = str.charAt(i);
-        }
+        for (int i = 0 ; i < str.length(); i++) charArr[i] = str.charAt(i);
 
+        StringBuilder sb = new StringBuilder();
         
-
         for (char c : charArr) {
-            if (!isOperation(c)) {
+ 
+            if (Character.isUpperCase(c)) {
                 sb.append(c);
-                continue;
-            }
+         
+            } else {
+                if (stack.isEmpty() || c == '(') {
+                    stack.push(c);
+                    continue;
+                } 
+                
+                if (c == ')') {
+                    while (true) {
+                        char top = stack.pop();
+                        if (top == '(') break;
 
-            boolean isInsert = shouldInsertStack(c);
-            
-            if (isInsert) stack.push(c);
-            else removeStack(c);
-            
+                        sb.append(top);
+                    }
+                } else {
+                    while (!stack.isEmpty()) {
+                        int top_level = getOprLevel(stack.peek());
+                        int cur_level = getOprLevel(c);
+
+                        if (top_level < cur_level) break;
+
+                        sb.append(stack.pop());
+                    }
+                    stack.push(c);
+                }
+            }
         }
 
-        while(!stack.isEmpty()) sb.append(stack.pop());
+        while (!stack.isEmpty()) sb.append(stack.pop());
 
         System.out.println(sb.toString());
-        
     }
 
-    static void removeStack(char opr) {
-
-        if (opr == ')') {
-            
-            while (true) {
-                char top = stack.pop();
-
-                if (top == '(') break;
-                sb.append(top);
-            }
-        } else{
-            while (!stack.isEmpty()) {
-                char top = stack.peek();
-                
-                if (top == '(' || getOperationLevel(top) > getOperationLevel(opr)) break;
-
-                sb.append(stack.pop());
-                
-            }
-
-            stack.push(opr);
-        }
-        
-    }
-    
-    static boolean shouldInsertStack(char opr) {
-        if (stack.size() == 0) return true;
-        if (opr == '(') return true;
-        if (opr == ')') return false;
-        
-        char top = stack.peek();
-        if (getOperationLevel(top) > getOperationLevel(opr)) return true;
-        return false;
-    }
-
-    static boolean isOperation(char c) {
-        for (char opr : operation) if (opr == c) return true;
-        return false;
-    }
-
-    static int getOperationLevel(char opr) {
-        for (int i = 0; i < operation.length; i++) {
-            if (operation[i] == opr) return level[i];
-        }
-        return -1;
+    static int getOprLevel (char c) {
+        if (c == '-' || c == '+') return 1;
+        else if(c == '*' || c == '/') return 2;
+        else return 0;
     }
 }
