da6f8f268c13475efef64b06feb3f23d67c45013
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-05-24 17:21:30 +0900

    [Gold V] Title: 블랙 프라이데이, Time: 408 ms, Memory: 206728 KB -BaekjoonHub

--- 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.java
+++ 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.java
@@ -4,56 +4,65 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int N, C;
-    static int[] arr;    
+    static class Info {
+        int value;
+        int idx = 0;
 
+        Info(int value, int i) {
+            this.value = value;
+            this.idx = i;
+        }
+    }
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
-        arr = new int[N];
-        HashMap<Integer, HashMap<Integer, Boolean>> memo = new HashMap<>();
+        int[] arr = new int[N];
+        HashMap<Integer, ArrayList<Info>> infoDict = new HashMap<>();
         
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
 
-            if (memo.containsKey(arr[i])) {
-                HashMap<Integer, Boolean> memo_tmp = memo.get(arr[i]);
-                memo_tmp.put(i, true);
-                
+            arr[i] = num;
+            
+            if (!infoDict.containsKey(num)) {
+                ArrayList<Info> list = new ArrayList<>();
+                list.add(new Info(num, i));
+                infoDict.put(num, list);
             } else {
-                HashMap<Integer, Boolean> memo_tmp = new HashMap<>();
-                memo_tmp.put(i, true);
-                memo.put(arr[i], memo_tmp);
+                infoDict.get(num).add(new Info(num, i));
             }
-            
-            
         }
 
         for (int i = 0; i < N; i++) {
             for (int j = i + 1; j < N; j++) {
                 int hap = arr[i] + arr[j];
+
                 if (arr[i] == C || arr[j] == C || hap == C) {
                     System.out.println(1);
                     return;
                 }
-    
-                int target = C - hap;
-                if (memo.containsKey(target)){
-                    HashMap<Integer, Boolean> memo_tmp = memo.get(target);
-                    if (memo_tmp.containsKey(i) && memo_tmp.size() == 1) continue;
-                    if (memo_tmp.containsKey(j) && memo_tmp.size() == 1) continue;
-                    if (i == j && memo_tmp.containsKey(i) && memo_tmp.size() == 2) continue;
+                 int num = C - hap;
+                if (!infoDict.containsKey(num)) continue; 
+
+                ArrayList<Info> list = infoDict.get(num);
+
+                for (Info info : list) {
+                    if (info.idx == i || info.idx == j) continue;
+
                     System.out.println(1);
                     return;
-                }            
-
+                }
             }
         }
+
+
+
         System.out.println(0);
     }
 }
