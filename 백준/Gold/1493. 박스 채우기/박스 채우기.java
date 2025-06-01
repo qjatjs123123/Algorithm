d14755732feaabbd658f20304a9b39633e1ddfa2
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-06-01 14:44:56 +0900

    [Gold II] Title: 박스 채우기, Time: 844 ms, Memory: 70280 KB -BaekjoonHub

--- 백준/Gold/1493. 박스 채우기/박스 채우기.java
+++ 백준/Gold/1493. 박스 채우기/박스 채우기.java
@@ -4,71 +4,68 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static long length, width, height;
+    static int length, width, height;
     static int N;
-    static boolean state = false;
-    static int[][] cubeArr;
-    static long total = 0;
-    static int answer = 0;
+    static int cubeArr[][];
+    static long answer;
+    static boolean state;
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        length = Long.parseLong(st.nextToken());
-        width = Long.parseLong(st.nextToken());
-        height = Long.parseLong(st.nextToken());
+        length = Integer.parseInt(st.nextToken());
+        width = Integer.parseInt(st.nextToken());
+        height = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
+
         cubeArr = new int[N][2];
-        
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int parent = Integer.parseInt(st.nextToken());
+            int pow = Integer.parseInt(st.nextToken());
             int count = Integer.parseInt(st.nextToken());
 
-            cubeArr[i][0] = parent;
+            cubeArr[i][0] = pow;
             cubeArr[i][1] = count;
         }
-        
-        Arrays.sort(cubeArr, (a, b) -> a[0] - b[0]);
-        
+
+        Arrays.sort(cubeArr, (a, b) -> b[0] - a[0]);
+
         recursion(length, width, height);
-        
-        if (!state) System.out.println(-1);
-        else System.out.println(answer);
+        if (state)
+            System.out.println(answer);
+        else System.out.println(-1);
     }
 
-    static void recursion(long length, long width, long height) {
-        if (length <= 0 || width <= 0 || height <= 0) return ;
-        
-        long min_num = Math.min(length, width);
-        min_num = Math.min(min_num, height);
+    static void recursion(int length, int width, int height) {
+        if (length == 0 || width == 0 || height == 0) return;
 
-        int pow_num = 0;
-        state = false;
-        for (int k = N - 1; k >= 0; k--) {
-            int i = cubeArr[k][0];
-            if (cubeArr[i][1] == 0) continue;
-            
-            pow_num = 1 << i;
-            if (min_num < pow_num) continue;
-            cubeArr[i][1]--;
+        int cube_len = 1;
+        boolean tmp = false;
+        for (int[] curArr : cubeArr) {
+            int pow = curArr[0];
+            int count = curArr[1];
+            cube_len = 1 << pow;
+
+            if (count <= 0 || cube_len > length || cube_len > width ||
+               cube_len > height) continue;
+
             state = true;
+            curArr[1]--;
             answer++;
+            tmp = true;
             break;
         }
 
-        
-        if (state) {
-
-            recursion(length - pow_num, pow_num, pow_num);
-            recursion(length, width - pow_num, pow_num);
-            recursion(length, width, height - pow_num);
-        }else {
-            return;
+        if (tmp) {
+            recursion(length - cube_len, cube_len, cube_len);
+            recursion(length, width - cube_len, cube_len);
+            recursion(length, width, height - cube_len);
+        } else {
+            state = false;
         }
     }
 }
