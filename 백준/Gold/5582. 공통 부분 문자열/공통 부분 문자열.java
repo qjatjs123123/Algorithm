402e8a8a94c4eaafc83837f862579449ba559584
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-06-02 14:15:10 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 192 ms, Memory: 74780 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -4,43 +4,32 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static char[] arr1;
-    static char[] arr2;
-    static int[][] dp;
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         String str1 = st.nextToken();
-        arr1 = new char[str1.length()];
-        for (int col = 0; col < str1.length(); col++) {
-            arr1[col] = str1.charAt(col);
-        }
+        char[] charArr1 = new char[str1.length()];
+        for (int i = 0; i < str1.length(); i++) charArr1[i] = str1.charAt(i);
 
         st = new StringTokenizer(br.readLine());
-        
         String str2 = st.nextToken();
-        arr2 = new char[str2.length()];
-        for (int col = 0; col < str2.length(); col++) {
-            arr2[col] = str2.charAt(col);
-        }
+        char[] charArr2 = new char[str2.length()];
+        for (int i = 0; i < str2.length(); i++) charArr2[i] = str2.charAt(i);
 
-        dp = new int[str1.length() + 1][str2.length() + 1];
-        int ans = 0 ;
-        for (int row = 0; row < str1.length(); row++) {
-            for (int col = 0; col < str2.length(); col++) {            
-                if (arr1[row] == arr2[col]) {
-                    dp[row + 1][col + 1] = dp[row][col] + 1;
-                    ans = Math.max(ans, dp[row + 1][col + 1]);
-                }
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
 
-                
+        int answer = 0;
+        for (int row = 1; row < str1.length() + 1; row++) {
+            for (int col = 1; col < str2.length() + 1; col++) {
+                if (charArr1[row - 1] == charArr2[col - 1]) dp[row][col] = dp[row - 1][col - 1] + 1;
+                else dp[row][col] = 0;
+
+                answer = Math.max(answer, dp[row][col]);
             }
         }
 
-        System.out.println(ans);
+        System.out.println(answer);
+        
     }
-
-
 }
