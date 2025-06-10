13b7d638bc958bb37550a10ba2596456c71615b4
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-06-10 15:21:25 +0900

    [Gold I] Title: 트리의 순회, Time: 1592 ms, Memory: 51976 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.java
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -4,7 +4,6 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int N;
     static int[] inorder;
     static int[] postorder;
     static StringBuilder sb = new StringBuilder();
@@ -13,39 +12,39 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-
+        int N = Integer.parseInt(st.nextToken());
         inorder = new int[N];
         postorder = new int[N];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            inorder[i] = Integer.parseInt(st.nextToken());
+        }
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) inorder[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            postorder[i] = Integer.parseInt(st.nextToken());
+        }
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) postorder[i] = Integer.parseInt(st.nextToken());
-
-        preorder(0, N, 0, N);
+        recursion(0, N - 1, 0, N - 1);
         System.out.println(sb.toString());
     }
 
-    static void preorder(int in_left, int in_right, int post_left, int post_right) {
-        if (in_right <= in_left) return;
-        
-
-
-        int root = postorder[post_right - 1];
+    static void recursion(int in_left, int in_right, int post_left, int post_right) {
+        if (in_left > in_right || post_left > post_right) return;
+        int root = postorder[post_right];
         int root_idx = 0;
-        
-        for (int i = in_left; i < in_right; i++) {
+
+        for (int i = in_left; i <= in_right; i++) {
             if (inorder[i] == root) {
                 root_idx = i;
                 break;
             }
         }
 
-        int leftCnt = root_idx - in_left;
-        sb.append(root + " ");
-        preorder(in_left, root_idx, post_left, post_left + leftCnt);
-        preorder(root_idx + 1, in_right, post_left + leftCnt, post_right - 1);
+        int len = root_idx - in_left;
+        sb.append(root).append(" ");
+        recursion(in_left, root_idx - 1, post_left, post_left + len - 1);
+        recursion(root_idx + 1, in_right, post_left + len, post_right - 1);
     }
 }
