66ff27272af6dea5f04d87ce65e79360e213b566
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-06-24 17:38:28 +0900

    [Gold III] Title: 공주님의 정원, Time: 476 ms, Memory: 54072 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
@@ -1,67 +1,77 @@
-
 import java.util.*;
-import java.util.stream.Collectors;
+import java.lang.*;
 import java.io.*;
 
-public class Main
-{	
-	private static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws Exception{
-		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		
-		ArrayList<int[]> array = new ArrayList<>();
-		
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			int d = Integer.parseInt(st.nextToken());
-			
-			array.add(new int[] {a*100 + b, c*100 + d});
-		}
-		
-		Collections.sort(array, (a, b) -> {
-			if (a[0] == b[0]) return a[1] - b[1];
-			return a[0] - b[0];
-		});
-		int target = 301;
-		int ans = 0;
-		boolean flg = false;
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		for (int i = 0; i < n; i++) {
-			int[] flower = array.get(i);
-			int start = flower[0];
-			int end = flower[1];
-			if (start <= target) {
-				pq.add(-end);
-			}else {
-				if (pq.size() == 0) {
-					ans = 0;
-					break;
-				} else {
-					int MAX_NUM = -pq.poll();
-					ans += 1;
-					target = MAX_NUM;
-					if (MAX_NUM >= 1131) {
-						flg = true;
-						break;
-					}
-					pq.clear();
-					i--;
-				}
-			}
-		}
-		if (!flg && pq.size() > 0) {
-			int MAX_NUM = -pq.poll();
-			if (MAX_NUM >= 1131) ans += 1;
-			else ans = 0;
-		}
-		System.out.println(ans);
-		//for (int[] a : array) System.out.println(Arrays.toString(a));
-	}
+// The main method must be in a class named "Main".
+class Main {
+    static int N;
+    static int[][] graph;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        graph = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int sMonth = Integer.parseInt(st.nextToken());
+            int sDay = Integer.parseInt(st.nextToken());
+            int eMonth = Integer.parseInt(st.nextToken());
+            int eDay = Integer.parseInt(st.nextToken());
+
+            graph[i][0] = (sMonth * 100) + sDay;
+            graph[i][1] = (eMonth * 100) + eDay;
+        }
+
+        Arrays.sort(graph, (a, b) -> a[0] - b[0]);
+
+        int start = 0;
+        int target = 301;
+        int cnt = 0;
+        while (start < N) {
+            int new_end = binary_search(target) - 1;
+
+            if (new_end < 0 || new_end == N) break;
+            if (graph[new_end][0] <= target && graph[new_end][1] >= target) {
+
+                int tmp = 0;
+                for (int i = start; i <= new_end; i++) {
+                    tmp = Math.max(tmp, graph[i][1]);
+                }
+    
+                target = tmp;
+                start = new_end + 1;
+                cnt++;
+            } else {
+                break;
+            }
+
+            if (target > 1130) {
+                System.out.println(cnt);
+                return;
+            }
+
+            // System.out.println(target);
+        }
+
+        System.out.println(0);
+    }
+
+    static int binary_search(int target) {
+        int start = 0;
+        int end = N - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int sMid = graph[mid][0];
+
+            if (sMid <= target) start = mid + 1;
+            else end = mid - 1;
+
+        }
+
+        return start;
+    }
 }
