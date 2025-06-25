e0d37346dca8d0bd775291bf5a2f9ceb1bbe144d
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-06-25 16:21:45 +0900

    [Gold III] Title: 세 용액, Time: 1060 ms, Memory: 292712 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -1,65 +1,69 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	static StringBuilder sb = new StringBuilder();
-	static int N;
-	static int[] graph;
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		graph = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) graph[i] = Integer.parseInt(st.nextToken());
-		
-		Arrays.sort(graph);
-		
-		long max_num = Long.MAX_VALUE;
-		int one = 0;
-		int two = 0;
-		int three = 0;
+// The main method must be in a class named "Main".
+class Main {
+    static int N;
+    static int[] arr;
+    static int[][] graph;
+    
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		for (int i = 0; i < N - 2; i++) {
-			int start = i + 1;
-			int end = N - 1;
-			int target = -graph[i];
-			boolean isStart= false;
-			boolean isEnd= false;
-			long cur_max = Long.MAX_VALUE;
-			while (start < end) {
-				long hap = graph[start] + graph[end];
-				long diff = Math.abs(target - hap);
-				
-				if (max_num >= diff) {
-					max_num = diff;
-					one = graph[i];
-					two = graph[start];
-					three = graph[end];
-				}
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
 
 
-				if (hap > target) {
-					end--;
-					isStart = true;
-				}
-				else if (hap < target) {
-					start++;
-					isStart = false;
-				}
-				else {
-					break;
-				}
-			}
+        
+        long min_num = Long.MAX_VALUE;
+        int[] answer = new int[3];
 
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                int pos[] = new int[3];
+                pos[0] = arr[i] + arr[j];
+                pos[1] = i;
+                pos[2] = j;
 
-		}
-		
-		System.out.println(one + " " + two + " " + three);
-	}
-	
+                int start = 0;
+                int end = N - 1;
+                int target = pos[0] * -1;
+    
+                
+                while (start <= end) {
+                    int mid = (start + end) / 2;
+    
+                    if (arr[mid] <= target) start = mid + 1;
+                    else end = mid - 1;
+                }
+    
+                int s = Math.max(0, start - 2);
+                int e = Math.min(start + 2, N - 1);
+                for (int k = s; k <= e; k++) {
+                    if (k == pos[1] || k == pos[2]) continue;
+                    
+                    long absNum = Math.abs((long)pos[0] + (long)arr[k]);
+                    if (min_num > absNum) {
+                        min_num = absNum;
+                        answer[0] = arr[pos[1]];
+                        answer[1] = arr[pos[2]];
+                        answer[2] = arr[k];
+                    }
+                    
+                }
+            }
+        }
+
+        Arrays.sort(answer);
+
+        System.out.println(answer[0] + " " + answer[1] + " " + answer[2]);
+    }
 }
