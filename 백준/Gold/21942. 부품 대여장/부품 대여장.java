89b3158345833a6fd52ff54216a00bcf3903784e
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-07-02 19:29:36 +0900

    [Gold II] Title: 부품 대여장, Time: 916 ms, Memory: 136484 KB -BaekjoonHub

--- 백준/Gold/21942. 부품 대여장/부품 대여장.java
+++ 백준/Gold/21942. 부품 대여장/부품 대여장.java
@@ -4,110 +4,94 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int N, DDD, hh, mm, F;
-    static int[] MONTHS = new int[] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    static int[] prefix;
-    static int ONE_DAY_MINUTE = 1440;
-    static HashMap<String, String[]> dict = new HashMap<>();
-    static HashMap<String, Long> fee = new HashMap<>();
-    static int saveTime = 0;
+    static int N, borrowTime, F;
+    static int[] prefix = new int[12];
+    static int[] months = new int[] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    static HashMap<String, Info> info_dict = new HashMap<>();
+    static HashMap<String, Long> money_dict = new HashMap<>();
+    
+    static class Info {
+        String name;
+        int minutes;
+
+        Info(String name, int minutes) {
+            this.name = name;
+            this.minutes = minutes;
+        }
+    }
+
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        for (int i = 1; i <= 11; i++) prefix[i] = prefix[i - 1] + months[i - 1];
+
         N = Integer.parseInt(st.nextToken());
-        prefix = new int[13];
-
-        for (int i = 1; i <= 12; i++) prefix[i] = prefix[i - 1] + MONTHS[i - 1];
-
-        String data = st.nextToken();
-        
-        DDD = Integer.parseInt(data.split("/")[0]);
-        hh = Integer.parseInt(data.split("/")[1].split(":")[0]);
-        mm = Integer.parseInt(data.split("/")[1].split(":")[1]);
-        saveTime += ONE_DAY_MINUTE * DDD + hh * 60 + mm;
-        
+        String[] tmp = st.nextToken().split("/");
+        borrowTime = (Integer.parseInt(tmp[0]) * 1440) + getMinuteByTime(tmp[1]);
         F = Integer.parseInt(st.nextToken());
 
-        for (int row = 0; row < N; row++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
 
-            String dateStr = st.nextToken();
-            String timeStr = st.nextToken();
-            String itemStr = st.nextToken();
-            String nameStr = st.nextToken();
+            String date = st.nextToken();
+            String time = st.nextToken();
+            String type = st.nextToken();
+            String name = st.nextToken();
+            String key = type + "-" + name;
+            int cur_minutes = getMinuteByDate(date) + getMinuteByTime(time);
 
-            int totalMinute = parseDateToMinute(dateStr) + parseTimeToMinute(timeStr);
-
-            if (!isReturn(itemStr, nameStr)) itemBorrow(itemStr, nameStr, totalMinute);
-            else itemReturn(itemStr, nameStr, totalMinute);
-        
-        }
-
-        display();
-    }
-
-    static void display() {
-        ArrayList<String> list = new ArrayList<>();
-        for(String key : fee.keySet()) {
-            list.add(key);
-        }
-
-        Collections.sort(list);
-
-        StringBuilder sb = new StringBuilder();
-        boolean isflg = false;
-        for (String key : list) {
-            isflg = true;
-            sb.append(key).append(" ").append(fee.get(key) * F);
-            sb.append("\n");
-        }
-        
-        if(!isflg) System.out.println(-1);
-        else System.out.println(sb.toString());
-    }
-    
-    static void itemReturn(String itemStr, String nameStr, int returnMinute) {
-        String[] data = dict.get(itemStr + nameStr);
-        String dataName = data[0];
-        int borrowMinute = Integer.parseInt(data[1]);
-
-        long timeGap = returnMinute - borrowMinute - saveTime;
-
-        if (timeGap > 0) {
-            if (fee.containsKey(dataName)) {
-                long cur_fee = fee.get(dataName);
-                fee.put(dataName, cur_fee + timeGap);
+            
+            if (!info_dict.containsKey(key)) {
+                info_dict.put(key, new Info(name, cur_minutes ));
             } else {
-                fee.put(dataName, timeGap);
+                Info prev_info = info_dict.get(key);
+                
+                int prev_minutes = prev_info.minutes;
+                long cur_money = ((cur_minutes - prev_minutes) - borrowTime) * F;
+
+                if (cur_money > 0) {
+                    if (money_dict.containsKey(name)) money_dict.put(name, cur_money + money_dict.get(name));
+                    else {
+                        money_dict.put(name, cur_money);
+                    }
+                } 
+                
+                info_dict.remove(key);
             }
         }
 
-        dict.remove(itemStr + nameStr);
-    }
-    
-    static void itemBorrow(String itemStr, String nameStr, int totalMinute) {
-        String[] data = new String[] {nameStr, Integer.toString(totalMinute)};
-        dict.put(itemStr + nameStr, data);
-    }
-    
-    static boolean isReturn (String itemStr, String nameStr) {
-        if (dict.containsKey(itemStr + nameStr)) return true;
-        return false;
-    }
-    
-    static int parseDateToMinute(String dateStr) {
-        int year = Integer.parseInt(dateStr.split("-")[0]);
-        int month = Integer.parseInt(dateStr.split("-")[1]);
-        int day = Integer.parseInt(dateStr.split("-")[2]);
+        if (money_dict.size() == 0) System.out.println(-1);
+        else {
+            ArrayList<String> list = new ArrayList<>();
 
-        return (prefix[month - 1] + (day - 1)) * ONE_DAY_MINUTE;
+            
+            for (String name : money_dict.keySet()) list.add(name);
+
+            Collections.sort(list);
+            StringBuilder sb = new StringBuilder();
+
+            for (String name : list) sb.append(name).append(" ").append(money_dict.get(name)).append("\n");
+
+            System.out.println(sb.toString());
+        }
+        
     }
 
-    static int parseTimeToMinute(String timeStr) {
-        int hour = Integer.parseInt(timeStr.split(":")[0]);
-        int minute = Integer.parseInt(timeStr.split(":")[1]);
+    static int getMinuteByDate(String date) {
+        String[] tmp = date.split("-");
+        int year = Integer.parseInt(tmp[0]);
+        int month = Integer.parseInt(tmp[1]);
+        int day = Integer.parseInt(tmp[2]);
+
+        return (prefix[month - 1] + day) * 1440;
+    }
+    
+    static int getMinuteByTime(String time) {
+        String[] tmp = time.split(":");
+        int hour = Integer.parseInt(tmp[0]);
+        int minute = Integer.parseInt(tmp[1]);
 
         return hour * 60 + minute;
     }
