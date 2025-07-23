4491c696b5c1458ea12d6ab13f2af1106ec9dd66
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-07-23 19:57:22 +0900

    [Gold III] Title: 줄 세우기, Time: 384 ms, Memory: 45492 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -4,51 +4,47 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    static int N, M;
+    static ArrayList<Integer>[] graph;
+    static int[] countArr;
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N, M;
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        int[] countArr = new int[N + 1];
-        ArrayList<Integer>[] graph = new ArrayList[N + 1];
+        graph = new ArrayList[N + 1];
+        countArr = new int[N + 1];
 
-        for (int i = 0; i <= N; i++) graph[i] = new ArrayList<>();
-
+        for (int i = 0; i < N + 1; i++) graph[i] = new ArrayList<>();
+        
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
 
-            graph[from].add(to);
-            
-            countArr[to]++;
+            countArr[B]++;
+            graph[A].add(B);
         }
 
         Deque<Integer> deque = new ArrayDeque<>();
-        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= N; i++) {
-            if (countArr[i] == 0) {
-                deque.add(i);
-
-                sb.append(i + " ");
-            }
+            if (countArr[i] == 0) deque.add(i);
         }
 
-        
-        
+        StringBuilder sb = new StringBuilder();
         while (!deque.isEmpty()) {
-            int no = deque.pollFirst();
-            for (int new_no : graph[no]) {
+            int num = deque.pollFirst();
 
-                countArr[new_no]--;
-                if (countArr[new_no] == 0) {
-                    deque.add(new_no);
-                    sb.append(new_no + " ");
-                }
+            sb.append(num).append(" ");
+            ArrayList<Integer> list = graph[num];
+            for (int new_num : list) {
+                countArr[new_num]--;
+
+                if (countArr[new_num] == 0) deque.add(new_num);
             }
         }
 
