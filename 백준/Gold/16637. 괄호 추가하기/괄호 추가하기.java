84750674fd4650e3fea741ed5f0ee832caddef9f
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-08-01 21:55:52 +0900

    [Gold III] Title: 괄호 추가하기, Time: 72 ms, Memory: 12008 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -5,127 +5,74 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
     static int N;
-    static String[] numArr;
-    static String[] oprArr;
-    static Stack<String> stack = new Stack<>();
-    static int result = Integer.MIN_VALUE;
+    static char[] arr;
+    static boolean[] visited;
+    static int answer = Integer.MIN_VALUE;
     
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
-        numArr = new String[N / 2 + 1];
-        oprArr = new String[N / 2];
-        
+        arr = new char[N];
+        visited = new boolean[N];
+
         st = new StringTokenizer(br.readLine());
         String str = st.nextToken();
-        for (int i = 0; i < N; i++) {
-            int idx = i / 2;
-            char c = str.charAt(i);
-            
-            if (i % 2 == 0) {
-                numArr[idx] = Character.toString(c);
-            } else {
-                oprArr[idx] = Character.toString(c);
-            }
-        }
-
-        dfs(0);
-
-        System.out.println(result);
+        for (int i = 0; i < N; i++) arr[i] = str.charAt(i);
+        
+        backtracking(0, false);
+        System.out.println(answer);
     }
 
-    static void dfs(int idx) {
-        int len = numArr.length;
-         if(idx >= len) {
-             calculate();
-             return;
-         }   
+    static int calculate(int n1, String opr, int n2) {
 
-        
-        // 괄호
-        if (idx < len - 1) {
-
-            int count = 5;
-            stack.push("(");
-            stack.push(numArr[idx]);
-            stack.push(oprArr[idx]);
-            stack.push(numArr[idx + 1]);
-            stack.push(")");
-
-            if (idx < len - 2) {
-                stack.push(oprArr[idx + 1]);
-                count++;
-            }
-
-            dfs(idx + 2);
-            for (int j = 0; j < count; j++) stack.pop();
-        }
-
-        int count1 = 1;
-        stack.push(numArr[idx]);
-
-        if (idx < len - 1) {
-            stack.push(oprArr[idx]);
-            count1++;
-        }
-        
-
-        dfs(idx + 1);
-        for (int j = 0; j < count1; j++) stack.pop();
-        
+        if (opr.equals("+")) return n1 + n2;
+        else if (opr.equals("-")) return n1 - n2;
+        else return n1 * n2;
     }
+    
+    static void backtracking(int depth, boolean prev) {
+        if (depth == N / 2) {
 
-    static int calculate() {
-        Stack<String> new_stack = copyStack();
+            Deque<String> deque = new ArrayDeque<>();
+            for (int i = 0; i < N; i++) {
+                if (visited[i]) {
+                    int num1 = Integer.parseInt(deque.pollLast());
+                    String opr = Character.toString(arr[i]);
+                    int num2 = arr[++i] - '0';
 
-        Stack<String> tmp_stack = new Stack<>();
+                    int result = calculate(num1, opr, num2);
+                    deque.add(Integer.toString(result));
+                } else {
 
-        
-        while (!new_stack.isEmpty()) {
-            String top = new_stack.pop();
-
-            if (top.equals(")")) {
-                Stack<String> tmp = new Stack<>();
-
-                while(!new_stack.peek().equals("(")) {
-                    tmp.push(new_stack.pop());
+                    deque.add(Character.toString(arr[i]));
                 }
-
-                new_stack.pop();
-
-                int total = Integer.parseInt(tmp.pop());
-                String opr = tmp.pop();
-                
-                if (opr.equals("+")) total += Integer.parseInt(tmp.pop());
-                else if(opr.equals("-")) total -= Integer.parseInt(tmp.pop());
-                else total *= Integer.parseInt(tmp.pop());
-
-                tmp_stack.push(Integer.toString(total));
-            } else {
-                tmp_stack.push(top);
             }
+
+            Stack<String> stack = new Stack<>();
+            while (!deque.isEmpty()) stack.push(deque.pollLast());
+
+            int total = 0;
+            while (stack.size() != 1 && !stack.isEmpty()) {
+                int num1 = Integer.parseInt(stack.pop());
+                String opr = stack.pop();
+                int num2 = Integer.parseInt(stack.pop());
+
+                int result = calculate(num1, opr, num2);
+                stack.push(Integer.toString(result));
+            }
+
+            answer = Math.max(answer, Integer.parseInt(stack.pop()));
+            return;
         }
 
-        int answer = Integer.parseInt(tmp_stack.pop());
+        if (prev) backtracking(depth + 1, false);
+        else {
+            visited[2 * depth + 1] = true;
+            backtracking(depth + 1, true);
+            visited[2 * depth + 1] = false;
 
-        while (!tmp_stack.isEmpty()) {
-            String opr = tmp_stack.pop();
-
-            if (opr.equals("+")) answer += Integer.parseInt(tmp_stack.pop());
-            else if(opr.equals("-")) answer -= Integer.parseInt(tmp_stack.pop());
-            else answer *= Integer.parseInt(tmp_stack.pop());
+            backtracking(depth + 1, false);
         }
-
-        result = Math.max(result, answer);
-        return answer;
-    }
-
-    static Stack<String> copyStack() {
-        Stack<String> new_stack = new Stack<>();
-
-        for (String ss : stack) new_stack.push(ss);
-        return new_stack;
     }
 }
