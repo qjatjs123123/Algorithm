dac59741a6de640c4aa4197bd004e4d8de3c41da
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-08-07 22:06:18 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 256 ms, Memory: 13960 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -1,39 +1,50 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		
-		int[][] graph = new int[N][2];
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int row = Integer.parseInt(st.nextToken());
-			int col = Integer.parseInt(st.nextToken());
-			
-			graph[i][0] = row;
-			graph[i][1] = col;
-		}
-		int[][] dp = new int[N][N];
-		
-		for (int gap = 1; gap < N; gap++) {
-			int max = N - gap;
-			for (int left = 0; left < max; left++) {
-				int right = left + gap;
-				dp[left][right] = Integer.MAX_VALUE;
-				
-				for (int k = left; k < right; k++) {
-					dp[left][right] = Math.min(dp[left][right], dp[left][k] + dp[k + 1][right] + graph[left][0] * graph[k][1]*graph[right][1]);
-				}
-			}
-		}
-		
-		System.out.println(dp[0][N-1]);
-	}
+// The main method must be in a class named "Main".
+class Main {
+    static int N;
+    static int[][] graph;
+    static int[][] dp;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        graph = new int[N][2];
+        dp = new int[N][N];
+        
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int left = Integer.parseInt(st.nextToken());
+            int right = Integer.parseInt(st.nextToken());
+
+            graph[i][0] = left;
+            graph[i][1] = right;
+            
+        }
+
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col < N; col++) dp[row][col] = -1;
+        }
+
+        System.out.println(dfs(0, N - 1));
+    }
+
+    static int dfs(int left, int right) {
+        if (left == right) return 0;
+        if (dp[left][right] != -1) return dp[left][right]; 
+        
+        int total = Integer.MAX_VALUE;
+        for (int i = left; i < right; i++) {
+            int tmp = graph[left][0] * graph[i][1] * graph[right][1];
+            total = Math.min(total, dfs(left, i) + dfs(i + 1, right) + tmp);
+        } 
+
+        dp[left][right] = total;
+        return total;
+    }
 }
