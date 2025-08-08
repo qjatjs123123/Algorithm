da8d74004511fd4b5a9f53d0a69c3421644788ff
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-08-08 18:37:11 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 552 ms, Memory: 57200 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -1,115 +1,102 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());	
-		int cityCnt = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		int busCnt = Integer.parseInt(st.nextToken());
-		
-		HashMap<Integer, ArrayList<Pos>> graph = new HashMap<>();
-		for (int i = 0; i < busCnt; i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int dist = Integer.parseInt(st.nextToken());
-			
-			if (!graph.containsKey(start)) {
-				ArrayList<Pos> tmp = new ArrayList<>();
-				tmp.add(new Pos(end, dist));
-				graph.put(start, tmp);
-			} else {
-				ArrayList<Pos> tmp = graph.get(start);
-				tmp.add(new Pos(end, dist));
-				graph.put(start, tmp);
-			}
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
-		
-		int[] distance = new int[cityCnt + 1];
-		int[] track = new int[cityCnt + 1];
+// The main method must be in a class named "Main".
+class Main {
+    static int N, M;
+    static ArrayList<Edge>[] graph;
+    static int[] parents;
+    static class Edge{
+        int no;
+        int dist;
 
-		
-		
-		for (int i = 0; i <= cityCnt; i++) {
-			distance[i] = Integer.MAX_VALUE;
-		}
-		distance[start] = 0;
-		
-		PriorityQueue<Pos> pq = new PriorityQueue<>();
-		Pos init_pos = new Pos(start, 0);
+        Edge(int no, int dist) {
+            this.no = no;
+            this.dist = dist;
+        }
+    }
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
 
-		pq.add(init_pos);
-		
-		while (!pq.isEmpty()) {
-			Pos cur_pos = pq.poll();
-			int cur_node = cur_pos.node;
-			int cur_dist = cur_pos.dist;
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
 
-			
-			if (distance[cur_node] < cur_dist) continue;
-			if (!graph.containsKey(cur_node)) continue;
-			
-			ArrayList<Pos> new_list = graph.get(cur_node);
+        graph = new ArrayList[N + 1];
+        parents = new int[N + 1];
 
-			for (Pos pos: new_list) {
-				if (distance[pos.node] > cur_dist + pos.dist) {
-					
-					distance[pos.node] = cur_dist + pos.dist;
-					
-					ArrayList<Integer> new_track = new ArrayList<>();
-					
-					Pos new_pos = new Pos(pos.node, cur_dist + pos.dist);
+        for (int i = 0; i <= N; i++) graph[i] = new ArrayList<>();
+        
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
 
-					pq.add(new_pos);
-					track[pos.node] = cur_node;
-					
-				}
-			}
-		}
-		
-		
-		System.out.println(distance[end]);
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int dist = Integer.parseInt(st.nextToken());
 
-		
-		Stack<Integer> stack = new Stack<>();
-		StringBuilder sb = new StringBuilder();
-		
-		int cur_node = end;
-		int cnt = 1;
-		while (cur_node != start) {
-			stack.push(cur_node);
-			cnt++;
-			cur_node = track[cur_node];
-		}
-		stack.push(start);
-		
-		while (!stack.isEmpty()) sb.append(stack.pop()).append(" ");
-		System.out.println(cnt);
-		System.out.println(sb.toString());
-	}
-	
-	static class Pos implements Comparable<Pos>{
-		int node, dist;
-		ArrayList<Integer> track = new ArrayList<>();
-		
-		Pos(int node, int dist) {
-			this.node = node;
-			this.dist = dist;
-		}
-		
-		@Override
-		public int compareTo(Pos o) {
-			return this.dist - o.dist; 
-		}
-	}
+            graph[from].add(new Edge(to, dist));
+            // graph[to].add(new Edge(from, dist));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        dijkstra(start, end);
+
+
+        
+    }
+
+    static void dijkstra(int start, int end) {
+        long[] distance = new long[N + 1];
+        Arrays.fill(distance, Long.MAX_VALUE);
+
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) -> Long.compare(a[0], b[0]));
+        pq.add(new long[] {0, start});
+        distance[start] = 0;
+
+        while (!pq.isEmpty()) {
+            long[] cur_arr = pq.poll();
+
+            long total = cur_arr[0];
+            int no = (int) cur_arr[1];
+
+            if (distance[no] < total) continue;
+
+            ArrayList<Edge> list = graph[no];
+            for (Edge edge: list) {
+                if (edge.dist + total >= distance[edge.no]) continue;
+
+                pq.add(new long[] {edge.dist + total, edge.no});
+                parents[edge.no] = no;
+                distance[edge.no] = edge.dist + total;
+            }
+        }
+
+        System.out.println(distance[end]);
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
+        int tmp = end;
+        int cnt = 0;
+        while (true) {
+            stack.push(tmp);
+            cnt++;
+            
+            if (tmp == start) {
+                break;
+            }
+            
+            tmp = parents[tmp];
+            
+        }
+
+        while(!stack.isEmpty()) sb.append(stack.pop()).append(" ");
+        System.out.println(cnt);
+        System.out.println(sb.toString());
+    }
 }
