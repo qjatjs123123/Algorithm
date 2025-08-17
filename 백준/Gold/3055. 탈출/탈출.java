c84cf0a476c5c5ab6ce41a008dc4655a7a769fee
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-08-17 15:55:06 +0900

    [Gold IV] Title: 탈출, Time: 68 ms, Memory: 11820 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -6,103 +6,115 @@ import java.io.*;
 class Main {
     static int R, C;
     static char[][] graph;
-    static boolean[][] visited;
-    static int[] dx = new int[] {0, 0, 1, -1};
+    static boolean[][] water_visited, biber_visited;
+    static Deque<int[]> water_deque, biber_deque;
     static int[] dy = new int[] {1, -1, 0, 0};
-    static Deque<Pos> waterQueue = new LinkedList<>();
-    static Deque<Pos> dociQueue = new LinkedList<>();
-    static boolean ans;
+    static int[] dx = new int[] {0, 0, 1, -1};
     
-    static class Pos{
-        int row, col;
-
-        Pos(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-    }
-
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-
         graph = new char[R][C];
-        visited = new boolean[R][C];
-        
+        water_visited = new boolean[R][C];
+        biber_visited = new boolean[R][C];
+        water_deque = new ArrayDeque<>();
+        biber_deque = new ArrayDeque<>();
+
         for (int row = 0; row < R; row++) {
             st = new StringTokenizer(br.readLine());
-            String rowData = st.nextToken();
+            String str = st.nextToken();
             
             for (int col = 0; col < C; col++) {
-                graph[row][col] = rowData.charAt(col);
+                graph[row][col] = str.charAt(col);
 
-                if (graph[row][col] == '*'){
-                    waterQueue.add(new Pos(row, col));
-                    visited[row][col] = true;
-                } 
+                if (graph[row][col] == '*') {
+                    water_deque.add(new int[] {row, col});
+                    water_visited[row][col] = true;
+                }
+
                 if (graph[row][col] == 'S') {
-                    dociQueue.add(new Pos(row, col));
-                    visited[row][col] = true;
+                    biber_deque.add(new int[] {row, col, 0});
+                    biber_visited[row][col] = true;
                 }
             }
         }
 
-        int time = 1;
+        int time = 0;
+        while (true) {
+            if (biber_deque.size() == 0) break;
 
-        while(true) {
-            bfs(waterQueue, false);
-            bfs(dociQueue, true);
+            spread_water();
+            boolean result = spread_biber();
 
-            if (ans) {
-                System.out.println(time);
-                break;
+            if (result) {
+                System.out.println(time + 1);
+                return;
             }
-            if (dociQueue.isEmpty()) {
-                System.out.println("KAKTUS");
-                break;
-            }
+
             time++;
         }
-        
+         
+        System.out.println("KAKTUS");
     }
 
-    static void bfs(Deque<Pos> queue, boolean isDoci) {
-        Deque<Pos> new_queue = new LinkedList<>();
-        
-        while(!queue.isEmpty()) {
-            Pos cur_pos = queue.pollFirst();
-    
-            
+    static boolean spread_biber() {
+        Deque<int[]> tmp = new ArrayDeque<>();
+
+        while (!biber_deque.isEmpty()) {
+            int[] cur_arr = biber_deque.pollFirst();
+            int cur_row = cur_arr[0];
+            int cur_col = cur_arr[1];
             
             for (int i = 0; i < 4; i++) {
-                int new_row = cur_pos.row + dy[i];
-                int new_col = cur_pos.col + dx[i];
-  
-                
-                if (new_row < 0 || new_row == R || new_col < 0 || new_col == C) continue;
-
-                if (isDoci && graph[new_row][new_col] == 'D') {
-                    ans = true;
-                    return;
-                }
-                if (!isDoci && graph[new_row][new_col] == 'D') continue;
-                
-                if (visited[new_row][new_col]) continue;
+                int new_row = cur_row + dy[i];
+                int new_col = cur_col + dx[i];
+        
+                if (isOutofBound(new_row, new_col)) continue;
+                if (water_visited[new_row][new_col] || biber_visited[new_row][new_col]) continue;
                 if (graph[new_row][new_col] == 'X') continue;
+                if (graph[new_row][new_col] == 'D') {
+                    return true;
+                }
 
-                visited[new_row][new_col] = true;
-                new_queue.add(new Pos(new_row, new_col));
-                
+                tmp.add(new int[] {new_row, new_col, cur_arr[2] + 1});
+                biber_visited[new_row][new_col] = true;
             }
-        }     
-        if (isDoci) {
-            dociQueue = new_queue;
-        } else{
-            waterQueue = new_queue;
         }
 
+        biber_deque = tmp;
+        return false;
+    }
+    
+    static void spread_water() {
+        Deque<int[]> tmp = new ArrayDeque<>();
+
+        while (!water_deque.isEmpty()) {
+            int[] cur_arr = water_deque.pollFirst();
+            int cur_row = cur_arr[0];
+            int cur_col = cur_arr[1];
+
+            for (int i = 0; i < 4; i++) {
+                int new_row = cur_row + dy[i];
+                int new_col = cur_col + dx[i];
+                
+                if (isOutofBound(new_row, new_col)) continue;
+                if (water_visited[new_row][new_col]) continue;
+                if (graph[new_row][new_col] == 'D' || graph[new_row][new_col] == 'X') continue;
+
+                tmp.add(new int[] {new_row, new_col});
+                water_visited[new_row][new_col] = true;
+            }
+            
+        }
+
+        water_deque = tmp;
+    }
+
+    static boolean isOutofBound(int row, int col) {
+        if (row < 0 || row == R || col < 0 || col == C) return true;
+        return false;
     }
 }
