9a7198eac084ac9eb1f941b4c55005d4a863b78a
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-08-17 14:58:26 +0900

    [Gold III] Title: 택배, Time: 608 ms, Memory: 35632 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.java
+++ 백준/Gold/1719. 택배/택배.java
@@ -2,73 +2,98 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+// The main method must be in a class named "Main".
 class Main {
-    static int n, m;
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    static int[][] weight;
-    static int[][] ans;
+    static int N, M;
+    static ArrayList<Edge>[] graph;
+    static int[][] dp;
+    static class Edge implements Comparable<Edge>{
+        int no, dist;
+
+        Edge(int no, int dist) {
+            this.no = no;
+            this.dist = dist;
+        }
+
+        public int compareTo(Edge edge) {
+            return Integer.compare(this.dist, edge.dist);
+        }
+    }
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        weight = new int[n + 1][n + 1];
-        ans = new int[n + 1][n + 1];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        for (int row = 0; row <= n; row++) {
-            ArrayList<Integer> new_list = new ArrayList<>();
-            graph.add(new_list);
-        }
-
-        for (int row = 0 ; row <= n; row++) {
-            for(int col = 0; col <= n; col++) {
-                if (row == col) weight[row][col] = 0;
-                else {
-                    if (weight[row][col] == 0) weight[row][col] = 1000*1000;
-                }
-            }
-        }
+        graph = new ArrayList[N + 1];
+        dp = new int[N + 1][N + 1];
         
-        for (int row = 0; row < m; row++) {
+        for (int i = 0; i <= N; i++) graph[i] = new ArrayList<>();
+
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
+            int dist = Integer.parseInt(st.nextToken());
 
-            ArrayList<Integer> cur_list = graph.get(from);
-            cur_list.add(to);
-            cur_list = graph.get(to);
-            cur_list.add(from);
-
-            weight[from][to] = Math.min(weight[from][to], w);
-            weight[to][from] = Math.min(weight[to][from], w);
-            ans[from][to] = to;
-            ans[to][from] = from;
+            graph[from].add(new Edge(to, dist));
+            graph[to].add(new Edge(from, dist));
         }
 
-        for (int mid = 1; mid <= n; mid++) {
-            for (int from = 1; from <= n; from++) {
-                for (int to = 1; to <= n; to++) {
-                    if (weight[from][to] > weight[from][mid] + weight[mid][to]) {
-                        weight[from][to] = weight[from][mid] + weight[mid][to];
-                        ans[from][to] = ans[from][mid];
-                    }
-                    
-                }
-            }
-        } 
-        StringBuffer sb = new StringBuffer();
-        for (int row = 1; row <= n; row++) {
-            for (int  col = 1; col <= n; col++) {
-                if (ans[row][col] == 0) sb.append("-");
-                else sb.append(ans[row][col]);
+        for (int start = 1; start <= N; start++) dijkstra(start);
+
+        StringBuilder sb = new StringBuilder();
+        for (int row = 1; row <= N; row++) {
+            for (int col = 1; col <= N; col++) {
+                if (row == col) sb.append("-");
+                else sb.append(getNextNode(row, col));
                 sb.append(" ");
             }
+
             sb.append("\n");
         }
 
         System.out.println(sb.toString());
+    }
+
+    static void dijkstra(int start) {
+        int[] distance = new int[N + 1];
+        for (int i = 0; i <= N; i++) distance[i] = Integer.MAX_VALUE;
+        distance[start] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        pq.add(new int[] {0, start});
+
+        while (!pq.isEmpty()) {
+            int[] cur_arr = pq.poll();
+            int cur_dist = cur_arr[0];
+            int cur_no = cur_arr[1];
+
+            if (distance[cur_no] < cur_dist) continue;
+
+            ArrayList<Edge> list = graph[cur_no];
+            for (Edge edge : list) {
+                if (distance[edge.no] <= cur_dist + edge.dist) continue;
+
+                distance[edge.no] = cur_dist + edge.dist;
+                pq.add(new int[] {cur_dist + edge.dist, edge.no});
+                dp[start][edge.no] = cur_no;
+            }
+        }
+    }
+
+    static int getNextNode(int start, int end) {
+        int answer = end;
+
+        
+        while (true) {
+            if (dp[start][answer] == start) break;
+
+            answer = dp[start][answer];
+        }
+
+        return answer;
     }
 }
