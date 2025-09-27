4a00d543e3ebfabc1cf34b718404da8e665badeb
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-09-27 14:05:45 +0900

    [Gold III] Title: 마라톤 2, Time: 196 ms, Memory: 13064 KB -BaekjoonHub

--- 백준/Gold/10653. 마라톤 2/마라톤 2.java
+++ 백준/Gold/10653. 마라톤 2/마라톤 2.java
@@ -5,8 +5,8 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
     static int N, K;
-    static int[][] graph;
     static int[][] dp;
+    static int[][] graph;
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,43 +14,46 @@ class Main {
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        graph = new int[N][2];
+
         dp = new int[N][K + 1];
+        graph = new int[N][2];
 
         for (int row = 0; row < N; row++) {
             for (int col = 0; col <= K; col++) dp[row][col] = -1;
         }
-        
-        for (int i = 0; i < N; i++) {
+
+        for (int row = 0; row < N; row++) {
             st = new StringTokenizer(br.readLine());
+
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            graph[i][0] = x;
-            graph[i][1] = y;
+            graph[row][0] = x;
+            graph[row][1] = y;
         }
 
         System.out.println(dfs(0, 0));
     }
 
     static int dfs(int from, int k) {
-        if (from >= N - 1) return 0;
+        if (from == N - 1) return 0;
         if (dp[from][k] != -1) return dp[from][k];
-        
-        int total = Integer.MAX_VALUE;
 
-        for (int cnt = 0; cnt <= K - k; cnt++) {
-            int to = from + cnt + 1;
+        int result = Integer.MAX_VALUE;
+        int i = 0;
+        for (int new_k = k; new_k <= K; new_k++) {
+            int to = from + i + 1;
 
-            if (to == N) break;
-            total = Math.min(total, dfs(to, k + cnt) + manhatan(from, to));
+            if (to >= N) break;
+            result = Math.min(result, dfs(to, new_k) + manhantan(from, to));
+            i++;
         }
 
-        dp[from][k] = total;
-        return total;
+        dp[from][k] = result;
+        return result;
     }
-    
-    static int manhatan(int from, int to) {
+
+    static int manhantan(int from, int to) {
         return Math.abs(graph[from][0] - graph[to][0]) + Math.abs(graph[from][1] - graph[to][1]);
     }
 }
