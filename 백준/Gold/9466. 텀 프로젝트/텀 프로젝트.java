181fa85f69f69a8b9900912207b05f9d7b9d2269
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-09-27 18:17:49 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2804 ms, Memory: 317196 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -1,74 +1,72 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	static int T, N;
-	static int[] arr;
-	static HashMap<Integer, Boolean> visited;
-	static int[] dp;
-	static Stack<Integer> stack;
-	static int[] check;
-	public static void main(String[] args) throws IOException {
-		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		T = Integer.parseInt(st.nextToken());
-		
-		for (int t = 1; t <= T; t++) {
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			arr = new int[N + 1];
-			dp = new int[N + 1];
-			check = new int[N+1];
-			st = new StringTokenizer(br.readLine());
-			for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(st.nextToken());
-			
-			int ans = 0;
-			for (int i = 1; i <= N; i++) {
-				visited = new HashMap<>();
-				stack = new Stack<>();
-				stack.push(i);
-				
-				visited.put(i, true);
-				if (dp[i] == 1) {
-					ans += 1;
-				}
-				else {
-					if (dp[i] == -1) continue;
-					dfs(i, arr[i]);
-					if (dp[i] == 0) {
-						dp[i] = -1;
-					}else {
-						ans++;
-					}
-				}
-				
-			}
-			System.out.println(N - ans);
-			
-		}
-	}
-	
-	static int dfs(int start_node, int next_node) {
-		if (dp[next_node] == -1 || dp[next_node] == 1) return 0;
-		check[next_node]++;
-		if (visited.containsKey(next_node)) {
-			
-			dp[next_node] = 1;
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-			while (stack.peek() != next_node) {
-				dp[stack.pop()] = 1;
-			}
-			while (!stack.isEmpty()) {
-				dp[stack.pop()] = -1;
-			}
-			return 0;
-		}
-		
-		visited.put(next_node, true);
-		stack.push(next_node);
-		return dfs(start_node, arr[next_node]);
-	}
+        int T = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        
+        for (int tc = 0; tc < T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            
+            int[] parents = new int[n + 1];
+            boolean[] visited = new boolean[n + 1];
+            int result = 0;
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= n; i++) parents[i] = Integer.parseInt(st.nextToken());
+
+            for (int i = 1; i <= n; i++) {
+                if (visited[i]) continue;
+
+                Stack<Integer> stack = new Stack<>();
+                HashMap<Integer, Boolean> map = new HashMap<>();
+
+                stack.push(i);
+                map.put(i, true);
+
+                int new_node = i;
+                int point = -1;
+                boolean flg = false;
+                
+                while (true) {
+                    new_node = parents[new_node];
+
+                    if (visited[new_node]) {
+                        flg = true;
+                        break;
+                    }
+                    if (map.containsKey(new_node)) {
+                        point = new_node;
+                        break;
+                    }
+        
+                    stack.push(new_node);
+                    map.put(new_node, true);
+                }
+
+                boolean isCycle = true;
+                
+                while (!stack.isEmpty()) {
+                    int top = stack.pop();
+
+                    if (isCycle && point != -1) result++;
+                    if (top == point) isCycle = false;
+
+                    visited[top] = true;
+                }
+
+            } 
+
+            sb.append(n - result).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
 }
-
