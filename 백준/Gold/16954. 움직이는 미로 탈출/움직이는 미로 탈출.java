f7678f9faf2b36ded46ff4330a086e752b76a6f3
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-09-27 19:27:43 +0900

    [Gold III] Title: 움직이는 미로 탈출, Time: 72 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
+++ 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
@@ -1,98 +1,92 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	static char[][] graph = new char[8][8];
-	static Deque<int[]> WALL_LIST = new LinkedList<>();
-	static Deque<int[]> CHARACTER_LIST = new LinkedList<>();
-	static int[][] direction = {
-			{1, 0},
-			{-1, 0},
-			{0, 1},
-			{0, -1},
-			{1, 1},
-			{1, -1},
-			{-1, 1},
-			{-1, -1},
-			{0, 0}
-	};
-	
-	public static void main(String[] args) throws IOException {		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		
-		for (int row = 0; row < 8; row++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String line = st.nextToken();
-			for (int col = 0; col < 8; col++) {
-				graph[row][col] = line.charAt(col);
-				
-				if (graph[row][col] == '#') WALL_LIST.add(new int[] {row, col});
-			}
-		}
-		CHARACTER_LIST.add(new int[] {7, 0});
-		
-		int ans = 0;
-		for (int time = 0; time < 9; time++) {
-			if (CHARACTER_LIST.isEmpty()) {
-				break;
-			}
-			if (WALL_LIST.isEmpty() && !CHARACTER_LIST.isEmpty()) {
-				ans = 1;
-				break;
-			}
-			
-			character_move();
-			wall_move();
-			
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
 
-		}
-		if (!CHARACTER_LIST.isEmpty()) ans = 1;
-		System.out.println(ans);
-	}
-	
-	static void character_move() {
-		Deque<int[]> NEW_LIST = new LinkedList<>();
-		while (!CHARACTER_LIST.isEmpty()) {
-			int[] character_pos = CHARACTER_LIST.pollFirst();
-			int character_row = character_pos[0];
-			int character_col = character_pos[1];
-			
-			if (graph[character_row][character_col] == '#') continue;
-			
-			for (int[] direct : direction) {
-				int new_row = character_row + direct[0];
-				int new_col = character_col + direct[1];
-				
-				if (new_row < 0 || new_row == 8 || new_col < 0 || new_col == 8) continue;
-				if (graph[new_row][new_col] == '#') continue;
-				
-				NEW_LIST.add(new int[] {new_row, new_col});
-			}
-		}
-		
-		CHARACTER_LIST = NEW_LIST;
-	}
+        char[][] graph = new char[8][8];
 
-	
-	static void wall_move() {
-		Deque<int[]> NEW_LIST = new LinkedList<>();
-		while (!WALL_LIST.isEmpty()) {
-			int[] wall_pos = WALL_LIST.pollLast();
-			int wall_row = wall_pos[0];
-			int wall_col = wall_pos[1];
-			
-			if (wall_row == 7) {
-				graph[wall_row][wall_col] = '.';
-				continue;
-			}
-			
-			graph[wall_row][wall_col] = '.';
-			graph[wall_row + 1][wall_col] = '#';
-			
-			NEW_LIST.addFirst(new int[] {wall_row + 1, wall_col});
-		}
-		WALL_LIST = NEW_LIST;
-	}
-	
+        ArrayList<int[]> wallList = new ArrayList<>();
+        for (int row = 0; row < 8; row++) {
+            st = new StringTokenizer(br.readLine());
+            String strData = st.nextToken();
+                
+            for (int col = 0; col < 8; col++) {
+                graph[row][col] = strData.charAt(col);
+
+                if (graph[row][col] == '#') wallList.add(new int[] {row, col});
+            }
+        }
+
+        graph[7][0] = 'X';
+        int[] dy = new int[] {0, 0, 0, 1, 1, 1, -1, -1, -1};
+        int[] dx = new int[] {-1, 0, 1, -1, 0, 1, -1, 0, 1};
+
+        while (true) {
+            Deque<int[]> deque = new ArrayDeque<>();
+
+            for (int row = 0; row < 8; row++) {
+                for (int col = 0; col < 8; col++) {
+                    if (graph[row][col] == 'X') deque.add(new int[] {row, col});
+                }
+            }
+
+            if (deque.isEmpty()) {
+                System.out.println(0);
+                return;
+            }
+
+            while(!deque.isEmpty()) {
+                int[] cur_arr = deque.pollFirst();
+
+                for (int i = 0; i < 9; i++) {
+                    int new_row = cur_arr[0] + dy[i];
+                    int new_col = cur_arr[1] + dx[i];
+
+                    if (isOutofBoundary(new_row, new_col) || graph[new_row][new_col] == '#') continue;
+                    if (new_row == 0 && new_col == 7) {
+                        System.out.println(1);
+                        return;
+                    }
+                    graph[new_row][new_col] = 'X';
+                }
+            }
+
+            for (int[] arr : wallList) {
+                if (isOutofBoundary(arr[0], arr[1])) continue;
+
+                graph[arr[0]][arr[1]] = '.';
+            }
+
+            for (int[] arr : wallList) {
+                arr[0]++;
+
+                if (isOutofBoundary(arr[0], arr[1])) continue;
+                graph[arr[0]][arr[1]] = '#';
+            }
+
+        }
+        
+    }
+
+    static boolean isOutofBoundary(int row, int col) {
+        if (row < 0 || row >= 8 || col < 0 || col >= 8) return true;
+        return false;
+    }
+    
+    static void display(char[][] graph) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) sb.append(graph[row][col]).append(" ");
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
 }
