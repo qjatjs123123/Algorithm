b0330af9a68fefd03bbde3ce59127860d2c4040a
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-09-27 15:14:26 +0900

    [Gold III] Title: 중량제한, Time: 412 ms, Memory: 60052 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -2,75 +2,72 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+// The main method must be in a class named "Main".
 class Main {
     static int N, M;
-    static ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
-    static int start_node, end_node;
+    static ArrayList<Edge>[] graph;
+    static int start, end;
+    static class Edge implements Comparable<Edge> {
+        int to, dist;
 
-    public static void main(String[] args) throws IOException {
+        Edge(int to, int dist) {
+            this.to = to;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Edge edge) {
+            return Integer.compare(edge.dist, this.dist);
+        }
+    }
+    
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
-        }
+        graph = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) graph[i] = new ArrayList<>();
 
-        for (int i = 0; i < M; i++) {
+        for (int row = 0; row < M; row++) {
             st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
-            graph.get(A).add(new int[]{B, C});
-            graph.get(B).add(new int[]{A, C});
+
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int dist = Integer.parseInt(st.nextToken());
+
+            graph[from].add(new Edge(to, dist));
+            graph[to].add(new Edge(from, dist));
         }
 
         st = new StringTokenizer(br.readLine());
-        start_node = Integer.parseInt(st.nextToken());
-        end_node = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
 
-        int start = 1;
-        int end = 1_000_000_000;
-        int result = 0;
+        int[] visited = new int[N + 1];
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
 
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (bfs(mid)) {
-                result = mid;
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        visited[start] = 0;
+        pq.add(new Edge(start, Integer.MAX_VALUE));
+
+        while (!pq.isEmpty()) {
+            Edge cur_edge = pq.poll();
+
+            if (cur_edge.to == end) {
+                System.out.println(cur_edge.dist);
+                return;
+            }
+            
+            ArrayList<Edge> list = graph[cur_edge.to];
+            for (Edge new_edge : list) {
+                int new_dist = Math.min(cur_edge.dist, new_edge.dist);
+
+                if (visited[new_edge.to] >= new_dist) continue;
+                pq.add(new Edge(new_edge.to, new_dist));
+                visited[new_edge.to] = new_dist;
             }
         }
-
-        System.out.println(result);
-    }
-
-    static boolean bfs(int weight) {
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[N + 1];
-
-        queue.add(start_node);
-        visited[start_node] = true;
-
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-
-            if (cur == end_node) return true;
-
-            for (int[] edge : graph.get(cur)) {
-                int next = edge[0];
-                int edgeWeight = edge[1];
-
-                if (!visited[next] && edgeWeight >= weight) {
-                    visited[next] = true;
-                    queue.add(next);
-                }
-            }
-        }
-
-        return false;
     }
 }
