291cf51b3cefee414b4d5332674c92131cd8fe29
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-09-28 15:42:42 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 344 ms, Memory: 40592 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -4,55 +4,53 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int N;
-    static int[][] graph;
-    static int[][] dp;
-    static int[] dy = new int[] {0, 0, 1, -1};
-    static int[] dx = new int[] {1, -1, 0, 0};
+    static int n;
+    static int[][] graph, dp;
+    static int[] dy = {1, -1, 0, 0};
+    static int[] dx = {0, 0, 1, -1};
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        graph = new int[N][N];
-        dp = new int[N][N];
+        n = Integer.parseInt(st.nextToken());
+        graph = new int[n][n];
+        dp = new int[n][n];
 
-        for (int row = 0; row < N; row++) {
+        for (int row = 0; row < n; row++) {
             st = new StringTokenizer(br.readLine());
 
-            for (int col = 0; col < N; col++) {
+            for (int col = 0; col < n; col++) {
                 graph[row][col] = Integer.parseInt(st.nextToken());
                 dp[row][col] = -1;
             }
         }
 
         int answer = 0;
-        for (int row = 0; row < N; row++) {
-            for (int col = 0; col < N; col++) {
-                answer = Math.max(answer, dfs(row, col));
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                answer = Math.max(answer, dfs(row, col) + 1);
             }
         }
 
         System.out.println(answer);
     }
 
-    static int dfs(int row, int col) {
-        if (dp[row][col] != -1) return dp[row][col];
+    static int dfs(int cur_row, int cur_col) {
+        if (dp[cur_row][cur_col] != -1) return dp[cur_row][cur_col];
 
-        int result = 1;
-
+        int result = 0;
         for (int i = 0; i < 4; i++) {
-            int new_row = row + dy[i];
-            int new_col = col + dx[i];
+            int new_row = cur_row + dy[i];
+            int new_col = cur_col + dx[i];
 
-            if (new_row < 0 || new_row == N || new_col < 0 || new_col == N) continue;
-            if (graph[row][col] >= graph[new_row][new_col]) continue;
-            
+            if (new_row < 0 || new_row == n || new_col < 0 || new_col == n) continue;
+            if (graph[cur_row][cur_col] >= graph[new_row][new_col]) continue;
+
             result = Math.max(result, dfs(new_row, new_col) + 1);
         }
 
-        dp[row][col] = result;
+        dp[cur_row][cur_col] = result;
         return result;
     }
 }
