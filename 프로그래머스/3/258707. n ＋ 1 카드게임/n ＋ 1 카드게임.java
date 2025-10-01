4d177de7fd123f3308a8093e8548f86cd36cca8f
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-10-01 20:57:22 +0900

    [level 3] Title: n + 1 카드게임, Time: 21.79 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -2,95 +2,91 @@ import java.util.*;
 
 class Solution {
     public int solution(int coin, int[] cards) {
+        HashSet<Integer> mineSet = new HashSet<>();
+        HashSet<Integer> trashSet = new HashSet<>();
+        ArrayDeque<Integer> cardList = new ArrayDeque<>();
+        
         int answer = 0;
-        HashMap<Integer, Boolean> own_card = new HashMap<>();
-        HashMap<Integer, Boolean> res_card = new HashMap<>();
-        int N = cards.length + 1;
-        // 초기 4개 저장
-        for (int i = 0; i < cards.length/3; i++) {
-            own_card.put(cards[i], true);
-        }
-        boolean flg1 = false;
-        int idx = cards.length/3;
+        int len = cards.length;
+        int point = len / 3;
+        
+        for (int i = 0; i < point; i++) mineSet.add(cards[i]);
+        for (int i = point; i < len; i++) cardList.addLast(cards[i]);
+        
         while (true) {
             answer++;
-            if (idx >= cards.length) break;
-            // 2장 뽑은 카드 res_card_list저장하기
-            if (idx < cards.length){
-                res_card.put(cards[idx++], true);
-                res_card.put(cards[idx++], true);
-            }
-            
-            // 해시맵을 리스트로
-            ArrayList<Integer> own_card_list = getArrayList(own_card);
-            ArrayList<Integer> res_card_list = getArrayList(res_card);
-
-            boolean flg = false;
-            // 먼저 소유하고 있는 카드에서 찾을 수 있는지 확인 => 코인 0개 없앰
-            for (int own_num : own_card_list) {
-                int target = N - own_num;
-                
-                // 찾을 수 있다면 own_card_list에서 지우기
-                if (own_card.containsKey(target)) {
-                    own_card.remove(target);
-                    own_card.remove(own_num);
-                    flg = true;
-                    break;
-                }
-            }
-            
-            if (flg) continue;
-            
-            // 소유하고 있는 카드 기준으로 넘긴 카드에서 찾을 수 있는지 확인 => 코인 1개 없앰
-            for (int own_num : own_card_list) {
-                // 코인이 1개 미만이면 나가기
-                if (coin < 1) break;
-                int target = N - own_num;
-                
-                // 찾을 수 있다면 own_card_list와 ,res_card_list에 지우기
-                if (res_card.containsKey(target)) {
-                    own_card.remove(own_num);
-                    res_card.remove(target);
-                    coin--;
-                    flg = true;
-                    break;
-                }
-            }
-            
-            if (flg) continue;
-            
-            
-            //넘긴 카드에서 찾을 수 있는지 확인 => 코인 2개 없앰
-            for (int res_num : res_card_list) {
-                //코인이 2개미만이면 나가기
-                if (coin < 2) break;
-                int target = N - res_num;
-                
-                // 찾을 수 있다면 res_card_list에 지우기
-                 if (res_card.containsKey(target)) {
-                    res_card.remove(res_num);
-                    res_card.remove(target);
-                    flg = true;
-                    coin -= 2;
-                    break;
-                }
-            }
-            
-            if (!flg){
-                
+            if (cardList.isEmpty() || mineSet.isEmpty()) {
                 break;
-            } 
+            }
             
-        }
-        
+            // 2장 뽑기
+            trashSet.add(cardList.pollFirst());
+            trashSet.add(cardList.pollFirst());
 
-        
+            
+            boolean flg = false;
+            int target = -1;
+            for (int mine : mineSet) {
+                int tmp = (len + 1) - mine;
+                if (mineSet.contains(tmp)) {
+                    target = mine;
+                    flg = true;
+                    break;
+                }
+            }
+            
+            if (flg) {
+                mineSet.remove(target);
+                mineSet.remove( (len + 1) - target );
+                continue;
+            }
+            
+            // 1개 뽑기
+            if (coin >= 1) {
+                
+                for (int mine: mineSet) {
+                    int tmp = (len + 1) - mine;
+                    if (trashSet.contains(tmp)) {
+                        target = mine;
+                        flg = true;
+                        break;
+                    }
+                }
+
+                if (flg) {
+                    mineSet.remove(target);
+                    trashSet.remove( (len + 1) - target );
+                    coin--;
+                    continue;
+                }
+                
+            }
+            
+            // 2개 뽑기
+            if (coin >= 2) {
+                
+                for (int mine: trashSet) {
+                    int tmp = (len + 1) - mine;
+                    if (trashSet.contains(tmp)) {
+                        target = mine;
+                        flg = true;
+                        break;
+                    }
+                }
+
+                if (flg) {
+                    trashSet.remove(target);
+                    trashSet.remove( (len + 1) - target );
+                    coin--;
+                    coin--;
+                    continue;
+                }
+                
+            }
+            
+            break;
+        }
+
         return answer;
-    }
-    
-    static ArrayList<Integer> getArrayList (HashMap<Integer, Boolean> map) {
-        ArrayList<Integer> tmp = new ArrayList<>();
-        for (int n : map.keySet()) tmp.add(n);
-        return tmp;
     }
 }
