e55377a530b9fc393ab164c8ef38ff95eceae3b3
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-10-05 15:46:56 +0900

    [Gold II] Title: 문제집, Time: 412 ms, Memory: 49876 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -1,61 +1,69 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+// The main method must be in a class named "Main".
+class Main {
 
-public class Main {
-	static int N, M;
-	static int[] count;
-	static StringBuilder sb = new StringBuilder();
-	static HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
-	
-	public static void main(String[] args) throws IOException {
-//		BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		count = new int[N + 1];
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			if (graph.containsKey(a)) {
-				ArrayList<Integer> tmp = graph.get(a);
-				tmp.add(b);
-				graph.put(a, tmp);
-			} else {
-				ArrayList<Integer> tmp = new ArrayList<>();
-				tmp.add(b);
-				graph.put(a, tmp);
-			}
-			count[b] += 1;
-		}
-		
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		
-		for (int i = 1; i <= N; i++) {
-			if (count[i] == 0) pq.add(i);
-		}
-		
-		while (!pq.isEmpty()) {
-			int num = pq.poll();
-			sb.append(num).append(" ");
-			
-			ArrayList<Integer> list = graph.get(num);
-			if (!graph.containsKey(num)) continue;
-			
-			for (int new_num : list) {
-				count[new_num] -= 1;
-				
-				if (count[new_num] == 0) pq.add(new_num);
-			}
-			
-		}
-		
-		System.out.println(sb.toString());
-	}
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+    
+    public static void main(String[] args) throws IOException{
+        int N, M;
+        FastScanner fs = new FastScanner();
+        
+        N = fs.nextInt();
+        M = fs.nextInt();
+
+        ArrayList<Integer>[] list = new ArrayList[N + 1];
+        int[] count = new int[N + 1];
+        for (int i = 0; i <= N; i++) list[i] = new ArrayList<>();
+
+        for (int i = 0; i < M; i++) {
+            int from = fs.nextInt();
+            int to = fs.nextInt();
+
+            count[to]++;
+            list[from].add(to);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 1; i <= N; i++) {
+            if (count[i] == 0) pq.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!pq.isEmpty()) {
+            int node = pq.poll();
+
+            if (count[node] == 0) sb.append(node).append(" ");
+
+            ArrayList<Integer> new_list = list[node];
+            for (int new_node : new_list) {
+                count[new_node]--;
+
+                if (count[new_node] == 0) {
+                    pq.add(new_node);
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
 }
