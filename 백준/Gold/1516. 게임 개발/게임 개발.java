62af82886996500f3ef640e78fd71dbd405d03de
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-10-07 01:01:41 +0900

    [Gold III] Title: 게임 개발, Time: 196 ms, Memory: 20368 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -2,56 +2,64 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+// The main method must be in a class named "Main".
 class Main {
-    static int N;
-    static ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-    static int[] memo;
-    
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        memo = new int[N + 1];
-        for (int i = 0; i <= N; i++) {
-            ArrayList<Integer> inner_list = new ArrayList();
-            memo[i] = -1;
-            if (i == 0) {
-                list.add(inner_list);
-                continue;
-            }
-            
+        int N = Integer.parseInt(st.nextToken());
+        ArrayList<Integer>[] graph = new ArrayList[N + 1];
+
+        for (int i = 0; i <= N; i++) graph[i] = new ArrayList<>();
+        int[] times = new int[N + 1];
+        int[] counts = new int[N + 1];
+        int[] answer = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
 
-            while (true) {
-                int n = Integer.parseInt(st.nextToken());
+            int time = Integer.parseInt(st.nextToken());
+            times[i] = time;
 
-                if (n == -1) break;
-                inner_list.add(n);
+            while (true) {
+                int node = Integer.parseInt(st.nextToken());
+
+                if (node == -1) break;
+                graph[node].add(i);
+                counts[i]++;
             }
-            list.add(inner_list);
         }
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            if (counts[i] == 0) {
+                deque.add(i);
+                answer[i] = times[i];
+            }
+        }
+
+        while (!deque.isEmpty()) {
+            int node = deque.pollFirst();
+
+            ArrayList<Integer> list = graph[node];
+
+            for (int new_node : list) {
+                counts[new_node]--;
+                answer[new_node] = Math.max(answer[new_node], answer[node]);
+
+                if (counts[new_node] == 0) {
+                    deque.add(new_node);
+                    answer[new_node] += times[new_node];
+                }
+            }
+        }
+
         StringBuilder sb = new StringBuilder();
-        
-        for (int i = 1; i <= N; i++) sb.append(dp(i)).append("\n");
+        for (int i = 1 ; i <= N; i++) {
+            sb.append(answer[i]).append("\n");
+        }
 
         System.out.println(sb.toString());
-    }
-
-    static int dp(int num) {
-        if (memo[num] != -1) return memo[num];
-
-        ArrayList<Integer> connect = list.get(num);
-
-        int total = 0;
-   
-
-        for (int i = 1; i < connect.size(); i++) {
-            total = Math.max(dp(connect.get(i)) , total);
-        }
-
-        total += connect.get(0);
-        memo[num] = total;
-        return total;
     }
 }
