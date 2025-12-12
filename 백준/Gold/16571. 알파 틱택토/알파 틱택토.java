b5d983145a8699daf290909b5eaedcdcfedf6a02
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2025-12-12 16:33:29 +0900

    [Gold III] Title: 알파 틱택토, Time: 188 ms, Memory: 18076 KB -BaekjoonHub

--- 백준/Gold/16571. 알파 틱택토/알파 틱택토.java
+++ 백준/Gold/16571. 알파 틱택토/알파 틱택토.java
@@ -4,143 +4,150 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int[][] graph = new int[3][3];
-    static int player;
-    static int isWin = -1;
+
+    static class TicTakToe {
+        int[][] board;
+        int myTurn;
+        int[][] dy = new int[][] { 
+            { 0, 1, 2 }, 
+            { 0, 0, 0 },
+            { 0, 1, 2 },
+            { 2, 2, 2 },
+            { 0, 1, 2 },
+            { 0, 1, 2 },
+            { 1, 1, 1 },
+            { 0, 1, 2 }
+        };
+        int[][] dx = new int[][] {
+            { 0, 0, 0 },
+            { 0, 1, 2 },
+            { 2, 2, 2 },
+            { 0, 1, 2 },
+            { 0, 1, 2 },
+            { 2, 1, 0 },
+            { 0, 1, 2 },
+            { 1, 1, 1 }
+        };
+
+        
+        TicTakToe(int[][] board) {
+            this.board = board;
+            int oneCnt = 0;
+            int twoCnt = 0;
+
+            for (int row = 0; row < 3; row++) {
+                for (int col = 0; col < 3; col++) {
+                    if (board[row][col] == 1) oneCnt++;
+                    if (board[row][col] == 2) twoCnt++;
+                }
+            }
+
+            if (oneCnt == twoCnt) myTurn = 1;
+            else myTurn = 2;
+        }
+
+        int isGameOver() {
+            for (int i = 0; i < 8; i++) {
+                int init = board[ dy[i][0] ][ dx[i][0] ];
+                int cnt = 0;
+                
+                if (init == 0) continue;
+                for (int j = 0; j < 3; j++) {
+                    if ( init == board[ dy[i][j] ][ dx[i][j] ] ) cnt++;
+                }
+
+                if (cnt == 3) {
+                    if (myTurn == init) return 1;
+                    else return -1;
+                }
+            }
+
+            int totalCnt = 0;
+            for (int row = 0; row < 3; row++) {
+                for (int col = 0; col < 3; col++) {
+                    if (board[row][col] != 0) totalCnt++;
+                }
+            }
+
+            if (totalCnt == 9) return 0;
+            else return -2;
+        }
+
+        void start() {
+            int result = dfs(myTurn, 0);
+
+            if (result == 1) System.out.println("W");
+            else if(result == 0) System.out.println("D");
+            else System.out.println("L");
+        }
+        
+        int dfs (int turn, int cnt) {
+            int gameOver = isGameOver();
+            
+            if (gameOver != -2) return gameOver;
+            boolean isMyTurn = (turn == myTurn);
+
+            int result;
+            if (isMyTurn) result = Integer.MIN_VALUE;
+            else result = Integer.MAX_VALUE;
+
+            int newTurn = turn == 1 ? 2 : 1;
+            for (int row = 0; row < 3; row++) {
+                for (int col = 0; col < 3; col++) {
+                    if (board[row][col] != 0) continue;
+
+                    board[row][col] = turn;
+                    
+                    if (isMyTurn) result = Math.max(result, dfs(newTurn, 1));
+                    else result = Math.min(result, dfs(newTurn, 1));
+                    
+                    board[row][col] = 0;
+                    
+                }
+            }
+
+            return result;
+        }
+        
+        void display() {
+            StringBuilder sb = new StringBuilder();
+
+            for (int row = 0; row < 3; row++) {
+                for (int col = 0; col < 3; col++) {
+                    sb.append(board[row][col]).append(" ");
+                }
+                sb.append("\n");
+            }
+
+            System.out.println(sb.toString());
+        }
+    }
+    
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        int nextInt() throws IOException {
+            while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+            return Integer.parseInt(st.nextToken());
+        }
+    }
+    
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        for (int row = 0; row < 3; row++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int col = 0; col < 3; col++){
-                graph[row][col] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        int count = 0;
-
-        for (int row = 0; row < 3; row++){
-            for (int col = 0; col < 3; col++) {
-                if (graph[row][col] == 1) count++;
-                if (graph[row][col] == 2) count++;
-            }
-        }
-
-        player = count % 2 == 0 ? 1 : 2;
-
-        isWin = backtracking(count);
-        
-        if (isWin > 0) System.out.println("W");
-        else if (isWin == 0) System.out.println("D");
-        else System.out.println("L");
-    }
-
-    static int leftResult() {
-        int cur_num = graph[0][0];
-
-        if (cur_num == 0) return 0;
-
-        if ((cur_num == graph[1][1]) && (cur_num == graph[2][2])) return cur_num;
-        return 0;
-    }
-
-    static int rightResult() {
-        int cur_num = graph[0][2];
-
-        if (cur_num == 0) return 0;
-
-        if ((cur_num == graph[1][1]) && (cur_num == graph[2][0])) return cur_num;
-        return 0;
-    }
-    
-    static int colResult() {
-        int cur_player = 0;
-        
-        for (int row = 0; row < 3; row++) {
-            int cur_num = graph[row][0];
-            if (cur_num == 0) continue;
-            
-            cur_player = cur_num;
-            for (int col = 1; col < 3; col++){
-                if  (graph[row][col]  != cur_num) {
-                    cur_player = 0;
-                    break;
-                }
-            }
-
-            if (cur_player != 0) break;
-        }
-        return cur_player;
-    }
-    
-    static int rowResult() {
-        int cur_player = 0;
-        
-        for (int col = 0; col < 3; col++) {
-            int cur_num = graph[0][col];
-            if (cur_num == 0) continue;
-            
-            cur_player = cur_num;
-            for (int row = 1; row < 3; row++){
-                if  (graph[row][col]  != cur_num) {
-                    cur_player = 0;
-                    break;
-                }
-            }
-
-            if (cur_player != 0) break;
-        }
-        return cur_player;
-    }
-    
-    static int getResult() {
-        int lResult = leftResult();
-        if (lResult != 0) return lResult;
-
-        int rResult = rightResult();
-        if (rResult != 0) return rResult;
-
-        int rowRe = rowResult();
-        if (rowRe != 0) return rowRe;
-
-        int colRe = colResult();
-        if(colRe != 0) return colRe;
-
-        return 0;
-    }
-
-    
-    static int backtracking(int count) {
-        int cur_node = count % 2 == 0 ? 1 : 2;
-        int cur_result = getResult();
-        
-        if (cur_result != 0) {
-            if (cur_result == player) return 1;
-            return -1;
-        }
-        
-        if (count == 9) {
-
-            return 0;
-        }
-
-        int total = cur_node == player ?  -1 : 1;
-        
+        FastScanner fs = new FastScanner();
+        int[][] board = new int[3][3];
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
-                if (graph[row][col] != 0) continue;
-
-                graph[row][col] = cur_node;
-
-                if (cur_node == player) total = Math.max(total , backtracking(count + 1));
-                else total = Math.min(total, backtracking(count + 1));
-
-                
-                graph[row][col] = 0;
-                
+                board[row][col] = fs.nextInt();
             }
         }
 
-        return total;
+        TicTakToe tictaktoe = new TicTakToe(board);
+        tictaktoe.start();
     }
+
 }
