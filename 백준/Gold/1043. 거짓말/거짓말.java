9404a32da39ce47be8426be1f357b7b365d9e308
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2026-01-24 17:42:02 +0900

    [Gold IV] Title: 거짓말, Time: 68 ms, Memory: 11672 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,73 +1,93 @@
-
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+// The main method must be in a class named "Main".
+class Main {
+    static int N, M;
+    static boolean[] visited;
+    static ArrayList<Integer>[] graph, partyList;
+    static class FastScanner {
+        StringTokenizer st;
+        BufferedReader br;
 
-public class Main {
-	static int N, M;
-	static HashMap<Integer, Boolean> dict = new HashMap<>(); 
-	static ArrayList<int[]> list = new ArrayList<>();
-	
-	public static void main(String[] args) throws IOException {
-		//BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		
-		for (int i = 0; i < n; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			dict.put(num, true);
-		}
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int cnt = Integer.parseInt(st.nextToken());
-			int[] tmp = new int[cnt];
-			
-			for (int j = 0; j < cnt; j++) {
-				tmp[j] = Integer.parseInt(st.nextToken());
-			}
-			
-			list.add(tmp);
-		}
-		int ans = list.size();
-		boolean[] visited = new boolean[list.size()];
-		while (true) {
-			boolean flg = false;
-			
-			int idx = 0;
-  			for (int[] arr : list) {
-  				if (visited[idx++]) continue;
-  				
-				boolean flg1 = false;
-				for (int k : arr) {
-					if (dict.containsKey(k)) { 
-						flg1 = true;
-					}
-				}
-				
- 				if (!flg1) continue;
- 				visited[idx-1] = true;
- 				
-				ans -= 1;
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-       			for (int k : arr) {
-					if (!dict.containsKey(k)) {
-						dict.put(k, true);
-						flg = true;
-					}
-				}
-			}
-			
-        if (!flg) break;
-		}
-		
-		System.out.println(ans);
-	}
+        int nextInt() throws IOException {
+            while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+            return Integer.parseInt(st.nextToken());
+        }
+    }
+    
+    public static void main(String[] args) throws IOException{
+        FastScanner fs = new FastScanner();
 
+        N = fs.nextInt(); M = fs.nextInt();
+        visited = new boolean[N + 1];
+        graph = new ArrayList[N + 1];
+        partyList = new ArrayList[M];
+
+        for (int i = 0; i < M; i++) partyList[i] = new ArrayList<>();
+        for (int i = 0; i <= N; i++) graph[i] = new ArrayList<>();
+        
+        int truthCnt = fs.nextInt();
+        int[] truthArr = new int[truthCnt];
+        for (int i = 0; i < truthCnt; i++) {
+            truthArr[i] = fs.nextInt();
+            visited[ truthArr[i] ] = true;
+        }
+
+        for (int row = 0; row < M; row++) {
+            int cnt = fs.nextInt();
+
+            int[] tmpArr = new int[cnt];
+            for (int i = 0; i < cnt; i++) {
+                tmpArr[i] = fs.nextInt();
+                partyList[row].add(tmpArr[i]);
+            }
+
+            for (int i = 0; i < cnt; i++) {
+                for (int j = 0; j < cnt; j++) {
+                    if (i == j) continue;
+
+                    graph[ tmpArr[i] ].add( tmpArr[j] );
+                }
+            }
+        }
+
+        for (int truth_no : truthArr) {
+            dfs(truth_no);
+        }
+
+        int answer = 0;
+        for (ArrayList<Integer> party : partyList) {
+            boolean isAnswer = true;
+
+            for (int no : party) {
+                if (visited[no]) {
+                    isAnswer = false;
+                    break;
+                }
+            }
+
+            if (isAnswer) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    static void dfs(int cur_no) {
+
+        ArrayList<Integer> cur_list = graph[cur_no];
+        for (int new_no : cur_list) {
+            if (visited[new_no]) continue;
+
+            visited[new_no] = true;
+            dfs(new_no);
+        }
+    }
 }
