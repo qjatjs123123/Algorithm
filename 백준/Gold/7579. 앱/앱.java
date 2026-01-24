052a982b6ecf940d5bdb28aebaca81b82d5248df
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2026-01-24 18:42:58 +0900

    [Gold III] Title: 앱, Time: 88 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -4,59 +4,60 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int N, M;
-    static int[] memoryArr;
-    static int[] costArr;
+    static class FastScanner {
+        StringTokenizer st;
+        BufferedReader br;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        int nextInt() throws IOException {
+            while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+            return Integer.parseInt(st.nextToken());
+        }
+    }
     
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        FastScanner fs = new FastScanner();
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        memoryArr = new int[N + 1];
-        costArr = new int[N + 1];
-        st = new StringTokenizer(br.readLine());
+        int N = fs.nextInt(); int M = fs.nextInt();
+        int[] memoryArr = new int[ N + 1 ];
+        int[] costArr = new int[ N + 1 ];
+        int[][] dp;
+        int total = 0;
         
         for (int i = 1; i <= N; i++) {
-            memoryArr[i] = Integer.parseInt(st.nextToken());    
+            memoryArr[i] = fs.nextInt();
         }
 
-        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            costArr[i] = Integer.parseInt(st.nextToken());    
+            costArr[i] = fs.nextInt();
+            total += costArr[i];
         }
 
-        int[][] dp = new int[101][10_001];
-        int answer = Integer.MAX_VALUE;
-        
+        dp = new int[N + 1][total + 1];
         for (int row = 1; row <= N; row++) {
             int memory = memoryArr[row];
             int cost = costArr[row];
-            
-            if (row == 1) {
-                dp[row][cost] = memory;
-                continue;
-            }
 
-            
-            
-            for (int col = 0; col <= 10_000; col++) {
-                dp[row][col] = dp[row - 1][col];
-                if (col >= cost) {
-                    dp[row][col] = Math.max(dp[row - 1][col - cost] + memory, dp[row - 1][col]);
+            for (int col = 0; col <= total; col++) {
+                if (col < cost) {
+                    dp[row][col] = dp[row - 1][col];
+                } else {
+                    dp[row][col] = Math.max(
+                        dp[row - 1][col],
+                        memory + dp[row - 1][col - cost]
+                    );
                 }
             }
         }
 
-        for (int c = 0; c <= 10000; c++) {
-            if (dp[N][c] >= M) {
-                System.out.println(c);
-                break;
+        for (int col = 0; col <= total; col++) {
+            if (dp[N][col] >= M) {
+                System.out.println(col);
+                return;
             }
         }
-
-        
     }
 }
