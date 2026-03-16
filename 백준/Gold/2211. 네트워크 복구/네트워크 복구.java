fff96e1cba954de54b79bfccef97e78ff4eeb166
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2026-03-16 22:25:35 +0900

    [Gold II] Title: 네트워크 복구, Time: 500 ms, Memory: 81468 KB -BaekjoonHub

--- 백준/Gold/2211. 네트워크 복구/네트워크 복구.java
+++ 백준/Gold/2211. 네트워크 복구/네트워크 복구.java
@@ -5,11 +5,23 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
     static int N, M;
-    static ArrayList<Edge>[] graph;
-    static int[] visited;
-    static class Edge{
-        int no;
-        int dist;
+    static ArrayList<Edge> graph[];
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        int nextInt() throws IOException {
+            while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+            return Integer.parseInt(st.nextToken());
+        }
+    }
+    
+    static class Edge  {
+        int no, dist;
 
         Edge(int no, int dist) {
             this.no = no;
@@ -17,91 +29,78 @@ class Main {
         }
     }
 
-    static class Info implements Comparable<Info>{
-        int dist, from;
+    static class Route implements Comparable<Route>{
+        int no, cnt, dist;
 
-        Info (int dist, int from) {
+        Route(int no, int cnt, int dist) {
+            this.no = no;
+            this.cnt = cnt;
             this.dist = dist;
-            this.from = from;
         }
 
-        public int compareTo(Info info) {
-            return this.dist - info.dist;
+        public int compareTo(Route o){
+            if (this.dist < o.dist) return Integer.compare(this.dist, o.dist);
+            return Integer.compare(this.cnt, o.cnt);
         }
     }
     
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        FastScanner fs = new FastScanner();
+        N = fs.nextInt(); M = fs.nextInt();
 
         graph = new ArrayList[N + 1];
-        visited = new int[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
-        for (int i = 0; i <= N; i++) graph[i] = new ArrayList<>();
-
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int dist = Integer.parseInt(st.nextToken());
+            int from = fs.nextInt();
+            int to = fs.nextInt();
+            int dist = fs.nextInt();
 
             graph[from].add(new Edge(to, dist));
             graph[to].add(new Edge(from, dist));
         }
 
-        dijkstra();
-
-        StringBuilder sb = new StringBuilder();
         int cnt = 0;
-        for (int row = 1; row <= N; row++) {
-            if (visited[row] != 0) {
-                cnt++;
-                sb.append(row).append(" ").append(visited[row]).append("\n");
-            }
-        }
-
-        System.out.println(cnt);
-        System.out.println(sb.toString());
+        dijkstra();
     }
 
     static void dijkstra() {
         int[] distance = new int[N + 1];
-        for (int i = 0; i < N + 1; i++) distance[i] = Integer.MAX_VALUE;
-        ArrayList<Integer>[] trackList = new ArrayList[N + 1];
+        int[] path = new int[N + 1];
 
-        PriorityQueue<Info> pq = new PriorityQueue<>();
-        Info tmp = new Info(0, 1);
+        for (int i = 0; i <= N; i++) distance[i] = Integer.MAX_VALUE;
         distance[1] = 0;
+        
+        PriorityQueue<Route> pq = new PriorityQueue<>();
+        pq.add(new Route(1, 0, 0));
 
-        pq.add(tmp);
+        while(!pq.isEmpty()) {
+            Route route = pq.poll();
 
-        while (!pq.isEmpty()) {
-            Info info = pq.poll();
+            if (route.dist > distance[route.no]) continue;
 
-            if (distance[info.from] < info.dist) continue;
-            ArrayList<Edge> list = graph[info.from];
+            ArrayList<Edge> list = graph[route.no];
 
-            for (Edge edge : list) {
-                if (info.dist + edge.dist >= distance[edge.no]) continue;
-                
-                Info t = new Info(info.dist + edge.dist, edge.no);
-                visited[edge.no] = info.from;
-                distance[edge.no] = info.dist + edge.dist;
-                pq.add(t);
+            for(Edge edge: list) {
+                int new_dist = route.dist + edge.dist;
+
+                if (new_dist > distance[edge.no]) continue;
+                path[edge.no] = route.no;
+                distance[edge.no] = new_dist;
+                pq.add(new Route(edge.no, route.cnt + 1, new_dist));
             }
-            
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(N - 1).append("\n");
+        
+        for (int i = 2; i < N + 1; i++) {
+            sb.append(i).append(" ").append(path[i]).append("\n");
         }
 
+        System.out.println(sb.toString());
     }
 
-    static ArrayList<Integer> deepCopy(ArrayList<Integer> list) {
-        ArrayList<Integer> result = new ArrayList<>();
-        for (Integer n : list) result.add(n);
-
-        return result;
-    }
 }
