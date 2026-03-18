afa96d6d11ff1bd78797524081fe52d9042b07b6
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2026-03-18 22:12:04 +0900

    [Gold III] Title: 서울 지하철 2호선, Time: 120 ms, Memory: 14956 KB -BaekjoonHub

--- 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
+++ 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
@@ -5,100 +5,108 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
     static int N;
-    static HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
-    static Deque<Integer> stack = new LinkedList<>();
+    static ArrayList<Integer>[] graph;
     static boolean[] visited;
-    static boolean flg;
+    static Stack<Integer> stack = new Stack<>();
+    static boolean[] isCycle;
+    static boolean isFinish = false;
+    
+    static class FastScanner {
+        StringTokenizer st;
+        BufferedReader br;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        int nextInt() throws IOException {
+            while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+            return Integer.parseInt(st.nextToken());
+        }
+    }
     
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        FastScanner fs = new FastScanner();
+        N = fs.nextInt();
 
-        N = Integer.parseInt(st.nextToken());
+        isCycle = new boolean[N + 1];
+        graph = new ArrayList[N + 1];
         visited = new boolean[N + 1];
-        
-        for (int i = 1; i <= N; i++) {
-            ArrayList<Integer> list = new ArrayList<>();
-            graph.put(i, list);
-        }
-
+        for (int i = 0; i < N + 1; i++) graph[i] = new ArrayList<>();
+    
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
+            int from = fs.nextInt();
+            int to = fs.nextInt();
 
-            ArrayList<Integer> cur_list = graph.get(from);
-            cur_list.add(to);
-            cur_list = graph.get(to);
-            cur_list.add(from);
+            graph[from].add(to);
+            graph[to].add(from);
         }
 
-        int[] ans = new int[N + 1];
-
-        for (int i = 1; i < N + 1; i++) ans[i] = -1;
-        
         visited[1] = true;
-        stack.add(1);
         dfs(1, -1);
-        
-        Deque<int[]> deque = new LinkedList<>();
-        visited = new boolean[N + 1];
-        
-        for (int node : stack) {
-            visited[node] = true;
-            ans[node] = 0;
-            deque.add(new int[] {node, 0});
+        bfs();
+    }
+
+    static void bfs() {
+        Deque<int[]> deque = new ArrayDeque<>();
+        boolean[] visited = new boolean[N + 1];
+        int[] answer = new int[N + 1];
+
+        for (int i = 1; i < N + 1; i++) {
+            if (isCycle[i] == false) continue;
+            
+            deque.add(new int[] {i, 0});
+            answer[i] = 0;
+            visited[i] = true;
         }
-        
-        while (!deque.isEmpty()) {
+
+        while(!deque.isEmpty()) {
             int[] cur_arr = deque.pollFirst();
-            int cur_node = cur_arr[0];
+            int cur_no = cur_arr[0];
             int cur_cnt = cur_arr[1];
 
-            ArrayList<Integer> new_list = graph.get(cur_node);
-
-            for(int new_node : new_list) {
-                if (visited[new_node]) continue;
-                visited[new_node] = true;
-                ans[new_node] = cur_cnt + 1;
-                deque.add(new int[] {new_node, cur_cnt + 1});
+            ArrayList<Integer> list = graph[cur_no];
+            for(int new_no : list) {
+                if (visited[new_no]) continue;
+                visited[new_no] = true;
+                deque.add(new int[] {new_no, cur_cnt + 1});
+                answer[new_no] = cur_cnt + 1;
             }
-            
         }
 
         StringBuilder sb = new StringBuilder();
-
-        for (int i = 1; i <= N; i++) sb.append(ans[i]).append(" ");
+        for (int i = 1; i <= N; i++) sb.append(answer[i]).append(" ");
 
         System.out.println(sb.toString());
     }
+    
+    static void dfs(int cur_no, int prev_no) {
 
-    static void dfs(int cur_node, int prev) {
-        if (flg) return;
-        ArrayList<Integer> new_list = graph.get(cur_node);
+        ArrayList<Integer> list = graph[cur_no];
+        for (int new_no : list) {
+            if (new_no == prev_no) continue;
+            if (!visited[new_no]) {
+                visited[new_no] = true;
+                stack.push(new_no);
+                dfs(new_no, cur_no);
+                if(isFinish) return;
+                stack.pop();
+            } else {
+                ArrayDeque<Integer> deque = new ArrayDeque<>();
 
-        for (int new_node : new_list) {
-            
-            if (prev == new_node) continue;
-            if (visited[new_node]) {
+                while(!stack.isEmpty()) {
+                    int top = (int)stack.pop();
 
-                while (!stack.isEmpty()) {
-                    int left = stack.pollFirst();
-
-                    if (left == new_node) {
-                        stack.add(new_node);
-                        break;
-                    }
+                    if (top == new_no) break;
+                    deque.add(top);
                 }
-                flg = true;
-                break;
+
+                deque.add(new_no);
+                for (int no : deque) isCycle[no] = true;
+
+                isFinish = true;
             }
 
-            stack.add(new_node);
-            visited[new_node] = true;
-            dfs(new_node, cur_node);
-            if (flg) break;
-            stack.pollLast();
         }
     }
 }
