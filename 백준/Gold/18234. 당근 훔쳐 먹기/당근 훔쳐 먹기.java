09e301b31325a96b834a9fc626ac1d9650db9a65
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2026-03-23 23:31:31 +0900

    [Gold III] Title: 당근 훔쳐 먹기, Time: 684 ms, Memory: 66788 KB -BaekjoonHub

--- 백준/Gold/18234. 당근 훔쳐 먹기/당근 훔쳐 먹기.java
+++ 백준/Gold/18234. 당근 훔쳐 먹기/당근 훔쳐 먹기.java
@@ -4,40 +4,46 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int N, T;
-    static int[][] graph;
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        
+        long nextLong() throws IOException {
+            while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+            return Long.parseLong(st.nextToken());
+        }
+
+        int nextInt() throws IOException {
+            while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+            return Integer.parseInt(st.nextToken());
+        }
+    }
     
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        FastScanner fs = new FastScanner();
+        int N = fs.nextInt(); 
+        int T = fs.nextInt();
 
-        N = Integer.parseInt(st.nextToken());
-        T = Integer.parseInt(st.nextToken());
-
-        graph = new int[N][2];
+        int[][] graph = new int[N + 1][2];
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            int w = fs.nextInt();
+            int p = fs.nextInt();
 
-            int w = Integer.parseInt(st.nextToken());
-            int p = Integer.parseInt(st.nextToken());
             graph[i][0] = w;
             graph[i][1] = p;
         }
 
-        Arrays.sort(graph, (a, b) -> {
-            if (a[1] == b[1]) return b[0] - a[0];
-            return b[1] - a[1];
-        });
-
         long answer = 0;
+        Arrays.sort(graph, (a,b) -> b[1] - a[1]);
 
         for (int i = 0; i < N; i++) {
             int[] arr = graph[i];
-            int cur_t = T - i - 1;
-
-            answer += (long)arr[1] * cur_t + arr[0];
+            answer += arr[0] + ((long)arr[1] * (T - i - 1));
         }
         System.out.println(answer);
-        
     }
 }
