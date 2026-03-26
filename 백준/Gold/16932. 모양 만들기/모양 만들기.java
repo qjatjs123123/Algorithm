2896c43354a711d84eaab86cf8cf049beebea254
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2026-03-27 00:38:40 +0900

    [Gold III] Title: 모양 만들기, Time: 1124 ms, Memory: 247524 KB -BaekjoonHub

--- 백준/Gold/16932. 모양 만들기/모양 만들기.java
+++ 백준/Gold/16932. 모양 만들기/모양 만들기.java
@@ -4,8 +4,6 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int N, M;
-    static int[][] graph, dp;
     static class FastScanner {
         BufferedReader br;
         StringTokenizer st;
@@ -22,93 +20,78 @@ class Main {
     
     public static void main(String[] args) throws IOException{
         FastScanner fs = new FastScanner();
-        N = fs.nextInt();
-        M = fs.nextInt();
-
-        graph = new int[N][M];
-        dp = new int[N][M];
+        int N = fs.nextInt(); int M = fs.nextInt();
+        int[][] graph = new int[N][M];
+        int[][] new_graph = new int[N][M];
+        boolean[][] visited = new boolean[N][M];
+        HashMap<Integer, Integer> countSet = new HashMap<>();
 
         for (int row = 0; row < N; row++) {
-            for (int col = 0; col < M; col++) {
-                graph[row][col] = fs.nextInt();
-                dp[row][col] = -1;
-            }
+            for (int col = 0; col < M; col++) graph[row][col] = fs.nextInt();
         }
-
 
         int[] dy = new int[] {1, -1, 0, 0};
         int[] dx = new int[] {0, 0, 1, -1};
-        boolean[][] visited = new boolean[N][M];
-        int group = 1;
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int id = 1;
+        
         for (int row = 0; row < N; row++) {
             for (int col = 0; col < M; col++) {
-                if (dp[row][col] != -1) continue;
-                if (graph[row][col] == 0) {
-                    dp[row][col] = 0;
-                    continue;
-                }
+                if (visited[row][col] || graph[row][col] == 0) continue;
 
                 Deque<int[]> deque = new ArrayDeque<>();
-                Stack<int[]> stack = new Stack<>();
                 deque.add(new int[] {row, col});
-                stack.push(new int[] {row, col});
                 visited[row][col] = true;
-                
+                new_graph[row][col] = id;
+                int count = 1;
+
                 while (!deque.isEmpty()) {
                     int[] cur_arr = deque.pollFirst();
 
-                    for (int i = 0; i < 4; i++) {
-                        int new_row = cur_arr[0] + dy[i];
-                        int new_col = cur_arr[1] + dx[i];
+                    int cur_row = cur_arr[0];
+                    int cur_col = cur_arr[1];
 
-                        if (new_row < 0 || new_row >= N || new_col < 0 || new_col >= M) continue;
+                    for (int i = 0; i < 4; i++) {
+                        int new_row = cur_row + dy[i];
+                        int new_col = cur_col + dx[i];
+
+                        if (new_row < 0 || new_row == N || new_col < 0 || new_col == M) continue;
                         if (visited[new_row][new_col]) continue;
                         if (graph[new_row][new_col] == 0) continue;
-                        
-                        stack.push(new int[] {new_row, new_col});
-                        deque.add(new int[] {new_row, new_col});
+
+                        new_graph[new_row][new_col] = id;
+                        count++;
                         visited[new_row][new_col] = true;
+                        deque.add(new int[] {new_row, new_col});
                     }
                 }
-                if (stack.isEmpty()) continue;
-                
-                int cnt = stack.size();
-                while (!stack.isEmpty()) {
-                    int[] cur_arr = stack.pop();
-
-                    dp[ cur_arr[0] ][ cur_arr[1] ] = group;
-                }
-                map.put(group, cnt);
-                group++;
-                
+                countSet.put(id, count);
+                id++;
             }
-        }   
+        }
 
-
-        
         int answer = 0;
         for (int row = 0; row < N; row++) {
             for (int col = 0; col < M; col++) {
-                int total = 0;
-                if (graph[row][col] == 0) {
+                if (new_graph[row][col] == 0) {
+
+                    int tmp = 1;
                     HashSet<Integer> set = new HashSet<>();
                     for (int i = 0; i < 4; i++) {
                         int new_row = row + dy[i];
                         int new_col = col + dx[i];
-                        
-                        if (new_row < 0 || new_row >= N || new_col < 0 || new_col >= M) continue;
-                        if (dp[new_row][new_col] == 0) continue;
-                        set.add(dp[new_row][new_col]);
+
+                        if (new_row < 0 || new_row == N || new_col < 0 || new_col == M) continue;
+                        if (new_graph[new_row][new_col] == 0) continue;
+                        set.add(new_graph[new_row][new_col]);
                     }
 
-                    for (int key : set) total += map.get(key);
-                    answer = Math.max(answer, total + 1);
+                    for (int n : set) tmp += countSet.get(n);
+                    answer = Math.max(answer, tmp);
                 }
-
-            }   
+            }
         }
 
+        
         System.out.println(answer);
     }
 }
