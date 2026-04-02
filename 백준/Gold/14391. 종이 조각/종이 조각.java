f4f0677e999376b647b4cf7615abafaf34025e77
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2026-04-02 23:40:45 +0900

    [Gold III] Title: 종이 조각, Time: 136 ms, Memory: 12668 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.java
+++ 백준/Gold/14391. 종이 조각/종이 조각.java
@@ -4,79 +4,78 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int N, M;
-    static int[][] graph;
-    static boolean[][] visited;
-    static int[] dy = new int[] {1, 0};
-    static int[] dx = new int[] {0, 1};
-    static int answer = 0;
-    static int tmp = 0;
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+        
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        int nextInt() throws IOException {
+            while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+            return Integer.parseInt(st.nextToken());
+        }
+
+        String nextString() throws IOException {
+            while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+            return st.nextToken();
+        }
+    }
     
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
-        graph = new int[N][M]; visited = new boolean[N][M];
+        FastScanner fs = new FastScanner();
+        int N = fs.nextInt(); int M = fs.nextInt();
+        int[][] graph = new int[N][M];
 
         for (int row = 0; row < N; row++) {
-            st = new StringTokenizer(br.readLine());
-            String str = st.nextToken();
-
-            for (int col = 0; col < M; col++) graph[row][col] = str.charAt(col) - '0';
+            String str = fs.nextString();
+            for (int col = 0; col < M; col++) {
+                graph[row][col] = str.charAt(col) - '0';
+            } 
         }
+        int answer = 0;
+        for (int cur_bit = 0; cur_bit < Math.pow(2, N * M); cur_bit++) {
+            int total = 0;
 
-        backtracking(0, 0);
+            for (int row = 0; row < N; row++) {
+                int tmp = 0;
+
+                for (int col = 0; col < M; col++) {
+                    int idx = 1 << ( (row * M) + col );
+                    int bit = cur_bit & idx;
+
+                    if (bit != 0) {
+                        tmp = (tmp * 10) + graph[row][col];
+                    }else {
+                        total += tmp;
+                        tmp = 0;
+                    }
+                }
+                
+                total += tmp;
+            }
+
+            for (int col = 0; col < M; col++) {
+                int tmp = 0;
+
+                for (int row = 0; row < N; row++) {
+                    int idx = 1 << ( (row * M) + col );
+                    int bit = cur_bit & idx;
+
+                    if (bit == 0) {
+                        tmp = (tmp * 10) + graph[row][col];
+                    }else {
+                        total += tmp;
+                        tmp = 0;
+                    }
+                }
+                total += tmp;
+            }
+
+            answer = Math.max(answer, total);
+        }
 
         System.out.println(answer);
-    }
-
-    static void backtracking(int row, int col) {
-        int next_row = row, next_col = col;
-
-        if (col == M - 1) {
-            next_col = 0;
-            next_row++;
-        } else {
-            next_col++;
-        }
-
-        if (row == N) {
-            answer = Math.max(answer, tmp);
-            return;
-        }
-
-        if (visited[row][col]) {
-            backtracking(next_row, next_col);
-            return;
-        }
-        
-        for (int i = 0; i < 2; i++) {
-            int base_row = row;
-            int base_col = col;
-
-            StringBuilder sb = new StringBuilder();
-            Stack<int[]> stack = new Stack<>();
-            
-            while (true) {
-                stack.push(new int[] {base_row, base_col});
-                
-                for (int[] arr : stack)
-                    visited[ arr[0] ][ arr[1] ] = true;      
-                sb.append(graph[base_row][base_col]);
-                tmp += Integer.parseInt(sb.toString());
-
-                backtracking(next_row, next_col);
-
-                for (int[] arr : stack)
-                    visited[ arr[0] ][ arr[1] ] = false;
-                tmp -= Integer.parseInt(sb.toString());
-    
-                base_row += dy[i]; base_col += dx[i];
-                if ( base_row < 0 || base_row == N 
-                     || base_col < 0 || base_col == M
-                       || visited[base_row][base_col]) break; 
-            }
-        }
     }
 }
