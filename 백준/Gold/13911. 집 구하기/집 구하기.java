22d120685109a152b6c846d6b863588d7985192b
Hong Boem Sun <74814641+qjatjs123123@users.noreply.github.com>
2026-04-07 00:03:40 +0900

    [Gold II] Title: 집 구하기, Time: 916 ms, Memory: 111684 KB -BaekjoonHub

--- 백준/Gold/13911. 집 구하기/집 구하기.java
+++ 백준/Gold/13911. 집 구하기/집 구하기.java
@@ -6,100 +6,99 @@ import java.io.*;
 class Main {
     static int V, E;
     static ArrayList<Edge>[] graph;
-    static class Edge {
+    static int M, limit_M;
+    static int S, limit_S;
+    static FastScanner fs;
+    static class Edge{
         int to, dist;
 
         Edge(int to, int dist) {
             this.to = to;
             this.dist = dist;
         }
+
+    }
+    
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        int nextInt() throws IOException {
+            while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+            return Integer.parseInt(st.nextToken());
+        }
     }
     
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-
+        fs = new FastScanner();
+        V = fs.nextInt(); E = fs.nextInt();
+        int[] distance_M = new int[V + 1];
+        int[] distance_S = new int[V + 1];
         graph = new ArrayList[V + 1];
-        for (int i = 0; i <= V; i++) graph[i] = new ArrayList<>();
+        
+        for (int i = 0; i <= V; i++) {
+            graph[i] = new ArrayList<>();
+            distance_M[i] = Integer.MAX_VALUE;
+            distance_S[i] = Integer.MAX_VALUE;
+        }
         
         for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int dist = Integer.parseInt(st.nextToken());
+            int from = fs.nextInt(); 
+            int to = fs.nextInt();
+            int dist = fs.nextInt();
 
             graph[from].add(new Edge(to, dist));
             graph[to].add(new Edge(from, dist));
         }
 
-        int M, X;
-        st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
-        int[] hamburgerArr = new int[M];
-        int[] hamburgerDp = new int[V + 1];
+        M = fs.nextInt();
+        limit_M = fs.nextInt();
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            hamburgerArr[i] = Integer.parseInt(st.nextToken());
-        }
+        dijkstra(distance_M, M);
 
-        int S, Y;
-        st = new StringTokenizer(br.readLine());
-        S = Integer.parseInt(st.nextToken());
-        Y = Integer.parseInt(st.nextToken());
-        int[] coffeeArr = new int[S];
-        int[] coffeeDp = new int[V + 1];
-        
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < S; i++) {
-            coffeeArr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for (int i = 0; i <= V; i++) {
-            hamburgerDp[i] = Integer.MAX_VALUE;
-            coffeeDp[i] = Integer.MAX_VALUE;
-        }
-        
-        dijkstra(hamburgerDp, hamburgerArr);
-        dijkstra(coffeeDp, coffeeArr);
+        S = fs.nextInt();
+        limit_S = fs.nextInt();
+        dijkstra(distance_S, S);
 
         int answer = Integer.MAX_VALUE;
         for (int i = 1; i <= V; i++) {
-            if (hamburgerDp[i] == 0 || coffeeDp[i] == 0) continue;
-            if (hamburgerDp[i] <= X && coffeeDp[i] <= Y) {
-                answer = Math.min(answer, hamburgerDp[i] + coffeeDp[i]);
-            }
+            if (distance_M[i] == 0 || distance_S[i] == 0) continue;
+            if (distance_M[i] > limit_M || distance_S[i] > limit_S) continue;
+            answer = Math.min(answer, distance_M[i] + distance_S[i]);
         }
-        
-        if (answer == Integer.MAX_VALUE) System.out.println(-1);
-        else System.out.println(answer);
+
+        if (answer == Integer.MAX_VALUE)
+            System.out.println(-1);
+        else
+            System.out.println(answer);
     }
 
-    static void dijkstra(int[] dp, int[] nodeArr) {
+    static void dijkstra(int[] distance, int cnt) throws IOException{
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         
-        for (int node : nodeArr) {
-            pq.add(new int[] {0, node});
-            dp[node] = 0;  
+        for (int i = 0; i < cnt; i++) {
+            int idx = fs.nextInt();
+            distance[idx] = 0;
+            pq.add(new int[] {0, idx});
         }
 
         while (!pq.isEmpty()) {
             int[] cur_arr = pq.poll();
-            int cur_dist = cur_arr[0], cur_node = cur_arr[1];
+            int cur_dist = cur_arr[0];
+            int cur_no = cur_arr[1];
 
-            if (dp[cur_node] < cur_dist) continue;
+            if (distance[cur_no] < cur_dist) continue;
+            ArrayList<Edge> list = graph[cur_no];
 
-            ArrayList<Edge> list = graph[cur_node];
-            for (Edge edge : list) {
-                int new_dist = edge.dist + cur_dist;
+            for (Edge edge: list) {
+                int new_dist = cur_dist + edge.dist;
 
-                if (dp[edge.to] <= new_dist) continue;
-                dp[edge.to] = new_dist;
+                if (distance[edge.to] <= new_dist) continue;
+                distance[edge.to] = new_dist;
                 pq.add(new int[] {new_dist, edge.to});
             }
         }
